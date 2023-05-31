26d283ce031cce51d18de1ac5c84ae00575696f2
cephaschapa <cephaschapa@gmail.com>
2023-05-31 14:41:55 +0200

    added initial ui files and deps

---
+++ ui-prototype/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import { Inter, Poppins } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ["300", "400", "500", "600"],
+})
+
+export const metadata = {
+  title: 'OpenClimate Cities',
+  description: 'OpenClimate Cities',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}

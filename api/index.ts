a8d4d2c948ca0f0ebe1df3a7b7242437d84eb75f
Milan Gruner <milangruner@gmail.com>
2023-07-05 15:37:30 +0200

    feat(api): create index.ts file with basic Express server

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import express, { Express,Request,Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('CityCatalyst API');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️Server running at http://localhost:${port}`);
+});
+

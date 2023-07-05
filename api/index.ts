4719dcfe59af1da3b82552db906db379c2efa963
Milan Gruner <milangruner@gmail.com>
2023-07-05 17:17:12 +0200

    feat(api): allow JSON and URL encoded request bodies and use auth router
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,17 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { errorHandler } from 'util/error-handler';
+import { errorHandler } from './util/error-handler';
+import authRouter from './routes/auth';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT ?? 8080;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(errorHandler);
+app.use('/api/v1/auth', authRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('CityCatalyst API');

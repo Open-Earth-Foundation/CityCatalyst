ba24d2b66af73c3955eb739aaf85dd1294675114
Milan Gruner <milangruner@gmail.com>
2023-07-05 15:44:52 +0200

    feat(api): set default value for port if not defined in env
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,10 @@
-import express, { Express,Request,Response } from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT ?? 8080;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('CityCatalyst API');

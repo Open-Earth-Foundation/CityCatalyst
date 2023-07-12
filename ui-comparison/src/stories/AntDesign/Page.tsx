38268d77f7cc4933584dab005ae24d64c6ff834c
Milan Gruner <milangruner@gmail.com>
2023-07-12 12:22:20 +0200

    feat(ui): create ant design story and page
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

---
+++ ui-comparison/src/stories/AntDesign/Page.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Poppins } from 'next/font/google';
+import { MdAddchart, MdArrowForward, MdBarChart, MdOutlineAnalytics, MdArrowOutward, MdOutlinePeopleAlt, MdAspectRatio, MdOutlineHomeWork, MdInfoOutline } from 'react-icons/md';
+import { FaFileExport } from 'react-icons/fa';
+import { PiNotePencil, PiTrashLight } from 'react-icons/pi';
+import { BsTruck } from 'react-icons/bs';
+import { FiDownload } from 'react-icons/fi';
+import { Button } from "antd";
+import { DownloadOutlined } from '@ant-design/icons';
+import type { SizeType } from 'antd/es/config-provider/SizeContext';
+
+// import { Header } from "./Header";
+// import { Footer } from "./Footer";
+
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+});
+
+export function Page() {
+  const [size, setSize] = useState<SizeType>('large'); // default is 'middle'
+  return (
+    <div className={`${poppins.className}`}>
+      Hello
+      <Button type="primary" shape="round" icon={<DownloadOutlined />}>
+        Download
+      </Button>
+    </div>
+  );
+}

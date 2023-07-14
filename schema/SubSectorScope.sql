809c16429e4dd8279e065db38eb4470cf7a6543f
Luke Gloege <lukegloege@gmail.com>
2023-07-14 16:12:22 -0400

    feat: add SubSectorScope table

---
+++ schema/SubSectorScope.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "SubSectorScope"(
+    "subsector_id" uuid,
+    "scope_id" uuid,
+    "created" timestamp,
+    "last_updated" timestamp,
+    PRIMARY KEY ("subsector_id", "scope_id");
+    CONSTRAINT "FK_SubSectorScope.subsector_id"
+        FOREIGN KEY("subsector_id")
+        REFERENCES "SubSector" ("subsector_id"),
+    CONSTRAINT "FK_SubSectorScope.scope_id"
+        FOREIGN KEY("scope_id")
+        REFERENCES "Scope" ("scope_id")
+);

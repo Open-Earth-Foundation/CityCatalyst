0fc02a8329da284176c6715d0fc624683720f110
Luke Gloege <lukegloege@gmail.com>
2023-07-14 14:46:10 -0400

    feat: add Version table

---
+++ schema/Version.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "Version"(
+  "version_id" uuid,
+  "year" int,
+  "version" varchar(255),
+  "inventory_id" uuid,
+  PRIMARY KEY "version_id",
+  CONSTRAINT ("FK_Version.inventory_id")
+    FOREIGN KEY ("inventory_id")
+      REFERENCES "Inventory"("inventory_id")
+);

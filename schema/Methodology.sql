5f4d06efd7cf9c4812045fe16744a38c7eeb9894
Luke Gloege <lukegloege@gmail.com>
2023-07-14 15:09:28 -0400

    feat: add FK to DataSource and change methodology_link to methodology_url

--- schema/Methodology.sql
+++ schema/Methodology.sql
@@ -1,8 +1,12 @@
 CREATE TABLE "Methodology"(
     "methodology_id" uuid, /* Unique identifier for the methodology */
     "methodology" varchar(255), /* Description or name of methodology being used */
-    "methodology_link" varchar(255), /* Link for human-readable methodology documentation */
+    "methodology_url" varchar(255), /* Link for human-readable methodology documentation */
+    "datasource_id" uuid,
     "created" timestamp,
     "last_updated" timestamp,
-    PRIMARY KEY("methodology_id")
+    PRIMARY KEY("methodology_id"),
+    CONSTRAINT "FK_Methodology.datasource_id"
+      FOREIGN KEY ("datasource_id")
+        REFERENCES "DataSource"("datasource_id")
 );

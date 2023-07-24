3c2214af0680f00d91f74214140cf1e2608db7e0
Milan Gruner <milangruner@gmail.com>
2023-07-24 10:37:45 +0200

    fix(schema): resolve comments about User schema

--- schema/User.sql
+++ schema/User.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user (
-    id uuid PRIMARY KEY,
+    user_id uuid PRIMARY KEY,
     name text,
     picture_url text,
     is_organization boolean DEFAULT false,
@@ -7,6 +7,7 @@ CREATE TABLE user (
     password_hash text,
     role text,
     created timestamp,
+    last_updated timestamp,
     organization_id uuid,
     CONSTRAINT "FK_user.organization_id"
         FOREIGN KEY("organization_id")

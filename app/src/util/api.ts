00994a570ad089da644c306da9de54a715050b09
Milan Gruner <milangruner@gmail.com>
2023-07-27 15:29:04 +0200

    feat(api): initialize Sequelize on first API request
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/util/api.ts
+++ app/src/util/api.ts
@@ -2,10 +2,18 @@ import createHttpError from "http-errors";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
 
+import { db } from "@/models";
+
 export function apiHandler(handler: (req: Request) => Promise<NextResponse>) {
   return async (req: Request) => {
     try {
+      if (!db.initialized) {
+        await db.initialize();
+      }
+
       // TODO JWT authentication logic here
+      // await jwtMiddleware(req);
+
       return await handler(req);
     } catch (err) {
       return errorHandler(err, req);

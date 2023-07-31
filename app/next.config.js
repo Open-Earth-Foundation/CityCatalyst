ff0edc550a73d90b4d938dd0f7bc9e146a1e527f
Milan Gruner <milangruner@gmail.com>
2023-07-31 15:57:32 +0200

    fix(api): remove unused serverRuntimeConfig from next.config.js
    Using env directly is recommended for newer NextJS versions
    
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/next.config.js
+++ app/next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverRuntimeConfig: {
-    dbConfig: {
-      host: process.env.DATABASE_HOST,
-      name: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-    },
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
 

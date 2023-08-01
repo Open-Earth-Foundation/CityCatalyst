2c27eb28e5d166801d790eaaf37a878ff18bfbb6
Milan Gruner <milangruner@gmail.com>
2023-08-01 18:14:44 +0200

    fix(schema): add created and last_updated fields to Version

--- schema/db.sql
+++ schema/db.sql
@@ -459,6 +459,8 @@ CREATE TABLE "Version" (
   "year" int,
   "version" varchar(255),
   "inventory_id" uuid,
+  "created" timestamp,
+  "last_updated" timestamp,
   PRIMARY KEY ("version_id"),
   CONSTRAINT "FK_Version.inventory_id"
     FOREIGN KEY ("inventory_id")

39cb42998c5ea314b0061151705d07f0102a7fed
Milan Gruner <milangruner@gmail.com>
2023-08-02 17:53:39 +0200

    feat(ui): setup Chakra UI providers
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

---
+++ app/src/app/providers.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import React from 'react';
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider } from '@chakra-ui/react';
+
+export function Providers({
+  children
+}: {
+  children: React.ReactNode,
+}) {
+  return (
+    <CacheProvider>
+      <ChakraProvider>
+        {children}
+      </ChakraProvider>
+    </CacheProvider>
+  );
+}
+

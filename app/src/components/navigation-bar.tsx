dfe78f5fc5f05d93afe1ed1fbabbf748efebcab5
Milan Gruner <milangruner@gmail.com>
2023-08-02 21:22:53 +0200

    feat(ui): add styling and icon to NavigationBar
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/components/navigation-bar.tsx
+++ app/src/components/navigation-bar.tsx
@@ -1,7 +1,13 @@
+import { Link } from '@chakra-ui/next-js';
+import { Text } from '@chakra-ui/react';
+import Image from 'next/image';
+
 export function NavigationBar() {
   return (
-    <div className="bg-blue-500 h-15">
-      CityCatalyst
+    <div className="flex flex-row space-between px-8 py-4 align-middle bg-[#001EA7]">
+      <Image src="/logo.svg" width={36} height={36} alt="CityCatalyst logo" className="mr-[56px]" />
+      <Text size="18" color="white" className="font-bold mt-2">CityCatalyst</Text>
+      <Link href="/help" color="white" size="16" className="opacity-75 mt-2 ml-auto">Help</Link>
     </div>
   )
 }

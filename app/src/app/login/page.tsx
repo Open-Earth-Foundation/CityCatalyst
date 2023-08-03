826c41aa5fe988c1d26a46b5b154b6ed42044dce
Milan Gruner <milangruner@gmail.com>
2023-08-03 15:26:21 +0200

    feat(ui): clean up styling on login page
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/login/page.tsx
+++ app/src/app/login/page.tsx
@@ -14,23 +14,25 @@ type Inputs = {
 };
 
 export default function Login() {
-  const { register, handleSubmit, formState: { errors }, control } = useForm<Inputs>();
+  const { handleSubmit, formState: { errors }, control } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   const [showPassword, setShowPassword] = useState(false);
   const handlePasswordVisibility = () => setShowPassword(!showPassword);
 
   return (
-    <main>
+    <main className="h-screen">
       <NavigationBar />
-      <div className="flex flex-row">
-        <Image src="/path_vector_login.svg" width={0} height={0} sizes="100vw 200px" alt="City roads" className="h-full w-[400px]" />
+      <div className="flex flex-row items-stretch h-full">
+        <div className="bg-[#02061c] h-full">
+          <Image src="/path_vector_login.svg" width={0} height={0} sizes="100vw 200px" alt="City roads" className="w-[400px]" />
+        </div>
         <div className="w-full">
           <div className="pt-[148px] w-[445px] mx-auto">
             <Heading>Log In to City Catalyst</Heading>
             <Text className="my-4" color="#7A7B9A">Please enter your details to log in to your account</Text>
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-              <FormControl isRequired>
+              <FormControl>
                 <FormLabel>Email address</FormLabel>
                 <Controller
                   name="email"
@@ -39,12 +41,12 @@ export default function Login() {
                     required: true,
                   }}
                   render={({ field }) => (
-                    <Input type="email" placeholder="e.g. youremail@domain.com" size="md" {...field} aria-required={true} />
+                    <Input type="email" placeholder="e.g. youremail@domain.com" size="lg" {...field} aria-required={true} />
                   )}
                 />
                 {errors.email && <FormErrorMessage>Email is required.</FormErrorMessage>}
               </FormControl>
-              <FormControl isRequired>
+              <FormControl>
                 <FormLabel>Password</FormLabel>
                 <Controller
                   name="password"
@@ -54,25 +56,25 @@ export default function Login() {
                   }}
                   render={({ field }) => (
                     <InputGroup>
-                      <Input type={showPassword ? 'text' : 'password'} size="md" placeholder="········" {...field} aria-required={true} />
-                      <InputRightElement width="3rem">
-                        <Button h="2rem" size="sm" onClick={handlePasswordVisibility}>
-                          {showPassword ? <ViewOffIcon /> : <ViewIcon />}
+                      <Input type={showPassword ? 'text' : 'password'} size="lg" placeholder="········" {...field} aria-required={true} />
+                      <InputRightElement width="3rem" mr={2}>
+                        <Button h="2rem" size="md" mt={2} onClick={handlePasswordVisibility} variant="ghost">
+                          {showPassword ? <ViewOffIcon color="#7A7B9A" /> : <ViewIcon color="#7A7B9A" />}
                         </Button>
                       </InputRightElement>
                     </InputGroup>
                   )}
                 />
-                {errors.email && <FormErrorMessage>Email is required.</FormErrorMessage>}
+                {errors.password && <FormErrorMessage>Password is required.</FormErrorMessage>}
               </FormControl>
               <div className="w-full text-right">
-                <Link href="/forgot-password" color="#2351DC">Forgot password</Link>
+                <Link href="/forgot-password">Forgot password</Link>
               </div>
-              <Button type="submit" color="white" className="w-full bg-[#2351DC] uppercase rounded-[50px] h-16">Log in</Button>
+              <Button type="submit" h={16} width="full" className="bg-[#2351DC]">Log in</Button>
             </form>
-            <Text className="w-full text-center mt-4 font-font text-sm" color="#7A7B9A">
+            <Text className="w-full text-center mt-4 text-sm" color="#7A7B9A">
               Don't have an account?{' '}
-              <Link href="/signup" color="#2351DC">Sign up</Link>
+              <Link href="/signup">Sign up</Link>
             </Text>
           </div>
         </div>

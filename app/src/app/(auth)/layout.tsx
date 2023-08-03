fea48538cd9e332c76570a3795f78bb71fa3391a
Milan Gruner <milangruner@gmail.com>
2023-08-03 17:12:49 +0200

    feat(ui): make auth layout responsive
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/(auth)/layout.tsx
+++ app/src/app/(auth)/layout.tsx
@@ -8,11 +8,11 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
     <main className="h-screen flex flex-col">
       <NavigationBar />
       <div className="flex flex-row items-stretch h-full">
-        <div className="bg-[#02061c] h-full">
+        <div className="bg-[#02061c] h-full hidden md:block">
           <Image src="/path_vector_login.svg" width={0} height={0} sizes="100vw 200px" alt="City roads" className="w-[400px]" />
         </div>
         <div className="w-full">
-          <div className="pt-[148px] w-[445px] mx-auto">
+          <div className="pt-[148px] w-[445px] max-w-full mx-auto px-4">
             {children}
           </div>
         </div>

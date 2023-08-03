c28fadbdf5afd6c8dfd2b9be47813204521b8b19
Milan Gruner <milangruner@gmail.com>
2023-08-03 15:20:35 +0200

    feat(ui): extract custom button styles to theme config
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/providers.tsx
+++ app/src/app/providers.tsx
@@ -16,6 +16,52 @@ export const theme = extendTheme({
     heading: 'var(--font-poppins)',
     body: 'var(--font-rubik)',
   },
+  components: {
+    Button: {
+      baseStyle: {
+        textTransform: 'uppercase',
+        borderRadius: 50,
+      },
+      variants: {
+        outline: {
+          border: '2px solid',
+          borderColor: '#2351DC',
+          color: '#2351DC',
+          _hover: {
+            transform: 'scale(0.98)',
+            borderColor: '#5a7be0',
+            color: '#5a7be0',
+          },
+          _active: {
+            borderColor: '#899ee0',
+            color: '#899ee0',
+          },
+          _loading: {
+            opacity: 0.8,
+          },
+        },
+        solid: {
+          bg: '#2351DC',
+          color: 'white',
+          _hover: {
+            transform: 'scale(0.98)',
+            bg: '#5a7be0',
+          },
+          _active: {
+            bg: '#899ee0',
+          },
+          _loading: {
+            opacity: 0.8,
+          },
+        },
+      },
+    },
+    Link: {
+      baseStyle: {
+        color: '#2351DC',
+      },
+    },
+  },
 });
 
 export function Providers({
@@ -33,7 +79,7 @@ export function Providers({
         `}
       </style>
       <CacheProvider>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
           {children}
         </ChakraProvider>
       </CacheProvider>

7209f2d1e02b762cded5e25940dee057c1d641e4
Milan Gruner <milangruner@gmail.com>
2023-08-03 20:00:24 +0200

    fix(ui): properly use NextLink instead of router on CheckEmail page
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/(auth)/check-email/page.tsx
+++ app/src/app/(auth)/check-email/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 
 import { Button, Heading, Text } from "@chakra-ui/react";
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { Link } from '@chakra-ui/next-js';
+import NextLink from 'next/link';
 
 export default function CheckEmail() {
-  const router = useRouter();
   const searchParams = useSearchParams();
   const email = searchParams.get('email');
 
   const emailSection = email ? (
-    <>At <Link href={`mailto://${email}`}>{email}</Link></>
+    <>At <Link href={`mailto:${email}`}>{email}</Link></>
   ) : 'In your inbox';
 
   return (
@@ -18,16 +18,11 @@ export default function CheckEmail() {
       <Heading size="xl">Check Your Email</Heading>
       <Text className="my-4" color="#7A7B9A">Thank you for creating your City Catalyst account!</Text>
       <Text className="my-4" color="#7A7B9A">{emailSection}, you will receive an email with instructions and credentials for your new account. If you do not receive it, please check your spam folder.</Text>
-      <Button
-        as="a"
-        h={16}
-        width="full"
-        className="bg-[#2351DC]"
-        mt={4}
-        onClick={() => router.push('/login')}
-      >
-        Back to Log In
-      </Button>
+      <NextLink href="/login" passHref legacyBehavior>
+        <Button as="a" h={16} width="full" mt={4}>
+          Back to Log In
+        </Button>
+      </NextLink>
     </>
   );
 }

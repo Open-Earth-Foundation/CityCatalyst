902776cdb83e6065934982987d6f3c1e212563a1
Milan Gruner <milangruner@gmail.com>
2023-08-03 19:27:09 +0200

    fix(ui): text not vertically centered in NavigationBar
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/components/navigation-bar.tsx
+++ app/src/components/navigation-bar.tsx
@@ -6,8 +6,8 @@ export function NavigationBar() {
   return (
     <div className="flex flex-row space-between px-8 py-4 align-middle bg-[#001EA7]">
       <Image src="/logo.svg" width={36} height={36} alt="CityCatalyst logo" className="mr-[56px]" />
-      <Text size="18" color="white" className="font-bold mt-2">CityCatalyst</Text>
-      <Link href="/help" color="white" size="16" className="opacity-75 mt-2 ml-auto">Help</Link>
+      <Text size="18" color="white" className="font-bold mt-1">CityCatalyst</Text>
+      <Link href="/help" color="white" size="16" className="opacity-75 mt-1 ml-auto">Help</Link>
     </div>
   )
 }

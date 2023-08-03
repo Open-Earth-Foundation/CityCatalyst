445118a1cef7f4184cdcb82f4f021e9cd067eb43
Milan Gruner <milangruner@gmail.com>
2023-08-03 20:26:05 +0200

    feat(ui): change content on check email page if it's for a password reset
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/(auth)/check-email/page.tsx
+++ app/src/app/(auth)/check-email/page.tsx
@@ -8,16 +8,27 @@ import NextLink from 'next/link';
 export default function CheckEmail() {
   const searchParams = useSearchParams();
   const email = searchParams.get('email');
-
-  const emailSection = email ? (
-    <>At <Link href={`mailto:${email}`}>{email}</Link></>
-  ) : 'In your inbox';
+  const isReset = !!searchParams.get('reset');
 
   return (
     <>
       <Heading size="xl">Check Your Email</Heading>
-      <Text className="my-4" color="#7A7B9A">Thank you for creating your City Catalyst account!</Text>
-      <Text className="my-4" color="#7A7B9A">{emailSection}, you will receive an email with instructions and credentials for your new account. If you do not receive it, please check your spam folder.</Text>
+      {isReset ? (
+        <Text my={4} color="#7A7B9A">
+          If an account exists for{' '}
+          {email ? <Link href={`mailto:${email}`}>{email}</Link> : 'your email'}
+          , you will get an email with instructions on resetting your password. If it doesn't arrive, be sure to check your spam folder.
+        </Text>
+      ) : (
+        <>
+          <Text className="my-4" color="#7A7B9A">Thank you for creating your City Catalyst account!</Text>
+          <Text className="my-4" color="#7A7B9A">
+            {email ? (
+              <>At <Link href={`mailto:${email}`}>{email}</Link></>
+            ) : 'In your inbox'}
+            , you will receive an email with instructions and credentials for your new account. If you do not receive it, please check your spam folder.</Text>
+        </>
+      )}
       <NextLink href="/login" passHref legacyBehavior>
         <Button as="a" h={16} width="full" mt={4}>
           Back to Log In

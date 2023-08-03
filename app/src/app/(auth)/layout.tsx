f92bea823fc265c9d58b2b81c8ede4ae83a84a4d
Milan Gruner <milangruner@gmail.com>
2023-08-03 20:32:22 +0200

    fix(ui): increase width of auth image and simplify markup
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/(auth)/layout.tsx
+++ app/src/app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
       <NavigationBar />
       <div className="flex flex-row items-stretch h-full">
         <div className="bg-[#02061c] h-full hidden md:block">
-          <Image src="/path_vector_login.svg" width={0} height={0} sizes="100vw 200px" alt="City roads" className="w-[400px]" />
+          <Image src="/path_vector_login.svg" width={485} height={0} sizes="100vw" alt="City roads" />
         </div>
         <div className="w-full">
           <div className="pt-[148px] w-[480px] max-w-full mx-auto px-4">

92f1ed5ed5772d5b7ae014fb16c893004474f5e5
Milan Gruner <milangruner@gmail.com>
2023-08-03 15:20:15 +0200

    fix(ui): disable Tailwind default styles (preflight)
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/stories/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {
       backgroundImage: {

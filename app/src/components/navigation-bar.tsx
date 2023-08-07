1c68bff839f4211e72c7ba7f5ab594480ba12652
Milan Gruner <milangruner@gmail.com>
2023-08-07 13:18:05 +0200

    feat(ui): implement i18n on NavigationBar
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/components/navigation-bar.tsx
+++ app/src/components/navigation-bar.tsx
@@ -1,13 +1,15 @@
+import { useTranslation } from '@/i18n/client';
 import { Link } from '@chakra-ui/next-js';
 import { Text } from '@chakra-ui/react';
 import Image from 'next/image';
 
-export function NavigationBar() {
+export function NavigationBar({ lng }: { lng: string }) {
+  const { t } = useTranslation(lng, 'navigation');
   return (
     <div className="flex flex-row space-between px-8 py-4 align-middle bg-[#001EA7]">
       <Image src="/assets/logo.svg" width={36} height={36} alt="CityCatalyst logo" className="mr-[56px]" />
-      <Text size="18" color="white" className="font-bold mt-1">CityCatalyst</Text>
-      <Link href="/help" color="white" size="16" className="opacity-75 mt-1 ml-auto">Help</Link>
+      <Text size="18" color="white" className="font-bold mt-1">{t('title')}</Text>
+      <Link href="/help" color="white" size="16" className="opacity-75 mt-1 ml-auto">{t('help')}</Link>
     </div>
   )
 }

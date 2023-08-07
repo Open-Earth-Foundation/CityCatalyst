e7394e5019fa698ea830cfc48ce9cf4d1517fad2
Milan Gruner <milangruner@gmail.com>
2023-08-07 17:37:04 +0200

    feat(ui): implement language picker in NavigationBar
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/components/navigation-bar.tsx
+++ app/src/components/navigation-bar.tsx
@@ -2,16 +2,36 @@
 
 import { useTranslation } from '@/i18n/client';
 import { Link } from '@chakra-ui/next-js';
-import { Text } from '@chakra-ui/react';
+import { Select, Text } from '@chakra-ui/react';
+import i18next from 'i18next';
 import Image from 'next/image';
+import { ChangeEventHandler } from 'react';
 
 export function NavigationBar({ lng }: { lng: string }) {
   const { t } = useTranslation(lng, 'navigation');
+  const onChangeLanguage: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    i18next.changeLanguage(event.target.value);
+  };
+
   return (
     <div className="flex flex-row space-between px-8 py-4 align-middle bg-[#001EA7]">
       <Image src="/assets/logo.svg" width={36} height={36} alt="CityCatalyst logo" className="mr-[56px]" />
       <Text size="18" color="white" className="font-bold mt-1">{t('title')}</Text>
-      <Link href="/help" color="white" size="16" className="opacity-75 mt-1 ml-auto">{t('help')}</Link>
+      <div className="w-full" />
+      <Select
+        variant="unstyled"
+        onChange={onChangeLanguage}
+        defaultValue={lng}
+        w={20}
+        size="md"
+        color="white"
+        mt={1}
+      >
+        <option value="en">EN</option>
+        <option value="de">DE</option>
+        <option value="es">ES</option>
+      </Select>
+      <Link href="/help" color="white" size="16" className="opacity-75 mt-1" ml={6}>{t('help')}</Link>
     </div>
   )
 }

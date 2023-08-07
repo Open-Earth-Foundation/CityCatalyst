4c7c99140879081bb06a1bb91463a01a6864e91d
Milan Gruner <milangruner@gmail.com>
2023-08-07 12:07:28 +0200

    refactor(ui): move all routes into [lng] folder to enable i18n for routes
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

---
+++ app/src/app/[lng]/layout.tsx
@@ -0,0 +1,33 @@
+import '../globals.css'
+import type { Metadata } from 'next'
+import { Providers } from '../providers'
+import { dir } from 'i18next'
+import { languages } from '@/i18n/settings'
+
+export const metadata: Metadata = {
+  title: 'CityCatalyst',
+  description: 'Make building a climate inventory a breeze',
+}
+
+export async function generateStaticParams() {
+  return languages.map((lng: string) => ({ lng }));
+}
+
+export default function RootLayout({
+  children,
+  params: { lng }
+}: {
+  children: React.ReactNode,
+  params: { lng: string }
+}) {
+  return (
+    <html lang={lng} dir={dir(lng)}>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  )
+}
+
--- app/src/app/[lng]/page.tsx
+++ app/src/app/[lng]/page.tsx
@@ -1,21 +1,21 @@
 'use client'
 
 import { NavigationBar } from '@/components/navigation-bar'
-import { Button, Heading, Text } from '@chakra-ui/react'
+import { Button, Heading } from '@chakra-ui/react'
 import Link from 'next/link'
 
-export default function Home() {
+export default function Home({ params: { lng } }: { params: { lng: String } }) {
   return (
     <main>
       <NavigationBar />
       <div className="p-24 flex flex-col space-y-4 items-start">
         <Heading>Dashboard</Heading>
-        <Link href="/login" passHref legacyBehavior>
+        <Link href={`/${lng}/login`} passHref legacyBehavior>
           <Button as="a" w={32}>
             Login
           </Button>
         </Link>
-        <Link href="/signup" passHref legacyBehavior>
+        <Link href={`/${lng}/signup`} passHref legacyBehavior>
           <Button as="a" w={32}>
             Sign Up
           </Button>

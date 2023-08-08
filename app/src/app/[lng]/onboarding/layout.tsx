dd8e1f4d95a9477a6ad2ccc1ef9a0dbc2bdce742
Milan Gruner <milangruner@gmail.com>
2023-08-08 17:35:40 +0200

    refactor(ui): move content width div from layout to Onboarding page
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/[lng]/onboarding/layout.tsx
+++ app/src/app/[lng]/onboarding/layout.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import Image from 'next/image';
 import { NavigationBar } from "@/components/navigation-bar";
 
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
+export default function AuthLayout({
+  children,
+  params: { lng },
+}: {
+  children: React.ReactNode,
+  params: { lng: string },
+}) {
   return (
     <main className="h-screen flex flex-col">
-      <NavigationBar />
+      <NavigationBar lng={lng} />
       <div className="w-full h-full bg-city bg-left-bottom bg-no-repeat">
-        <div className="pt-[148px] w-[750px] max-w-full mx-auto px-4">
-          {children}
-        </div>
+        {children}
       </div>
     </main>
   );
--- app/src/app/[lng]/onboarding/page.tsx
+++ app/src/app/[lng]/onboarding/page.tsx
@@ -5,19 +5,19 @@ import NextLink from 'next/link';
 
 export default function Onboarding() {
   return (
-    <>
+    <div className="pt-[148px] w-[750px] max-w-full mx-auto px-4">
       <Heading size="xl" color="brand" mb={6}>Let's Start Your Emissions Inventory!</Heading>
       <Text color="tertiary">
         We need to set and confirm the city where you want to do the inventory.
         <br />
-You are one step closer to getting your GPC emissions inventory!
+        You are one step closer to getting your GPC emissions inventory!
       </Text>
       <NextLink href="/onboarding/setup" passHref legacyBehavior>
         <Button as="a" h={16} px={6} mt={8}>
           Start City Inventory Profile
         </Button>
       </NextLink>
-    </>
+    </div>
   );
 }
 

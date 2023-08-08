2af1c6e398e7055eb2d615d7e6301e36600d14ee
Milan Gruner <milangruner@gmail.com>
2023-08-08 13:54:00 +0200

    fix(ui): put search params related markup into a child component in Suspense boundary
    To prevent the whole page having to be rendered on the client
    
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/[lng]/(auth)/check-email/page.tsx
+++ app/src/app/[lng]/(auth)/check-email/page.tsx
@@ -5,33 +5,41 @@ import { useSearchParams } from 'next/navigation';
 import { Link } from '@chakra-ui/next-js';
 import NextLink from 'next/link';
 import { useTranslation } from "@/i18n/client";
+import { Suspense } from "react";
 
-export default function CheckEmail({ params: { lng } }: { params: { lng: string } }) {
-  const { t } = useTranslation(lng, 'forgot-password');
+function DynamicContent({ t }: { t: Function }) {
   const searchParams = useSearchParams();
   const email = searchParams.get('email');
   const isReset = !!searchParams.get('reset');
 
+  return isReset ? (
+    <Text my={4} color="#7A7B9A">
+      {t('check-email-reset-prefix')}{' '}
+      {email ? <Link href={`mailto:${email}`}>{email}</Link> : t('your email')}
+      {t('check-email-reset-details')}
+    </Text>
+  ) : (
+    <>
+      <Text className="my-4" color="#7A7B9A">{t('check-email-details1')}</Text>
+      <Text className="my-4" color="#7A7B9A">
+        {email ? (
+          <>{t('check-email-details2-prefix')} <Link href={`mailto:${email}`}>{email}</Link></>
+        ) : t('in-inbox')}
+        {t('check-email-details2')}
+      </Text>
+    </>
+  );
+}
+
+export default function CheckEmail({ params: { lng } }: { params: { lng: string } }) {
+  const { t } = useTranslation(lng, 'forgot-password');
+
   return (
     <>
       <Heading size="xl">{t('check-email-heading')}</Heading>
-      {isReset ? (
-        <Text my={4} color="#7A7B9A">
-          {t('check-email-reset-prefix')}{' '}
-          {email ? <Link href={`mailto:${email}`}>{email}</Link> : t('your email')}
-          {t('check-email-reset-details')}
-        </Text>
-      ) : (
-        <>
-          <Text className="my-4" color="#7A7B9A">{t('check-email-details1')}</Text>
-          <Text className="my-4" color="#7A7B9A">
-            {email ? (
-              <>{t('check-email-details2-prefix')} <Link href={`mailto:${email}`}>{email}</Link></>
-            ) : t('in-inbox')}
-            {t('check-email-details2')}
-          </Text>
-        </>
-      )}
+      <Suspense>
+        <DynamicContent t={t} />
+      </Suspense>
       <NextLink href="/login" passHref legacyBehavior>
         <Button as="a" h={16} width="full" mt={4}>
           {t('back-to-login')}

94cb68cd7b382a216dcbaad07ba5b11369a3c007
Milan Gruner <milangruner@gmail.com>
2023-08-08 17:34:40 +0200

    feat(ui): allow larger drop shadows than 2xl in tailwind config
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -22,6 +22,13 @@ module.exports = {
         'city': 'url("/assets/city_vector_onboarding.svg")',
         'roads': 'url("/assets/path_vector_login.svg")',
       },
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+        '4xl': [
+          '0 35px 35px rgba(0, 0, 0, 0.25)',
+          '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      }
     },
   },
   plugins: [],

c333c823a25ec9fa16c2a650083ee3d2f755a85f
Milan Gruner <milangruner@gmail.com>
2023-08-09 14:44:25 +0200

    feat(ui): translate ResetSuccessful page
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/[lng]/(auth)/reset-successful/page.tsx
+++ app/src/app/[lng]/(auth)/reset-successful/page.tsx
@@ -1,21 +1,21 @@
 'use client'
 
+import { useTranslation } from "@/i18n/client";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 import { Button, Heading, Text } from "@chakra-ui/react";
 import NextLink from 'next/link';
 
-export default function ResetSuccessful() {
+export default function ResetSuccessful({ params: { lng } }: { params: { lng: string } }) {
+  const { t } = useTranslation(lng, 'reset-successful');
   return (
     <>
-      <Heading size="xl">Password Reset</Heading>
+      <Heading size="xl">{t('reset-successful-heading')}</Heading>
       <Text className="my-4" color="#7A7B9A">
-        Your password has been successfully updated.
-        <br/ >
-        Click below to Log In magically.
+        {t('reset-successful-details')}
       </Text>
       <NextLink href="/" passHref legacyBehavior>
         <Button as="a" h={16} width="full" mt={4}>
-          Continue <ArrowForwardIcon ml={2} boxSize={6} />
+          {t('continue')} <ArrowForwardIcon ml={2} boxSize={6} />
         </Button>
       </NextLink>
     </>

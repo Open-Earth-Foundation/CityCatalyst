a3286e0579ea7a5f067aae6d8f2f6e02fc850873
Milan Gruner <milangruner@gmail.com>
2023-08-09 14:50:30 +0200

    feat(ui): translate Onboarding page
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/[lng]/onboarding/page.tsx
+++ app/src/app/[lng]/onboarding/page.tsx
@@ -1,20 +1,22 @@
 'use client'
 
+import { useTranslation } from "@/i18n/client";
 import { Button, Heading, Text } from "@chakra-ui/react";
 import NextLink from 'next/link';
+import { Trans } from "react-i18next/TransWithoutContext";
 
-export default function Onboarding() {
+export default function Onboarding({ params: { lng } }: { params: { lng: string } }) {
+  const { t } = useTranslation(lng, 'onboarding');
+
   return (
     <div className="pt-[148px] w-[750px] max-w-full mx-auto px-4">
-      <Heading size="xl" color="brand" mb={6}>Let's Start Your Emissions Inventory!</Heading>
+      <Heading size="xl" color="brand" mb={6}>{t('onboarding-heading')}</Heading>
       <Text color="tertiary">
-        We need to set and confirm the city where you want to do the inventory.
-        <br />
-        You are one step closer to getting your GPC emissions inventory!
+        <Trans i18nKey="onboarding-details" t={t} />
       </Text>
       <NextLink href="/onboarding/setup" passHref legacyBehavior>
         <Button as="a" h={16} px={6} mt={8}>
-          Start City Inventory Profile
+          {t('start-button')}
         </Button>
       </NextLink>
     </div>

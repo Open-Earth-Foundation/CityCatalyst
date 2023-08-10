04fc7fc4088a10a3873f0de84479ba87fbeb7cf5
mfonsecaOEF <maureen@openearth.org>
2023-08-10 10:16:29 -0600

    HTTP error code >=503, versions updated

--- Global-API/health.py
+++ Global-API/health.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
-from sqlalchemy import create_engine, exc
+from fastapi import FastAPI, HTTPException
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from decouple import config
 
 app = FastAPI()
 
-DATABASE_URL = "TBD"
+# Read the DATABASE_URL from the .env file
+DATABASE_URL = config("DATABASE_URL")
 
 # Create a SQLAlchemy engine for database connection
 engine = create_engine(DATABASE_URL)
@@ -19,8 +21,20 @@ Base = declarative_base()
 @app.get("/health")
 def health_check():
     try:
-        engine.connnect()
+        engine.connect()
         engine.close()
         return {'status': 'ok'}
-    except:
-        return {'status': 'fail'}
+    except Exception as e:
+        # If an exception occurs, return a 503 status code
+        raise HTTPException(status_code=503, detail="Service unavailable")
+
+@app.get("/health")
+def health_check():
+    try:
+        # Attempt to connect to the database
+        with engine.connect():
+            return {'status': 'ok'}
+    except Exception as e:
+        raise HTTPException(status_code=503, detail="Service unavailable")
+    finally:
+        engine.dispose()

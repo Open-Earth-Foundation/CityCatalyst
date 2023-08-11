ec894b1facf2b766b839c3807269e3fd6d11bbf4
Milan Gruner <milangruner@gmail.com>
2023-08-11 18:02:15 +0200

    feat(test): implement two e2e tests for login page
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

---
+++ app/cypress/e2e/login.cy.ts
@@ -0,0 +1,26 @@
+describe('Login page', () => {
+  it('redirects to dashboard after entering correct data', () => {
+    cy.visit('/login')
+    cy.contains('Log In')
+    cy.get('input[name="email"]').type('test@openearth.org')
+    cy.get('input[name="password"]').type('password')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('equal', Cypress.config().baseUrl + '/en')
+    cy.contains('Dashboard')
+  })
+
+  it('shows errors when entering invalid data', () => {
+    cy.visit('/login')
+    cy.contains('Log In')
+
+    cy.get('input[name="email"]').type('testopenearthorg')
+    cy.get('input[name="password"]').type('pas')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('equal', Cypress.config().baseUrl + '/en/login')
+    cy.contains('valid email address')
+    cy.contains('Minimum length')
+  })
+})
+

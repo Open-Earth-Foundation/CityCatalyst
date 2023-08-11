a98257ae7dd7db461d0ad5001bfcb3d548b7f7aa
Milan Gruner <milangruner@gmail.com>
2023-08-11 17:23:39 +0200

    fix(test): only delete entries created in tests, don't use real locode
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/tests/models.test.ts
+++ app/tests/models.test.ts
@@ -6,13 +6,13 @@ import 'dotenv/config';
 import { db } from '@/models';
 
 const email = 'test@openearth.org';
-const locode = 'DE_BER';
+const locode = 'XX_XYZ';
 
 describe('Models', () => {
   before(async () => {
     await db.initialize();
-    await db.models.User.destroy({ truncate: { cascade: true } });
-    await db.models.City.destroy({ truncate: { cascade: true } });
+    await db.models.User.destroy({ where: { email } });
+    await db.models.City.destroy({ where: { locode } });
   });
 
   describe('User model', () => {

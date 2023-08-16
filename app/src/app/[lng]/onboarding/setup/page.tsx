4c3efb152e7199209fb7bf1f996d16fb288a3aef
Milan Gruner <milangruner@gmail.com>
2023-08-16 14:00:30 +0200

    fix(ui): add appropriate bottom margings on onboarding setup and done margings for smaller screens
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/[lng]/onboarding/setup/page.tsx
+++ app/src/app/[lng]/onboarding/setup/page.tsx
@@ -153,7 +153,7 @@ export default function OnboardingSetup({ params: { lng } }: { params: { lng: st
 
   return (
     <>
-      <div className="pt-[64px] w-[1090px] max-w-full mx-auto px-8">
+      <div className="pt-16 w-[1090px] max-w-full mx-auto">
         <Button
           variant="ghost"
           leftIcon={<ArrowBackIcon boxSize={6} />}
@@ -169,7 +169,7 @@ export default function OnboardingSetup({ params: { lng } }: { params: { lng: st
             />
           </div>
         </div>
-        <div className="flex flex-col md:flex-row md:space-x-12 md:space-y-0 space-y-12 align-top mb-24 mt-[112px]">
+        <div className="flex flex-col md:flex-row md:space-x-12 md:space-y-0 space-y-12 align-top mb-24 mt-[112px] pb-16">
           {activeStep === 0 && <SetupStep errors={errors} register={register} t={t} />}
           {activeStep === 1 && <ConfirmStep cityName={getValues('city')} t={t} />}
         </div>

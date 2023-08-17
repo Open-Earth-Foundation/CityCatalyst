5d40058200af235f83d0b2dd2aa8b606d845037f
Milan Gruner <milangruner@gmail.com>
2023-08-17 15:46:55 +0200

    feat(ui): make WizardSteps responsive (becomes vertical on small screens)
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/components/wizard-steps.tsx
+++ app/src/components/wizard-steps.tsx
@@ -1,4 +1,15 @@
-import { Box, Step, StepIcon, StepIndicator, StepNumber, StepSeparator, StepStatus, StepTitle, Stepper, useSteps } from "@chakra-ui/react"
+import {
+  Box,
+  Step,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+  Stepper,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 interface WizardStep {
   title: string;
@@ -9,12 +20,22 @@ export default function WizardSteps({
   currentStep,
   onSelect = () => {},
 }: {
-  steps: WizardStep[],
-  currentStep: number,
-  onSelect?: (selectedStep: number) => void,
+  steps: WizardStep[];
+  currentStep: number;
+  onSelect?: (selectedStep: number) => void;
 }) {
+  const orientation: "horizontal" | "vertical" | undefined = useBreakpointValue(
+    { base: "vertical", md: "horizontal" },
+    { fallback: "md" }
+  );
   return (
-    <Stepper index={currentStep} my={8} colorScheme="brandScheme" size="lg">
+    <Stepper
+      index={currentStep}
+      my={8}
+      colorScheme="brandScheme"
+      size="lg"
+      orientation={orientation}
+    >
       {steps.map((step, index) => (
         <Step key={index} onClick={() => onSelect(index)}>
           <StepIndicator>
@@ -25,7 +46,7 @@ export default function WizardSteps({
             />
           </StepIndicator>
 
-          <Box flexShrink='0'>
+          <Box flexShrink="0">
             <StepTitle>{step.title}</StepTitle>
           </Box>
 
@@ -33,6 +54,5 @@ export default function WizardSteps({
         </Step>
       ))}
     </Stepper>
-  )
+  );
 }
-

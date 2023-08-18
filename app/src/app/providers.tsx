c4c360f981bfa09f38731e4492528793275129a0
Milan Gruner <milangruner@gmail.com>
2023-08-18 17:34:02 +0200

    feat(ui): add borderOverlay color to theme and reformat providers.tsx
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/providers.tsx
+++ app/src/app/providers.tsx
@@ -1,84 +1,88 @@
-'use client'
+"use client";
 
-import { CacheProvider } from '@chakra-ui/next-js';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-import { Open_Sans, Poppins } from 'next/font/google'
-const poppins = Poppins({ weight: '500', subsets: ['latin'] });
-const openSans = Open_Sans({ subsets: ['latin'] });
+import { Open_Sans, Poppins } from "next/font/google";
+const poppins = Poppins({ weight: "500", subsets: ["latin"] });
+const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const theme = extendTheme({
   colors: {
-    brand: '#2351DC',
-    tertiary: '#7A7B9A',
+    brand: "#2351DC",
+    tertiary: "#7A7B9A",
+    borderOverlay: "#E6E7FF",
     brandScheme: {
-      100: '#C5CBF5',
-      500: '#2351DC',
-    }
+      100: "#C5CBF5",
+      500: "#2351DC",
+    },
   },
   fonts: {
-    heading: 'var(--font-poppins)',
-    body: 'var(--font-opensans)',
+    heading: "var(--font-poppins)",
+    body: "var(--font-opensans)",
   },
   components: {
     Button: {
       baseStyle: {
-        textTransform: 'uppercase',
+        textTransform: "uppercase",
         borderRadius: 50,
       },
       variants: {
         outline: {
-          border: '2px solid',
-          borderColor: '#2351DC',
-          color: '#2351DC',
+          border: "2px solid",
+          borderColor: "#2351DC",
+          color: "#2351DC",
           _hover: {
-            transform: 'scale(0.98)',
-            borderColor: '#5a7be0',
-            color: '#5a7be0',
+            transform: "scale(0.98)",
+            borderColor: "#5a7be0",
+            color: "#5a7be0",
           },
           _active: {
-            borderColor: '#899ee0',
-            color: '#899ee0',
+            borderColor: "#899ee0",
+            color: "#899ee0",
           },
           _loading: {
             opacity: 0.8,
           },
         },
         solid: {
-          bg: '#2351DC',
-          color: 'white',
+          bg: "#2351DC",
+          color: "white",
           _hover: {
-            transform: 'scale(0.98)',
-            bg: '#5a7be0',
+            transform: "scale(0.98)",
+            bg: "#5a7be0",
           },
           _active: {
-            bg: '#899ee0',
+            bg: "#899ee0",
           },
           _loading: {
             opacity: 0.8,
             _hover: {
-              bg: '#5a7be0',
+              bg: "#5a7be0",
             },
           },
         },
         ghost: {
-          color: '#5a7be0',
+          color: "#5a7be0",
         },
       },
     },
     Link: {
       baseStyle: {
-        color: '#2351DC',
+        color: "#2351DC",
+      },
+    },
+    Card: {
+      baseStyle: {
+        container: {
+          borderRadius: 8,
+        },
       },
     },
   },
 });
 
-export function Providers({
-  children
-}: {
-  children: React.ReactNode,
-}) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
       <style jsx global>
@@ -90,11 +94,8 @@ export function Providers({
         `}
       </style>
       <CacheProvider>
-        <ChakraProvider theme={theme}>
-          {children}
-        </ChakraProvider>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </CacheProvider>
     </>
   );
 }
-

c349cd7e06bc1ce44718cf7e2d52baa07a4e62e6
Milan Gruner <milangruner@gmail.com>
2023-08-21 18:44:40 +0200

    feat(ui): create CircleIcon from Figma SVG
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

---
+++ app/src/components/icons.tsx
@@ -0,0 +1,11 @@
+import { Icon } from "@chakra-ui/react";
+
+export const CircleIcon = (props: any) => (
+  <Icon viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12C18 15.3137 15.3137 18 12 18Z"
+    />
+  </Icon>
+);
+

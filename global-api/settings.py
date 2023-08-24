a061b0351e1f8c9e00c465686f4ca32c5285ffaf
Praveen Singh <ratinsingh23@gmail.com>
2023-08-25 00:35:39 +0530

    settings file created to read the env file once and store values in instance of settings

---
+++ global-api/settings.py
@@ -0,0 +1,18 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str
+
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()

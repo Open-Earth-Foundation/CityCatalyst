b619df4834a733b15a26cd6bc4390eb2d752964f
Luke Gloege <lukegloege@gmail.com>
2023-09-08 09:40:18 -0400

    feat(ON-774): change column types and rename time_frame to year

--- app/migrations/20230906193347-modify-EmissionsFactor.cjs
+++ app/migrations/20230906193347-modify-EmissionsFactor.cjs
@@ -6,23 +6,23 @@ module.exports = {
     await queryInterface.sequelize.query(`
       -- GPC reference number the EF corresponds to (ex. I.1.2)
       ALTER TABLE "EmissionsFactor"
-      ADD COLUMN "gpc_refno" varchar(255);
+      ADD COLUMN "gpc_refno" varchar(20);
 
       -- chemical formula of the gas (ex. CH4)
       ALTER TABLE "EmissionsFactor"
-      ADD COLUMN "gas" varchar(255);
+      ADD COLUMN "gas" varchar(20);
 
       -- (optional) region the EF is appliable for (ex. US-NY)
       ALTER TABLE "EmissionsFactor"
-      ADD COLUMN "region" varchar(255);
+      ADD COLUMN "region" varchar(10);
 
       -- (optional) time frame the EF is appliable for (ex. 2010)
       ALTER TABLE "EmissionsFactor"
-      ADD COLUMN "time_frame" varchar(255);
+      ADD COLUMN "year" integer;
 
       -- (optional) reference or citation for the EF
       ALTER TABLE "EmissionsFactor"
-      ADD COLUMN "reference" varchar(255);
+      ADD COLUMN "reference" text;
     `);
   },
 
@@ -30,23 +30,23 @@ module.exports = {
     await queryInterface.sequelize.query(`
       -- (optional) reference or citation for the EF
       ALTER TABLE "EmissionsFactor"
-      DROP COLUMN "reference" varchar(255);
+      DROP COLUMN "reference" text;
 
       -- (optional) time frame the EF is appliable for (ex. 2010)
       ALTER TABLE "EmissionsFactor"
-      DROP COLUMN "time_frame" varchar(255);
+      DROP COLUMN "year" integer;
 
       -- (optional) region the EF is appliable for (ex. US-NY)
       ALTER TABLE "EmissionsFactor"
-      DROP COLUMN "region" varchar(255);
+      DROP COLUMN "region" varchar(10);
 
       -- chemical formula of the gas (ex. CH4)
       ALTER TABLE "EmissionsFactor"
-      DROP COLUMN "gas" varchar(255);
+      DROP COLUMN "gas" varchar(20);
 
       -- GPC reference number the EF corresponds to (ex. I.1.2)
       ALTER TABLE "EmissionsFactor"
-      DROP COLUMN "gpc_refno" varchar(255);
+      DROP COLUMN "gpc_refno" varchar(20);
     `);
   }
 };

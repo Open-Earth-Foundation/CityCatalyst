f7eccb99805b9c2ca7265f243bf5fe06c4163e10
Milan Gruner <milangruner@gmail.com>
2023-09-13 17:13:51 +0200

    feat(ui): implement example slice for city

--- app/src/lib/store.ts
+++ app/src/lib/store.ts
@@ -1,10 +1,12 @@
 import { api } from "@/services/api";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import cityReducer from "@/slices/citySlice";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
+    city: cityReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),

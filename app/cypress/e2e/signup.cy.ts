215da3af0f51f048011877f09be8da3afd70a1e4
Milan Gruner <milangruner@gmail.com>
2023-09-13 16:30:45 +0200

    feat(cypress): update signup test
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/cypress/e2e/signup.cy.ts
+++ app/cypress/e2e/signup.cy.ts
@@ -1,12 +1,14 @@
 describe("Signup page", () => {
   it("redirects to dashboard after entering correct data", () => {
+    cy.deleteAccount("signup-test@openearth.org", "Password123!");
+
     cy.visit("/auth/signup");
     cy.contains("Sign Up");
     cy.get('input[name="name"]').type("Test Account");
-    cy.get('input[name="email"]').type("test@openearth.org");
-    cy.get('input[name="password"]').type("Password1");
-    cy.get('input[name="confirmPassword"]').type("Password1");
-    cy.get('input[name="inviteCode"]').type("ABCDEF");
+    cy.get('input[name="email"]').type("signup-test@openearth.org");
+    cy.get('input[name="password"]').type("Password123!");
+    cy.get('input[name="confirmPassword"]').type("Password123!");
+    cy.get('input[name="inviteCode"]').type("123456");
     cy.get('input[name="acceptTerms"]')
       .siblings(".chakra-checkbox__control")
       .click();
@@ -23,7 +25,7 @@ describe("Signup page", () => {
     cy.get('input[name="email"]').type("testopenearthorg");
     cy.get('input[name="password"]').type("Pas");
     cy.get('input[name="confirmPassword"]').type("Pa1");
-    cy.get('input[name="inviteCode"]').type("ABCDE");
+    cy.get('input[name="inviteCode"]').type("12345");
     cy.get('button[type="submit"]').click();
 
     cy.url().should("contain", "/auth/signup");
@@ -40,7 +42,7 @@ describe("Signup page", () => {
     cy.get('input[name="email"]').type("test@openearth.org");
     cy.get('input[name="password"]').type("Password1");
     cy.get('input[name="confirmPassword"]').type("Password2");
-    cy.get('input[name="inviteCode"]').type("ABCDEF");
+    cy.get('input[name="inviteCode"]').type("123456");
     cy.get('input[name="acceptTerms"]')
       .siblings(".chakra-checkbox__control")
       .click();

648dc9d8959f7fba6a0d4d57f5b6c196ce2b32f2
Milan Gruner <milangruner@gmail.com>
2023-09-13 16:30:28 +0200

    feat(cypress): create deleteAccount command
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/cypress/support/commands.ts
+++ app/cypress/support/commands.ts
@@ -1,40 +1,4 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
 // https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
 
 Cypress.Commands.add("signup", (email, password) => {
   cy.request({
@@ -49,12 +13,10 @@ Cypress.Commands.add("signup", (email, password) => {
       inviteCode: "123456",
       acceptTerms: true,
     },
-  }).then((res) => {
-    console.log("Signup res", res);
   });
 });
 
-Cypress.Commands.add("login", async (email, password) => {
+Cypress.Commands.add("login", (email, password) => {
   cy.request("/api/auth/csrf").then((csrfRes) => {
     expect(csrfRes.body).to.have.property("csrfToken");
     const csrfToken = csrfRes.body.csrfToken;
@@ -73,13 +35,27 @@ Cypress.Commands.add("logout", () => {
   cy.get("form").submit();
 });
 
+Cypress.Commands.add("deleteAccount", (email, password) => {
+  // cy.login(email, password);
+  cy.visit("/auth/login");
+  cy.get('input[name="email"]').type(email, { log: false });
+  cy.get('input[name="password"]').type(password, { log: false });
+  cy.get('button[type="submit"]').click();
+
+  cy.request({
+    method: "POST",
+    url: "/api/v0/auth/delete",
+  });
+});
+
 export {};
 declare global {
   namespace Cypress {
     interface Chainable {
       signup(email: string, password: string): Chainable<void>;
-      login(email: string, password: string): Promise<void>;
+      login(email: string, password: string): Chainable<void>;
       logout(): Chainable<void>;
+      deleteAccount(email, password: string): Chainable<void>;
     }
   }
 }

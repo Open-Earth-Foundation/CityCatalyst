1ea72d71cc47f8e472872269614d55c246805b9e
Milan Gruner <milangruner@gmail.com>
2023-09-14 17:13:56 +0200

    fix(ui): enable credentials: "include" on RTK Query API to pass auth cookies
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api/v0/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "/api/v0/", credentials: "include" }),
   endpoints: (builder) => ({
     getCity: builder.query<CityAttributes, string>({
       query: (locode) => `city/${locode}`,

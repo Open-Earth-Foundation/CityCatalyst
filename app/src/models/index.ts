bf5f8345482c7edfe4a4d8503cec28f4c77b26e1
Milan Gruner <milangruner@gmail.com>
2023-09-14 21:08:42 +0200

    fix(api): enable type safety and LSP support for db.models
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/models/index.ts
+++ app/src/models/index.ts
@@ -1,17 +1,83 @@
 import { Sequelize } from "sequelize";
 import pg from "pg";
-import { initModels } from "./init-models";
+import * as models from "./init-models";
 
 export const db: {
   initialized: boolean;
   initialize: () => Promise<void>;
   sequelize?: Sequelize | null;
-  models: Record<string, any>;
+  models: {
+    ActivityData: typeof models.ActivityData;
+    City: typeof models.City;
+    CityUser: typeof models.CityUser;
+    DataSource: typeof models.DataSource;
+    DataSourceActivityData: typeof models.DataSourceActivityData;
+    DataSourceEmissionsFactor: typeof models.DataSourceEmissionsFactor;
+    DataSourceGHGs: typeof models.DataSourceGHGs;
+    DataSourceMethodology: typeof models.DataSourceMethodology;
+    DataSourceReportingLevel: typeof models.DataSourceReportingLevel;
+    DataSourceScope: typeof models.DataSourceScope;
+    DataSourceSector: typeof models.DataSourceSector;
+    DataSourceSubCategory: typeof models.DataSourceSubCategory;
+    DataSourceSubSector: typeof models.DataSourceSubSector;
+    EmissionsFactor: typeof models.EmissionsFactor;
+    GDP: typeof models.GDP;
+    GHGs: typeof models.GHGs;
+    Inventory: typeof models.Inventory;
+    Methodology: typeof models.Methodology;
+    Population: typeof models.Population;
+    Publisher: typeof models.Publisher;
+    ReportingLevel: typeof models.ReportingLevel;
+    Scope: typeof models.Scope;
+    Sector: typeof models.Sector;
+    SectorValue: typeof models.SectorValue;
+    SubCategory: typeof models.SubCategory;
+    SubCategoryValue: typeof models.SubCategoryValue;
+    SubSector: typeof models.SubSector;
+    SubSectorReportingLevel: typeof models.SubSectorReportingLevel;
+    SubSectorScope: typeof models.SubSectorScope;
+    SubSectorValue: typeof models.SubSectorValue;
+    User: typeof models.User;
+    Version: typeof models.Version;
+  };
 } = {
   initialized: false,
   sequelize: null,
   initialize,
-  models: {},
+  models: {
+    ActivityData: models.ActivityData,
+    City: models.City,
+    CityUser: models.CityUser,
+    DataSource: models.DataSource,
+    DataSourceActivityData: models.DataSourceActivityData,
+    DataSourceEmissionsFactor: models.DataSourceEmissionsFactor,
+    DataSourceGHGs: models.DataSourceGHGs,
+    DataSourceMethodology: models.DataSourceMethodology,
+    DataSourceReportingLevel: models.DataSourceReportingLevel,
+    DataSourceScope: models.DataSourceScope,
+    DataSourceSector: models.DataSourceSector,
+    DataSourceSubCategory: models.DataSourceSubCategory,
+    DataSourceSubSector: models.DataSourceSubSector,
+    EmissionsFactor: models.EmissionsFactor,
+    GDP: models.GDP,
+    GHGs: models.GHGs,
+    Inventory: models.Inventory,
+    Methodology: models.Methodology,
+    Population: models.Population,
+    Publisher: models.Publisher,
+    ReportingLevel: models.ReportingLevel,
+    Scope: models.Scope,
+    Sector: models.Sector,
+    SectorValue: models.SectorValue,
+    SubCategory: models.SubCategory,
+    SubCategoryValue: models.SubCategoryValue,
+    SubSector: models.SubSector,
+    SubSectorReportingLevel: models.SubSectorReportingLevel,
+    SubSectorScope: models.SubSectorScope,
+    SubSectorValue: models.SubSectorValue,
+    User: models.User,
+    Version: models.Version,
+  },
 };
 
 async function initialize() {
@@ -24,7 +90,7 @@ async function initialize() {
     dialectModule: pg,
   });
 
-  db.models = initModels(sequelize);
+  db.models = models.initModels(sequelize);
 
   db.sequelize = sequelize;
   db.initialized = true;

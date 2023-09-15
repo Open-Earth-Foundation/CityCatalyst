c45ae50d0b31db85d642baaf0a8e5f6f097d5c3a
Milan Gruner <milangruner@gmail.com>
2023-09-15 12:32:40 +0200

    fix(api): type error in update inventory route
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/api/v0/city/[city]/inventory/[year]/route.ts
+++ app/src/app/api/v0/city/[city]/inventory/[year]/route.ts
@@ -1,5 +1,4 @@
 import { db } from "@/models";
-import { City } from "@/models/City";
 import { apiHandler } from "@/util/api";
 import { createInventoryRequest } from "@/util/validation";
 import createHttpError from "http-errors";
@@ -41,7 +40,7 @@ export const DELETE = apiHandler(async (_req: NextRequest, { params }) => {
 export const PATCH = apiHandler(async (req: NextRequest, { params }) => {
   const body = createInventoryRequest.parse(await req.json());
 
-  let city: City = await db.models.City.findOne({
+  let city = await db.models.City.findOne({
     where: { locode: params.city },
   });
   if (!city) {

22ee75b9c6c259872fe4b51d77ccbb05ea902648
Milan Gruner <milangruner@gmail.com>
2023-09-19 16:25:29 +0200

    feat(ui): create custom SegmentedProgress component
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

---
+++ app/src/components/SegmentedProgress.tsx
@@ -0,0 +1,34 @@
+import { Box, useToken } from "@chakra-ui/react";
+
+export function SegmentedProgress({
+  values,
+  colors = ["interactive.connected", "interactive.tertiary"],
+  max = 1,
+  height = 4,
+}: {
+  values: number[];
+  colors?: string[];
+  max?: number;
+  height?: number;
+}) {
+  const colorValues = useToken("colors", colors);
+  return (
+    <Box
+      backgroundColor="background.neutral"
+      w="full"
+      className="flex flex-row"
+      borderRadius="full"
+    >
+      {values.map((value, i) => (
+        <Box
+          key={i}
+          backgroundColor={colorValues[i]}
+          h={height}
+          w={`${100 * value / max}%`}
+          borderStartRadius={i == 0 ? "full" : undefined}
+          borderEndRadius={i == values.length - 1 ? "full" : undefined}
+        />
+      ))}
+    </Box>
+  );
+}

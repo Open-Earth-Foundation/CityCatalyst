9342340bc9800b033ea4c888e715973ce762f571
Evan Prodromou <evan@openearth.org>
2023-09-25 11:02:52 -0400

    fix: allow setting CC global data from env vars
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- global-api/settings.py
+++ global-api/settings.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings
-
+from pydantic import root_validator
+import os
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
-
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
@@ -14,5 +14,12 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
 
+    @root_validator(pre=True)
+    def override_settings_from_env(cls, values):
+        for key in values:
+            env_key = key.upper()
+            if os.environ.get(env_key):
+                values[key] = os.environ.get(env_key)
+        return values
 
 settings = Settings()

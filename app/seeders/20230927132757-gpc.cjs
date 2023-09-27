2692f3b49e23b91526aca723f81b3330505b5eab
Milan Gruner <milangruner@gmail.com>
2023-09-27 15:59:09 +0200

    feat(api): create seeder for GPC data
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

---
+++ app/seeders/20230927132757-gpc.cjs
@@ -0,0 +1,58 @@
+"use strict";
+
+const fs = require("node:fs");
+const { parse } = require("csv-parse");
+
+async function parseFile(filename) {
+  const records = [];
+  const parser = fs
+    .createReadStream(
+      `${__dirname}/../../gpc_seeder/data_processed/${filename}.csv`,
+    )
+    .pipe(parse({ delimiter: "," }));
+
+  for await (const record of parser) {
+    records.push(record); // TODO transform to object
+  }
+
+  // transform into array of key/ value objects
+  const header = records.shift();
+  const entries = records.map((record) => {
+    return Object.assign(...record.map((value, i) => ({ [header[i]]: value })));
+  });
+  console.log("Entries", entries[0]);
+  return entries;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    const scopes = await parseFile("Scope");
+    const reportingLevels = await parseFile("ReportingLevel");
+    const sectors = await parseFile("Sector");
+    const subSectors = await parseFile("SubSector");
+    const subCategories = await parseFile("SubCategory");
+
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.bulkInsert("Scope", scopes, { transaction });
+      await queryInterface.bulkInsert("ReportingLevel", reportingLevels, {
+        transaction,
+      });
+      await queryInterface.bulkInsert("Sector", sectors, { transaction });
+      await queryInterface.bulkInsert("SubSector", subSectors, { transaction });
+      await queryInterface.bulkInsert("SubCategory", subCategories, {
+        transaction,
+      });
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.bulkDelete("SubCategory", null, { transaction });
+      await queryInterface.bulkDelete("SubSector", null, { transaction });
+      await queryInterface.bulkDelete("Sector", null, { transaction });
+      await queryInterface.bulkDelete("ReportingLevel", null, { transaction });
+      await queryInterface.bulkDelete("Scope", null, { transaction });
+    });
+  },
+};

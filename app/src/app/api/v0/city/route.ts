3592f2df060523cb871ea6ba6a3a3005702b0493
cephaschapa <cephaschapa@gmail.com>
2023-09-28 14:40:41 +0200

    feat: passed the next-auth session in handler, added condition to check session to city route

--- app/src/app/api/v0/city/route.ts
+++ app/src/app/api/v0/city/route.ts
@@ -1,14 +1,24 @@
 import { db } from "@/models";
 import { apiHandler } from "@/util/api";
 import { createCityRequest } from "@/util/validation";
+import createHttpError from "http-errors";
+import { Session } from "next-auth";
 import { NextResponse } from "next/server";
 import { randomUUID } from "node:crypto";
 
-export const POST = apiHandler(async (req: Request) => {
-  const body = createCityRequest.parse(await req.json());
-  const city = await db.models.City.create({
-    cityId: randomUUID(),
-    ...body,
-  });
-  return NextResponse.json({ data: city });
-});
+export const POST = apiHandler(
+  async (
+    req: Request,
+    context: { session?: Session; params: Record<string, string> },
+  ) => {
+    const body = createCityRequest.parse(await req.json());
+    console.log("context: ", context);
+    if (!context.session)
+      throw new createHttpError.Unauthorized("Must be logged in");
+    const city = await db.models.City.create({
+      cityId: randomUUID(),
+      ...body,
+    });
+    return NextResponse.json({ data: city });
+  },
+);

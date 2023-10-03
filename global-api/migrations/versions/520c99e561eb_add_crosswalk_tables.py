886cbddf920c395ac24eb4c394e36dc05f4dea81
Maureen Fonseca Mora <107511484+mfonsecaOEF@users.noreply.github.com>
2023-10-03 17:20:26 +0200

    migration script for crosswalk tables

---
+++ global-api/migrations/versions/520c99e561eb_add_crosswalk_tables.py
@@ -0,0 +1,58 @@
+"""add-Crosswalk-tables
+
+Revision ID: 520c99e561eb
+Revises: bd6c5bbd6eb4
+Create Date: 2023-09-28 08:48:47.879238
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import text
+
+
+# revision identifiers, used by Alembic.
+revision: str = '520c99e561eb'
+down_revision: Union[str, None] = 'bd6c5bbd6eb4'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('crosswalk_GridCell',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('lat_center', sa.Float, nullable=False),
+        sa.Column('lon_center', sa.Float, nullable=False),
+        sa.Column('geometry', sa.String, nullable=False),
+        sa.Column('area', sa.Float, nullable=False),
+        sa.Column("created_date", sa.DateTime(), nullable=False),
+        sa.Column("modified_date", sa.DateTime(), server_default=text('CURRENT_TIMESTAMP'))
+    )
+
+    op.create_table('crosswalk_GridCellEmissions',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('year', sa.Integer, nullable=False),
+        sa.Column('reference_number', sa.String, nullable=False),
+        sa.Column("gas", sa.String(), nullable=False),
+        sa.Column("emissions_quantity", sa.Integer(), nullable=False),
+        sa.Column("emissions_quantity_units", sa.String(), nullable=False),
+        sa.Column('grid_id', sa.Integer, sa.ForeignKey('edgar_grid.id'), nullable=False),
+        sa.Column("created_date", sa.DateTime(), nullable=False),
+        sa.Column("modified_date", sa.DateTime(), server_default=text('CURRENT_TIMESTAMP'))
+    )
+
+    op.create_table('crosswalk_CityGridOverlap',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('locode', sa.String, nullable=False),
+        sa.Column('fraction_in_city', sa.Float, nullable=False),
+        sa.Column('grid_id', sa.Integer, sa.ForeignKey('edgar_grid.id'), nullable=False),
+        sa.Column("created_date", sa.DateTime(), nullable=False),
+        sa.Column("modified_date", sa.DateTime(), server_default=text('CURRENT_TIMESTAMP'))
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('crosswalk_CityGridOverlap')
+    op.drop_table('crosswalk_GridCellEmissions')
+    op.drop_table('crosswalk_GridCell')

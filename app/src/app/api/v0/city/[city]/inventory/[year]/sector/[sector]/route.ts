23b1160f4e65ecbc09c98c75178ec880afe2224a
cephaschapa <cephaschapa@gmail.com>
2023-10-04 15:30:59 +0200

    feat: added get, delete & patch methods to sector route

---
+++ app/src/app/api/v0/city/[city]/inventory/[year]/sector/[sector]/route.ts
@@ -0,0 +1,49 @@
+import { db } from "@/models";
+import { apiHandler } from "@/util/api";
+import { createSectorRequest } from "@/util/validation";
+import createHttpError from "http-errors";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = apiHandler(async (req: NextRequest, { params }) => {
+  const sector = await db.models.Sector.findOne({
+    where: {
+      sectorId: params.sector,
+    },
+  });
+
+  if (!sector) {
+    throw new createHttpError.NotFound("Sector not found");
+  }
+
+  return NextResponse.json({ data: sector });
+});
+
+export const PATCH = apiHandler(async (req: NextRequest, { params }) => {
+  const body = createSectorRequest.parse(await req.json());
+  let sector = await db.models.Sector.findOne({
+    where: { sectorId: params.sector },
+  });
+
+  if (!sector) {
+    throw new createHttpError.NotFound("Sector not found");
+  }
+
+  sector = await sector.update(body);
+
+  return NextResponse.json({ data: sector });
+});
+
+export const DELETE = apiHandler(async (req: NextRequest, { params }) => {
+  const sector = await db.models.Sector.findOne({
+    where: {
+      sectorId: params.sector,
+    },
+  });
+  if (!sector) {
+    throw new createHttpError.NotFound("Sector not found");
+  }
+
+  await sector.destroy();
+
+  return NextResponse.json({ data: sector, delete: true });
+});

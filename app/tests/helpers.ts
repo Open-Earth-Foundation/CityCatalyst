838b78620bd9bbe1fa14ef6382de48d0e50c590f
Milan Gruner <milangruner@gmail.com>
2023-10-06 12:03:26 +0200

    feat(test): start writing tests for datasource routes
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/tests/helpers.ts
+++ app/tests/helpers.ts
@@ -1,8 +1,16 @@
 import { NextRequest } from "next/server";
 import { mock } from "node:test";
 
+const mockUrl = "http://localhost:3000/api/v0";
+
 export function createRequest(url: string, body?: any) {
   const request = new NextRequest(new URL(url));
+  request.json = mock.fn(() => Promise.resolve(body));
+  return request;
+}
+
+export function mockRequest(body?: any) {
+  const request = new NextRequest(new URL(mockUrl));
   request.json = mock.fn(() => Promise.resolve(body));
   return request;
 }

f749c4fe583ccd53b7c44e7889c089367441e364
Maureen Fonseca Mora <107511484+mfonsecaOEF@users.noreply.github.com>
2023-10-09 10:08:13 +0200

    Crosswalk tables updated

--- global-api/migrations/versions/520c99e561eb_add_crosswalk_tables.py
+++ global-api/migrations/versions/520c99e561eb_add_crosswalk_tables.py
@@ -20,39 +20,64 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table('crosswalk_GridCell',
-        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
-        sa.Column('lat_center', sa.Float, nullable=False),
-        sa.Column('lon_center', sa.Float, nullable=False),
-        sa.Column('geometry', sa.String, nullable=False),
-        sa.Column('area', sa.Float, nullable=False),
-        sa.Column("created_date", sa.DateTime(), nullable=False),
-        sa.Column("modified_date", sa.DateTime(), server_default=text('CURRENT_TIMESTAMP'))
+    op.create_table(
+        "crosswalk_GridCell",
+        sa.Column("id", sa.dialects.postgresql.UUID(as_uuid=True), primary_key=True),
+        sa.Column("lat_center", sa.Float, nullable=False),
+        sa.Column("lon_center", sa.Float, nullable=False),
+        sa.Column("geometry", sa.String, nullable=False),
+        sa.Column("area", sa.Float, nullable=False),
+        sa.Column(
+            "created_date", sa.DateTime(), server_default=text("CURRENT_TIMESTAMP")
+        ),
+        sa.Column(
+            "modified_date", sa.DateTime(), server_default=text("CURRENT_TIMESTAMP")
+        ),
     )
 
-    op.create_table('crosswalk_GridCellEmissions',
-        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
-        sa.Column('year', sa.Integer, nullable=False),
-        sa.Column('reference_number', sa.String, nullable=False),
+    op.create_table(
+        "crosswalk_GridCellEmissions",
+        sa.Column("id", sa.dialects.postgresql.UUID(as_uuid=True), primary_key=True),
+        sa.Column("year", sa.Integer, nullable=False),
+        sa.Column("reference_number", sa.String, nullable=False),
         sa.Column("gas", sa.String(), nullable=False),
-        sa.Column("emissions_quantity", sa.Integer(), nullable=False),
+        sa.Column("emissions_quantity", sa.Float(), nullable=False),
         sa.Column("emissions_quantity_units", sa.String(), nullable=False),
-        sa.Column('grid_id', sa.Integer, sa.ForeignKey('edgar_grid.id'), nullable=False),
-        sa.Column("created_date", sa.DateTime(), nullable=False),
-        sa.Column("modified_date", sa.DateTime(), server_default=text('CURRENT_TIMESTAMP'))
+        sa.Column(
+            "cell_id",
+            sa.dialects.postgresql.UUID(as_uuid=True),
+            sa.ForeignKey("crosswalk_GridCell.id"),
+            nullable=False,
+        ),
+        sa.Column(
+            "created_date", sa.DateTime(), server_default=text("CURRENT_TIMESTAMP")
+        ),
+        sa.Column(
+            "modified_date", sa.DateTime(), server_default=text("CURRENT_TIMESTAMP")
+        ),
     )
 
-    op.create_table('crosswalk_CityGridOverlap',
-        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
-        sa.Column('locode', sa.String, nullable=False),
-        sa.Column('fraction_in_city', sa.Float, nullable=False),
-        sa.Column('grid_id', sa.Integer, sa.ForeignKey('edgar_grid.id'), nullable=False),
-        sa.Column("created_date", sa.DateTime(), nullable=False),
-        sa.Column("modified_date", sa.DateTime(), server_default=text('CURRENT_TIMESTAMP'))
+    op.create_table(
+        "crosswalk_CityGridOverlap",
+        sa.Column("id", sa.dialects.postgresql.UUID(as_uuid=True), primary_key=True),
+        sa.Column("locode", sa.String, nullable=False),
+        sa.Column("fraction_in_city", sa.Float, nullable=False),
+        sa.Column(
+            "cell_id",
+            sa.dialects.postgresql.UUID(as_uuid=True),
+            sa.ForeignKey("crosswalk_GridCell.id"),
+            nullable=False,
+        ),
+        sa.Column(
+            "created_date", sa.DateTime(), server_default=text("CURRENT_TIMESTAMP")
+        ),
+        sa.Column(
+            "modified_date", sa.DateTime(), server_default=text("CURRENT_TIMESTAMP")
+        ),
     )
 
 
 def downgrade() -> None:
-    op.drop_table('crosswalk_CityGridOverlap')
-    op.drop_table('crosswalk_GridCellEmissions')
-    op.drop_table('crosswalk_GridCell')
+    op.drop_table("crosswalk_CityGridOverlap")
+    op.drop_table("crosswalk_GridCellEmissions")
+    op.drop_table("crosswalk_GridCell")

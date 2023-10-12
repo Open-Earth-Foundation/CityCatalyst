87c297e6fa257a01a3266f94df45336498192103
Evan Prodromou <evan@openearth.org>
2023-10-12 10:53:55 -0400

    feat: download endpoint for CSV; closes ON-856
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- app/src/app/api/v0/city/[city]/inventory/[year]/route.ts
+++ app/src/app/api/v0/city/[city]/inventory/[year]/route.ts
@@ -1,4 +1,6 @@
 import { db } from "@/models";
+import { SubCategoryValue } from "@/models/SubCategoryValue";
+import { SubSectorValue } from "@/models/SubSectorValue";
 import { apiHandler } from "@/util/api";
 import { createInventoryRequest } from "@/util/validation";
 import createHttpError from "http-errors";
@@ -18,8 +20,75 @@ export const GET = apiHandler(async (_req: NextRequest, { params }) => {
   }
   inventory.city = city;
 
-  return NextResponse.json({ data: inventory });
+  let body:Buffer|null = null;
+  let headers:Record<string, string>|null = null;
+
+  switch (_req.nextUrl.searchParams.get('format')?.toLowerCase()) {
+    case "csv":
+      body = await inventoryCSV(inventory);
+      headers = {
+        "Content-Type": "text/csv",
+        "Content-Disposition": `attachment; filename="inventory-${inventory.city.locode}-${inventory.year}.csv"`,
+      }
+      break;
+    case "xls":
+      body = await inventoryXLS(inventory);
+      headers = {
+        "Content-Type": "application/vnd.ms-excel",
+        "Content-Disposition": `attachment; filename="inventory-${inventory.city.locode}-${inventory.year}.xls"`,
+      }
+      break;
+    case "json":
+    default:
+      body = Buffer.from(JSON.stringify({data: inventory.toJSON()}), "utf-8");
+      headers = {
+        "Content-Type": "application/json"
+      }
+      break;
+  }
+
+  return new NextResponse(body, {headers});
 });
+
+async function inventoryCSV(inventory: any) : Promise<Buffer> {
+  const subSectorValues:SubSectorValue[] =
+    await inventory.getSubSectorValues();
+  const subCategoryValues:SubCategoryValue[] =
+    await inventory.getSubCategoryValues();
+  const headers = [
+    "Inventory Reference",
+    "Total Emissions",
+    "Activity Units",
+    "Activity Value",
+    "Emission Factor Value",
+    "Datasource ID"
+  ].join(",");
+  const subSectorLines = subSectorValues.map((value:SubSectorValue) => {
+    return [
+      value.subsectorId,
+      value.totalEmissions,
+      value.activityUnits,
+      value.activityValue,
+      value.emissionFactorValue,
+      value.datasourceId
+    ].join(",");
+  });
+  const subCategoryLines = subCategoryValues.map((value:SubCategoryValue) => {
+    return [
+      value.subcategoryId,
+      value.totalEmissions,
+      value.activityUnits,
+      value.activityValue,
+      value.emissionFactorValue,
+      value.datasourceId
+    ].join(",");
+  });
+  return Buffer.from([headers, ...subSectorLines, ...subCategoryLines].join("\n"));
+}
+
+async function inventoryXLS(inventory: any) : Promise<Buffer> {
+  return Buffer.from("Not implemented");
+}
 
 export const DELETE = apiHandler(async (_req: NextRequest, { params }) => {
   const city = await db.models.City.findOne({ where: { locode: params.city } });

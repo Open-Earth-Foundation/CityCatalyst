90a537420407eea004eceed404089cc2435cb4c7
Milan Gruner <milangruner@gmail.com>
2023-10-18 17:09:35 +0200

    feat(api): create migration for adding reference_number and changing SubSector/Scope relation
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/migrations/20231018131347-reference-number-scope-id.cjs
+++ app/migrations/20231018131347-reference-number-scope-id.cjs
@@ -5,7 +5,11 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     queryInterface.addColumn("Sector", "reference_number", Sequelize.STRING);
     queryInterface.addColumn("SubSector", "reference_number", Sequelize.STRING);
-    queryInterface.addColumn("SubCategory", "reference_number", Sequelize.STRING);
+    queryInterface.addColumn(
+      "SubCategory",
+      "reference_number",
+      Sequelize.STRING,
+    );
     queryInterface.addColumn("SubSector", "scope_id", Sequelize.UUID);
     queryInterface.addConstraint("SubSector", {
       type: "foreign key",
@@ -15,6 +19,7 @@ module.exports = {
       onDelete: "SET NULL",
       onUpdate: "SET NULL",
     });
+    queryInterface.dropTable("SubSectorScope");
   },
 
   async down(queryInterface) {
@@ -22,5 +27,22 @@ module.exports = {
     queryInterface.removeColumn("SubSector", "reference_number");
     queryInterface.removeColumn("SubCategory", "reference_number");
     queryInterface.removeColumn("SubSector", "scope_id");
+    queryInterface.sequelize.query(`
+      CREATE TABLE "SubSectorScope" (
+        "subsector_id" uuid,
+        "scope_id" uuid,
+        "created" timestamp,
+        "last_updated" timestamp,
+        PRIMARY KEY ("subsector_id", "scope_id"),
+        CONSTRAINT "FK_SubSectorScope.subsector_id"
+          FOREIGN KEY("subsector_id")
+          REFERENCES "SubSector" ("subsector_id")
+          ON DELETE CASCADE ON UPDATE CASCADE,
+        CONSTRAINT "FK_SubSectorScope.scope_id"
+          FOREIGN KEY("scope_id")
+          REFERENCES "Scope" ("scope_id")
+          ON DELETE CASCADE ON UPDATE CASCADE
+      );
+    `);
   },
 };

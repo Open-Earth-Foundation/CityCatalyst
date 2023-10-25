2db97d1691c4abe78942b812125366d8cee1420d
Milan Gruner <milangruner@gmail.com>
2023-10-25 15:16:13 +0200

    feat(test): add validation test for update user route
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/tests/api/user.test.ts
+++ app/tests/api/user.test.ts
@@ -15,6 +15,16 @@ const userUpdate = {
   defaultInventoryYear: 3000,
 };
 
+const invalidUserUpdate1 = {
+  defaultCityLocode: "XX_USER_TEST2",
+  defaultInventoryYear: -1,
+};
+
+const invalidUserUpdate2 = {
+  defaultCityLocode: "X",
+  defaultInventoryYear: 1,
+};
+
 describe("User API", () => {
   before(async () => {
     setupTests();
@@ -42,6 +52,24 @@ describe("User API", () => {
   });
 
   it("should not update a user with invalid data", async () => {
-    // TODO
+    const req = mockRequest(invalidUserUpdate1);
+    const res = await updateUser(req, { params: {} });
+    assert.equal(res.status, 400);
+    const user = await db.models.User.findOne({
+      where: { userId: userData.userId },
+    });
+    assert.ok(user != null);
+    assert.notEqual(user.defaultCityLocode, invalidUserUpdate1.defaultCityLocode);
+    assert.notEqual(user.defaultInventoryYear, invalidUserUpdate1.defaultInventoryYear);
+
+    const req2 = mockRequest(invalidUserUpdate2);
+    const res2 = await updateUser(req2, { params: {} });
+    assert.equal(res2.status, 400);
+    const user2 = await db.models.User.findOne({
+      where: { userId: userData.userId },
+    });
+    assert.ok(user2 != null);
+    assert.notEqual(user2.defaultCityLocode, invalidUserUpdate2.defaultCityLocode);
+    assert.notEqual(user2.defaultInventoryYear, invalidUserUpdate2.defaultInventoryYear);
   });
 });

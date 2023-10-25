3582ac853aeb2506597a1758c58e5f2e90a279ac
Milan Gruner <milangruner@gmail.com>
2023-10-25 14:44:47 +0200

    fix(test): mock getServerSession in User test
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/tests/api/user.test.ts
+++ app/tests/api/user.test.ts
@@ -1,11 +1,12 @@
 import { PATCH as updateUser } from "@/app/api/v0/user/route";
 import { db } from "@/models";
+import { UserAttributes } from "@/models/User";
 import env from "@next/env";
 import assert from "node:assert";
-import { after, before, describe, it } from "node:test";
-import { mockRequest } from "../helpers";
-import { UserAttributes } from "@/models/User";
 import { randomUUID } from "node:crypto";
+import { after, before, describe, it, mock } from "node:test";
+import { mockRequest } from "../helpers";
+import { AppSession, Auth } from "@/lib/auth";
 
 const userData: UserAttributes = {
   userId: randomUUID(),
@@ -24,6 +25,21 @@ describe("User API", () => {
     await db.initialize();
     await db.models.User.destroy({ where: { name: userData.name } });
     await db.models.User.create(userData);
+
+    mock.method(Auth, "getServerSession", (): AppSession => {
+      const expires = new Date();
+      expires.setDate(expires.getDate() + 1);
+      return {
+        user: {
+          id: randomUUID(),
+          name: "Test User",
+          email: "test@example.com",
+          image: null,
+          role: "user",
+        },
+        expires: expires.toISOString(),
+      };
+    });
   });
 
   after(async () => {
@@ -34,8 +50,6 @@ describe("User API", () => {
     const req = mockRequest(userUpdate);
     const res = await updateUser(req, { params: {} });
     assert.equal(res.status, 200);
-    const { data } = await res.json();
-    assert.equal(data.success, true);
     const user = await db.models.User.findOne({
       where: { userId: userData.userId },
     });

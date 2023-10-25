fb84e5bcee8cdc4b82d4d794a9d8f88ec6b35195
Milan Gruner <milangruner@gmail.com>
2023-10-25 14:44:14 +0200

    fix(api): errors with getServerSession/ Credentials in API tests
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/lib/auth.ts
+++ app/src/lib/auth.ts
@@ -1,12 +1,49 @@
-import { NextAuthOptions } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
-import { User } from "@/models/User";
 import { db } from "@/models";
+import { User } from "@/models/User";
+import bcrypt from "bcrypt";
+import { DefaultSession, NextAuthOptions, getServerSession } from "next-auth";
+import {
+  CredentialInput,
+  CredentialsConfig,
+} from "next-auth/providers/credentials";
 
 export enum Roles {
   User = "user",
   Admin = "admin",
+}
+
+// extracted from next-auth/providers/credentials
+// added here since the node test runner/ tsx wouldn't properly import ESM modules
+// error was: Credentials is not a function
+type UserCredentialsConfig<C extends Record<string, CredentialInput>> = Partial<
+  Omit<CredentialsConfig<C>, "options">
+> &
+  Pick<CredentialsConfig<C>, "authorize" | "credentials">;
+
+export default function Credentials<
+  C extends Record<string, CredentialInput> = Record<string, CredentialInput>,
+>(options: UserCredentialsConfig<C>): CredentialsConfig<C> {
+  return {
+    id: "credentials",
+    name: "Credentials",
+    type: "credentials",
+    credentials: {} as any,
+    authorize: () => null,
+    options,
+  };
+}
+
+export type AppSession = DefaultSession & {
+  user: {
+    id: string;
+    role: string;
+  };
+}
+
+export class Auth {
+  static async getServerSession(): Promise<AppSession | null> {
+    return await getServerSession(authOptions);
+  }
 }
 
 export const authOptions: NextAuthOptions = {
@@ -81,3 +118,4 @@ export const authOptions: NextAuthOptions = {
     },
   },
 };
+;

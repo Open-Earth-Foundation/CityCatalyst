3e0d1d82f14621e8cd41d9c49b1e52cdc569834f
cephaschapa <cephaschapa@gmail.com>
2023-10-31 13:50:47 +0200

    feat: added helper function to extact the package version

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
+import fs from 'fs';
+import path from 'path';
+
+const packagePath = path.join(process.cwd(), 'package.json');
+const packageJson = fs.readFileSync(packagePath);
+const packageInfo = JSON.parse(packageJson);
+
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["sequelize"],
   },
+  env: {
+    APP_VERSION: packageInfo.version
+  }
 };
 
 export default nextConfig;

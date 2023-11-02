d14261049601fda15a51afc3f37ed18b565a8561
Luke Gloege <lukegloege@gmail.com>
2023-11-02 13:15:18 -0400

    feat(alembic): change asset_emissions type from integer to bigint

---
+++ global-api/migrations/versions/66d4e6787ba6_asset_emissions_quantity_type_bigint.py
@@ -0,0 +1,26 @@
+"""asset_emissions_quantity_type_bigint
+
+Revision ID: 66d4e6787ba6
+Revises: 64eb87bae0a3
+Create Date: 2023-11-02 12:12:10.235702
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '66d4e6787ba6'
+down_revision: Union[str, None] = '64eb87bae0a3'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.alter_column('asset', 'emissions_quantity', type_=sa.BigInteger())
+
+
+def downgrade() -> None:
+    op.alter_column('asset', 'emissions_quantity', type_=sa.Integer())

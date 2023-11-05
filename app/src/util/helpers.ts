841b2a01ca0db8321e9724ec5e7fa9fad51c3384
cephaschapa <cephaschapa@gmail.com>
2023-11-05 20:00:34 +0200

    chore: prettier refactor

--- app/src/util/helpers.ts
+++ app/src/util/helpers.ts
@@ -7,7 +7,7 @@
 export function resolve(
   path: string | string[],
   obj: Record<string, any>,
-  separator: string = "."
+  separator: string = ".",
 ) {
   var properties = Array.isArray(path) ? path : path.split(separator);
   return properties.reduce((prev, curr) => prev?.[curr], obj);

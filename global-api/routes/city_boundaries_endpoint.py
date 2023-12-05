8e30c8a4d558c33fa4e369b32297017718e2c2be
Evan Prodromou <evan@openearth.org>
2023-12-05 12:47:32 -0500

    refactor: use a model for OSM data
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- global-api/routes/city_boundaries_endpoint.py
+++ global-api/routes/city_boundaries_endpoint.py
@@ -3,27 +3,33 @@ from fastapi import HTTPException, APIRouter
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import text
 import json
+from models.osm import Osm
 
 api_router = APIRouter(prefix="/api/v0")
 
 
 # Extract the polygon by locode
 def db_query(locode):
-    with SessionLocal() as session:
-        query = text("SELECT geometry FROM osm " + "WHERE locode = :locode ")
-        result = session.execute(
-            query,
-            {"locode": locode},
-        ).fetchone()
 
-    return result[0] if result else None
+    row = None
+
+    with SessionLocal() as session:
+        row = session.query(Osm).filter(Osm.locode == locode).limit(1).first()
+
+    return row
 
 
 @api_router.get("/cityboundary/city/{locode}")
 def get_city_boundary(locode: str):
-    city_geometry = db_query(locode)
+    city = db_query(locode)
 
-    if not city_geometry:
+    if not city:
         raise HTTPException(status_code=404, detail="City boundary not found")
 
-    return {"city_geometry": city_geometry}
+    return {
+        "city_geometry": city.geometry,
+        "bbox_north": city.bbox_north,
+        "bbox_south": city.bbox_south,
+        "bbox_east": city.bbox_east,
+        "bbox_west": city.bbox_west,
+    }

8c0b821ec29667d3619387563ab8f2527e70694c
Milan Gruner <milangruner@gmail.com>
2023-12-08 16:39:01 +0100

    fix(test): remove logging from database test
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/tests/api/datasource.test.ts
+++ app/tests/api/datasource.test.ts
@@ -113,7 +113,6 @@ describe("DataSource API", () => {
     });
     assert.equal(res.status, 200);
     const { data } = await res.json();
-    console.log("slag", data);
     assert.equal(data.length, 1);
     const { source } = data[0];
     assert.equal(source.name, "XX_DATASOURCE_TEST_0");

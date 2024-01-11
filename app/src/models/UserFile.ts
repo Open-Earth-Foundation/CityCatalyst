4eee565eb3470bd7d700e7a488e984a87000a769
cephaschapa <cephaschapa@gmail.com>
2024-01-11 14:28:43 +0200

    feat(api): create model class for userfile

---
+++ app/src/models/UserFile.ts
@@ -0,0 +1,102 @@
+import * as Sequelize from "sequelize";
+import { DataTypes, Model, Optional } from "sequelize";
+import { User, UserId } from "./User";
+
+export interface UserFileAttributes {
+  id: string;
+  user_id?: string;
+  file_reference?: string;
+  data?: Buffer;
+  status?: string;
+  gpc_ref_no?: string;
+  created?: Date;
+  lastUpdated?: Date;
+}
+
+export type UserFilePk = "id";
+export type UserFileId = UserFile[UserFilePk];
+export type UserFileOptionalAttributes =
+  | "user_id"
+  | "file_reference"
+  | "data"
+  | "status"
+  | "gpc_ref_no"
+  | "created"
+  | "lastUpdated";
+export type UserFileCreationAttributes = Optional<
+  UserFileAttributes,
+  UserFileOptionalAttributes
+>;
+
+export class UserFile
+  extends Model<UserFileAttributes, UserFileCreationAttributes>
+  implements UserFileAttributes
+{
+  id!: string;
+  user_id?: string | undefined;
+  file_reference?: string | undefined;
+  data: Buffer | undefined;
+  status: string | undefined;
+  gpc_ref_no?: string | undefined;
+  created?: Date | undefined;
+  lastUpdated?: Date | undefined;
+
+  //UserFile belongs to User via id
+  user!: User;
+  getUser!: Sequelize.BelongsToGetAssociationMixin<User>;
+  setUser!: Sequelize.BelongsToSetAssociationMixin<User, UserId>;
+  createUser!: Sequelize.BelongsToCreateAssociationMixin<User>;
+
+  static initModel(sequelize: Sequelize.Sequelize): typeof UserFile {
+    return UserFile.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          primaryKey: true,
+          field: "id",
+        },
+        user_id: {
+          type: DataTypes.UUID,
+          allowNull: true,
+          references: {
+            model: "User",
+            key: "user_id",
+          },
+          field: "user_id",
+        },
+        file_reference: {
+          type: DataTypes.STRING(255),
+          allowNull: true,
+        },
+        data: {
+          type: DataTypes.BLOB,
+          allowNull: true,
+        },
+        status: {
+          type: DataTypes.STRING(255),
+          allowNull: true,
+        },
+        gpc_ref_no: {
+          type: DataTypes.STRING(255),
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        tableName: "UserFile",
+        schema: "public",
+        timestamps: true,
+        createdAt: "created",
+        updatedAt: "last_updated",
+        indexes: [
+          {
+            name: "UserFile_pkey",
+            unique: true,
+            fields: [{ name: "id" }],
+          },
+        ],
+      },
+    );
+  }
+}

07b2c191cba6ee37b0dd327f9c4d990e6701f14f
Milan Gruner <milangruner@gmail.com>
2024-01-12 12:58:47 +0100

    Remove unused arguments and add folder name in gas-to-co2eq seeder
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/seeders/20240112083330-gas-to-co2eq.cjs
+++ app/seeders/20240112083330-gas-to-co2eq.cjs
@@ -4,14 +4,14 @@ const { parseFile, bulkUpsert } = require("./util/util.cjs");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    const gwps = await parseFile("gwp");
+  async up(queryInterface) {
+    const gwps = await parseFile("gwp", "gwp");
     await queryInterface.sequelize.transaction(async (transaction) => {
       await bulkUpsert(queryInterface, "GasToCO2Eq", gwps, "gas", transaction);
     });
   },
 
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.sequelize.transaction(async (transaction) => {
       await queryInterface.bulkDelete("GasToCO2Eq", null, { transaction });
     });

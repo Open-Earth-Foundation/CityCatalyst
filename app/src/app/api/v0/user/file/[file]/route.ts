f6aa52c00343038fb97f6396071e8c7b24fac369
cephaschapa <cephaschapa@gmail.com>
2024-01-16 14:09:25 +0200

    feat(api): added delete and get file by id routes

---
+++ app/src/app/api/v0/user/file/[file]/route.ts
@@ -0,0 +1,74 @@
+import { db } from "@/models";
+import { apiHandler } from "@/util/api";
+import createHttpError from "http-errors";
+import { Session } from "next-auth";
+import { NextResponse } from "next/server";
+
+export const GET = apiHandler(
+  async (
+    _req: Request,
+    context: { session?: Session; params: Record<string, string> },
+  ) => {
+    if (!context.session) {
+      throw new createHttpError.Unauthorized("Unauthorized");
+    }
+
+    const user = await db.models.User.findOne({
+      attributes: ["userId"],
+      where: {
+        userId: context.session.user.id,
+      },
+    });
+    if (!user) {
+      throw new createHttpError.NotFound("User not found");
+    }
+
+    const userFile = await db.models.UserFile.findOne({
+      where: {
+        id: context.params.file,
+      },
+    });
+
+    if (!userFile) {
+      throw new createHttpError.NotFound("User file not found");
+    }
+
+    return NextResponse.json({ data: userFile });
+  },
+);
+
+export const DELETE = apiHandler(
+  async (
+    _req: Request,
+    context: { session?: Session; params: Record<string, string> },
+  ) => {
+    if (!context.session) {
+      throw new createHttpError.Unauthorized("Unauthorized");
+    }
+
+    const user = await db.models.User.findOne({
+      attributes: ["userId"],
+      where: {
+        userId: context.session.user.id,
+      },
+    });
+
+    if (!user) {
+      throw new createHttpError.NotFound("File does not belong to this user");
+    }
+
+    const userFile = await db.models.UserFile.findOne({
+      where: {
+        id: context.params.file,
+      },
+    });
+
+    if (!userFile) {
+      throw new createHttpError.NotFound("User file not found");
+    }
+
+    await userFile.destroy();
+
+    return NextResponse.json({ data: userFile, deleted: true });
+  },
+);

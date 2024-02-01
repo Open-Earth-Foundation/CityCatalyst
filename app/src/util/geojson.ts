225f18485bc51989b67d2884669a14a517b5ad08
Milan Gruner <milangruner@gmail.com>
2024-02-01 14:23:20 +0100

    Implement getBoundsZoomLevel to calculate zoom from GeoJSON bounding box
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/util/geojson.ts
+++ app/src/util/geojson.ts
@@ -1,8 +1,10 @@
-// adapter from https://www.npmjs.com/package/geojson-bbox
+type BoundingBox = [number, number, number, number];
+
+// adapted from https://www.npmjs.com/package/geojson-bbox
 // MIT license, rewritten to support TypeScript
 export function geoJSONBoundingBox(
   geoJson: GeoJSON.GeoJSON,
-): number[] | undefined {
+): BoundingBox | undefined {
   if (!geoJson.hasOwnProperty("type")) {
     return;
   }
@@ -12,19 +14,19 @@ export function geoJSONBoundingBox(
     return;
   }
 
-  let bbox = [
+  let bbox: BoundingBox = [
     Number.POSITIVE_INFINITY,
     Number.POSITIVE_INFINITY,
     Number.NEGATIVE_INFINITY,
     Number.NEGATIVE_INFINITY,
   ];
-  return coords.reduce((prev: number[], coord: number[]) => {
+  return coords.reduce((prev: BoundingBox, coord: number[]) => {
     return [
       Math.min(coord[0], prev[0]),
       Math.min(coord[1], prev[1]),
       Math.max(coord[0], prev[2]),
       Math.max(coord[1], prev[3]),
-    ];
+    ] as BoundingBox;
   }, bbox);
 }
 
@@ -70,4 +72,38 @@ function getCoordinatesDump(
     }, [] as GeoJSON.Position[]);
   }
   return coords;
+}
+
+// Calculate correct zoom level for a given bounding box
+// Adapted from: https://stackoverflow.com/a/13274361
+const WORLD_DIM = { height: 256, width: 256 };
+const ZOOM_MAX = 21;
+
+export function getBoundsZoomLevel(
+  bounds: BoundingBox,
+  mapDimensions: { height: number; width: number },
+): number {
+  // GeoJSON has longitude first, then latitude
+  const [neLng, neLat, swLng, swLat] = bounds;
+
+  const latFraction =
+    (latitudeToRadians(neLat) - latitudeToRadians(swLat)) / Math.PI;
+
+  const lngDiff = neLng - swLng;
+  const lngFraction = (lngDiff < 0 ? lngDiff + 360 : lngDiff) / 360;
+
+  const latZoom = zoom(mapDimensions.height, WORLD_DIM.height, latFraction);
+  const lngZoom = zoom(mapDimensions.width, WORLD_DIM.width, lngFraction);
+
+  return Math.min(latZoom, lngZoom, ZOOM_MAX);
+}
+
+function latitudeToRadians(lat: number): number {
+  const sin = Math.sin((lat * Math.PI) / 180);
+  const radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
+  return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
+}
+
+function zoom(mapPx: number, worldPx: number, fraction: number): number {
+  return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
 }

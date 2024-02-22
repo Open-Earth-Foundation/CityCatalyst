03dd1b59ca12eaf73f76734e72bc64138dfd028d
Milan Gruner <milangruner@gmail.com>
2024-02-22 18:50:15 +0100

    Remove chat page used for testing
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/app/[lng]/chat/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ChatPopover from "@/components/ChatBot/chat-popover";
-
-export default function Chat({ params: { lng } }: { params: { lng: string } }) {
-  return <ChatPopover />;
-}

aa5bb155c27e655d567ca7cc3e847e0baaef28a3
Milan Gruner <milangruner@gmail.com>
2024-02-22 15:22:36 +0100

    Add default border to Textarea, prevent resize and increase border radius
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- app/src/lib/app-theme.ts
+++ app/src/lib/app-theme.ts
@@ -480,6 +480,10 @@ export const appTheme = extendTheme({
     Textarea: {
       variants: {
         brand: {
+          borderWidth: "1px",
+          borderRadius: "16px",
+          resize: "none",
+          borderColor: "border.neutral",
           _invalid: {
             background: "sentiment.negativeOverlay",
             borderWidth: "2px",

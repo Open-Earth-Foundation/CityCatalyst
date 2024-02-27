9239ba01da1a33d16dbb239bcbe71ccafb133242
mfonsecaOEF <maureen@openearth.org>
2024-02-27 08:24:35 -0600

    loading script

--- global-api/importer/mendoza_arg/load_mendoza_stationary_energy.sql
+++ global-api/importer/mendoza_arg/load_mendoza_stationary_energy.sql
@@ -1,22 +1,22 @@
-/* Create a staging table */
-
+-- Create a staging table
 CREATE TEMP TABLE IF NOT EXISTS mendoza_stationary_energy_staging (LIKE citywide_emissions INCLUDING ALL);
 
-/* Clear the staging table */
-
+-- Clear the staging table
 TRUNCATE mendoza_stationary_energy_staging;
 
-/* Load the staging table from the downloaded file */
+-- Load the staging table from the downloaded file
+COPY mendoza_stationary_energy_staging (year, activity_name, activity_value, activity_units, "GPC_refno", gas_name, emission_factor_value, emission_factor_units, emissions_value, emissions_units, city_name, source_name, temporal_granularity, locode, id)
 
-\copy mendoza_stationary_energy_staging (id,source_name,"GPC_refno",city_name,locode,temporal_granularity,year, activity_name, activity_value, activity_units, gas_name, emission_factor_value, emission_factor_units, emissions_value, emissions_units) FROM 'epa.csv' WITH CSV HEADER;
+-- FROM '/opt/airflow/data/stationary_energy_mendoza.csv';
+FROM '/Users/maureenfonseca/Desktop/airflow-docker/scripts/stationary_energy_mendoza.csv' DELIMITER ';' CSV HEADER;
 
-/* Update the main table with the staging table */
-
-INSERT INTO citywide_emissions (id,source_name,"GPC_refno",city_name,locode,temporal_granularity,year, activity_name, activity_value, activity_units, gas_name, emission_factor_value, emission_factor_units, emissions_value, emissions_units)
-    SELECT id,source_name,"GPC_refno",city_name,locode,temporal_granularity,year, activity_name, activity_value, activity_units, gas_name, emission_factor_value, emission_factor_units, emissions_value, emissions_units
+-- Update the main table with the staging table
+INSERT INTO citywide_emissions (year, activity_name, activity_value, activity_units, "GPC_refno", gas_name, emission_factor_value, emission_factor_units, emissions_value, emissions_units, city_name, source_name, temporal_granularity, locode, id)
+    SELECT year, activity_name, activity_value, activity_units, "GPC_refno", gas_name, emission_factor_value, emission_factor_units, emissions_value, emissions_units, city_name, source_name, temporal_granularity, locode, id
     FROM mendoza_stationary_energy_staging
     ON CONFLICT ON CONSTRAINT citywide_emissions_pkey
     DO UPDATE SET
+        id = excluded.id,
         source_name = excluded.source_name,
         "GPC_refno" = excluded."GPC_refno",
         city_name = excluded.city_name,
@@ -32,6 +32,6 @@ INSERT INTO citywide_emissions (id,source_name,"GPC_refno",city_name,locode,temp
         emissions_value = excluded.emissions_value, 
         emissions_units = excluded.emissions_units;
 
-/* Drop the staging table */
-
+-- Drop the staging table
 DROP TABLE mendoza_stationary_energy_staging;
+

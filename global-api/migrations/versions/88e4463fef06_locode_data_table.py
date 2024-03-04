b4b0276202003bcb9137b228817145e21d9d404b
mfonsecaOEF <maureen@openearth.org>
2024-03-04 14:22:02 -0600

    fix:no changes applied

--- global-api/migrations/versions/88e4463fef06_locode_data_table.py
+++ global-api/migrations/versions/88e4463fef06_locode_data_table.py
@@ -13,8 +13,8 @@ from sqlalchemy.sql import text
 
 
 # revision identifiers, used by Alembic.
-revision: str = '88e4463fef06'
-down_revision: Union[str, None] = 'cc8d00d4372e'
+revision: str = "88e4463fef06"
+down_revision: Union[str, None] = "cc8d00d4372e"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

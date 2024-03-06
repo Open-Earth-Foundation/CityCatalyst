de1ec8f9d492dc88e1b66292e09feb6a75589624
Milan Gruner <milangruner@gmail.com>
2024-03-06 12:56:13 +0100

    Add new headings and styles to SourceDrawer

--- app/src/app/[lng]/data/[step]/SourceDrawer.tsx
+++ app/src/app/[lng]/data/[step]/SourceDrawer.tsx
@@ -1,11 +1,14 @@
-import { ArrowBackIcon } from "@chakra-ui/icons";
+import type { SectorAttributes } from "@/models/Sector";
+import { ArrowBackIcon, InfoOutlineIcon } from "@chakra-ui/icons";
 import {
+  Box,
   Button,
   Drawer,
   DrawerBody,
   DrawerContent,
   DrawerOverlay,
   Flex,
+  HStack,
   Heading,
   Icon,
   Link,
@@ -14,16 +17,20 @@ import {
   TagLabel,
   TagLeftIcon,
   Text,
+  Tooltip,
   chakra,
 } from "@chakra-ui/react";
+import type { TFunction } from "i18next";
 import { RefObject } from "react";
 import {
   MdCalendarToday,
   MdHomeWork,
+  MdOutlineTimer,
   MdPlaylistAddCheck,
+  MdToday,
 } from "react-icons/md";
 import type { DataSourceWithRelations } from "./types";
-import { SectorAttributes } from "@/models/Sector";
+import { DataCheckIcon } from "@/components/icons";
 
 export function SourceDrawer({
   source,
@@ -42,8 +49,9 @@ export function SourceDrawer({
   onConnectClick: () => void;
   finalFocusRef?: RefObject<any>;
   isConnectLoading: boolean;
-  t: Function;
+  t: TFunction;
 }) {
+  console.dir(source);
   return (
     <Drawer
       isOpen={isOpen}
@@ -66,44 +74,126 @@ export function SourceDrawer({
             {t("go-back")}
           </Button>
           {source && (
-            <DrawerBody className="space-y-6">
+            <DrawerBody className="space-y-6" px={0}>
               <Icon as={MdHomeWork} boxSize={9} />
-              <Heading size="sm" color="content.link" textTransform="uppercase">
+              <Heading
+                size="sm"
+                color="content.link"
+                textTransform="uppercase"
+                letterSpacing="1.25px"
+                fontSize="14px"
+                lineHeight="16px"
+              >
                 {source.subcategoryId ? "Scope Data" : "Sub-sector Data"}
               </Heading>
               <Heading
                 size="sm"
                 color="content.tertiary"
                 textTransform="uppercase"
+                letterSpacing="1.25px"
+                fontSize="14px"
+                lineHeight="16px"
               >
                 {sector?.sectorName} /{" "}
                 {source.subSector?.subsectorName ||
-                  source.subCategory?.subSector?.subsectorName}
+                  source.subCategory?.subsector?.subsectorName}
               </Heading>
 
-              <Heading size="lg">{source.name}</Heading>
-              <Flex direction="row" my={4}>
-                <Tag mr={1}>
+              <Heading
+                fontSize="32px"
+                lineHeight="40px"
+                textTransform="capitalize"
+              >
+                {source.subCategory?.referenceNumber ||
+                  source.subSector?.referenceNumber}{" "}
+                {source.subCategory?.subcategoryName ||
+                  source.subSector?.subsectorName}
+              </Heading>
+
+              <Text
+                color="content.link"
+                fontSize={12}
+                fontFamily="heading"
+                textTransform="capitalize"
+                fontWeight="500"
+                lineHeight="16px"
+                letterSpacing="0.5px"
+              >
+                {t("by")}{" "}
+                <Link
+                  href={source.publisher?.url}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  textDecoration="underline"
+                >
+                  {source.publisher?.name}
+                </Link>
+              </Text>
+
+              <Heading fontSize="16px" lineHeight="24px">
+                {t("total-emissions-included")}{" "}
+                <Tooltip
+                  hasArrow
+                  label={t("total-emissions-tooltip")}
+                  placement="bottom-end"
+                >
+                  <InfoOutlineIcon color="interactive.control" boxSize={4} />
+                </Tooltip>
+              </Heading>
+
+              <HStack align="baseline">
+                <Heading fontSize="57px" lineHeight="64px">
+                  999.99
+                </Heading>
+                <Text
+                  color="content.tertiary"
+                  fontSize="22px"
+                  lineHeight="28px"
+                  fontFamily="heading"
+                  fontWeight={600}
+                >
+                  TCO2e
+                </Text>
+              </HStack>
+
+              <Flex
+                direction="row"
+                my={4}
+                className="gap-3 flex-wrap"
+                alignItems="start"
+              >
+                <Tag>
                   <TagLeftIcon
-                    as={MdPlaylistAddCheck}
+                    as={DataCheckIcon}
                     boxSize={6}
+                    mr={2}
                     color="content.tertiary"
                   />
-                  <TagLabel fontSize={14}>
+                  <TagLabel>
                     {t("data-quality")}: {t("quality-" + source.dataQuality)}
                   </TagLabel>
                 </Tag>
                 <Tag>
                   <TagLeftIcon
-                    as={MdCalendarToday}
+                    as={MdToday}
                     boxSize={6}
                     color="content.tertiary"
                   />
-                  <TagLabel fontSize={14}>
+                  <TagLabel>
                     {t("updated-every")}{" "}
                     {source.frequencyOfUpdate == "annual"
                       ? t("year")
-                      : t(source.frequencyOfUpdate)}
+                      : t(source.frequencyOfUpdate ?? "unknown")}
+                  </TagLabel>
+                </Tag>
+                <Tag>
+                  <TagLeftIcon
+                    as={MdOutlineTimer}
+                    boxSize={6}
+                    color="content.tertiary"
+                  />
+                  <TagLabel>
+                    {source.startYear} - {source.endYear}
                   </TagLabel>
                 </Tag>
               </Flex>

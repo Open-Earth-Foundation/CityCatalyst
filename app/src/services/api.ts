a7273fc9847876b5ee989ff3ac131fd625f1c9e2
Evan Prodromou <evan@openearth.org>
2024-03-20 08:21:04 -0400

    fix: add falllback for OpenClimate API endpoint
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -392,7 +392,7 @@ export const api = createApi({
 export const openclimateAPI = createApi({
   reducerPath: "openclimateapi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_OPENCLIMATE_API_URL,
+    baseUrl: process.env.NEXT_PUBLIC_OPENCLIMATE_API_URL || "https://openclimate.network",
   }),
   endpoints: (builder) => ({
     getOCCity: builder.query<any, string>({

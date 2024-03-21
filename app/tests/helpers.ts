2bdf7c3ec4d1b25f97a2c383ce97af7c7c1f5ad7
Milan Gruner <milangruner@gmail.com>
2024-03-21 13:47:22 +0100

    Fix __dirname definition to not depend on __filename
    Not available in Jest

--- app/tests/helpers.ts
+++ app/tests/helpers.ts
@@ -55,8 +55,7 @@ const createTestCsvFile = async (
   fileName: string,
   data: string,
 ): Promise<string> => {
-  const __dirname = __filename.replace(path.basename(__filename), "");
-  // const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const filePath = path.join(__dirname, fileName);
 
   await fs.promises.writeFile(filePath, data, "utf8");

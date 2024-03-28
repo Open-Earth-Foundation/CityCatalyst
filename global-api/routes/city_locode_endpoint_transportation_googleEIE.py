2b309fe73dd29b6c069242fc91eda9a546a1839e
mfonsecaOEF <maureen@openearth.org>
2024-03-28 13:28:36 -0600

    add:emission_values and units

--- global-api/routes/city_locode_endpoint_transportation_googleEIE.py
+++ global-api/routes/city_locode_endpoint_transportation_googleEIE.py
@@ -46,6 +46,8 @@ def get_emissions_by_locode_and_year(source_name: str, locode: str, year: str, G
             "activity_value": str(record["activity_value"]),
             "activity_units": str(record["activity_units"]),
             "gas_name": str(record["gas_name"]),
+            "emissions_value": str(record["activity_value"]),
+            "emissions_units": str(record["activity_units"])
             "emission_factor_value": str(record["emission_factor_value"]),
             "emission_factor_units": str(record["emission_factor_units"])
         })

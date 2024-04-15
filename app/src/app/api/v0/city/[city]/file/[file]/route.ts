26ab06e161d4f4b7bd20c787a27678d50f134a4c
Milan Gruner <milangruner@gmail.com>
2024-04-15 12:30:53 +0200

    feat(api): use new access control for find file and delete file routes as well

--- app/src/app/api/v0/city/[city]/file/[file]/route.ts
+++ app/src/app/api/v0/city/[city]/file/[file]/route.ts
@@ -1,46 +1,24 @@
-import { db } from "@/models";
+import UserService from "@/backend/UserService";
 import { apiHandler } from "@/util/api";
-import createHttpError from "http-errors";
 import { NextResponse } from "next/server";
 
-export const GET = apiHandler(async (_req: Request, context) => {
-  const userId = context.session?.user.id;
-  if (!context.session) {
-    throw new createHttpError.Unauthorized("Unauthorized");
-  }
-
-  const userFile = await db.models.UserFile.findOne({
-    where: {
-      id: context.params.file,
-      cityId: context.params.city,
-    },
-  });
-
-  if (!userFile) {
-    throw new createHttpError.NotFound("User file not found");
-  }
+export const GET = apiHandler(async (_req: Request, { session, params }) => {
+  const userFile = await UserService.findUserFile(
+    params.file,
+    params.city,
+    session,
+  );
 
   return NextResponse.json({ data: userFile });
 });
 
-export const DELETE = apiHandler(async (_req: Request, context) => {
-  const userId = context.session?.user.id;
-  if (!context.session) {
-    throw new createHttpError.Unauthorized("Unauthorized");
-  }
-
-  const userFile = await db.models.UserFile.findOne({
-    where: {
-      id: context.params.file,
-      cityId: context.params.city,
-    },
-  });
-
-  if (!userFile) {
-    throw new createHttpError.NotFound("User file not found");
-  }
+export const DELETE = apiHandler(async (_req: Request, { session, params }) => {
+  const userFile = await UserService.findUserFile(
+    params.file,
+    params.city,
+    session,
+  );
 
   await userFile.destroy();
-
   return NextResponse.json({ data: userFile, deleted: true });
 });

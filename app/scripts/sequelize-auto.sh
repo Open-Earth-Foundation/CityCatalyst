ec905e374f8a05d4e1692a3fa31d72a7a80cc84b
Milan Gruner <milangruner@gmail.com>
2024-05-08 16:45:54 +0200

    fix(script): forward arguments to sequelize-auto from script

--- app/scripts/sequelize-auto.sh
+++ app/scripts/sequelize-auto.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npx sequelize-auto -h localhost -d citycatalyst -u citycatalyst --dialect postgres -o src/models -l ts --caseProp c --additional scripts/model-params.json -T SequelizeMeta
-
+shift
+npx sequelize-auto -h localhost -d citycatalyst -u citycatalyst --dialect postgres -o src/models -l ts --caseProp c --additional scripts/model-params.json -T SequelizeMeta "$@"

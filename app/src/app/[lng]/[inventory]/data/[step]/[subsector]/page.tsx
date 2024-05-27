3ad8f92dcd4d8af58e12bf6f80dfe09c33bd62da
Cephas Chapa <cephaschapa@gmail.com>
2024-05-27 11:51:41 +0200

    feat(ui): added suggested activities component

--- app/src/app/[lng]/[inventory]/data/[step]/[subsector]/page.tsx
+++ app/src/app/[lng]/[inventory]/data/[step]/[subsector]/page.tsx
@@ -35,6 +35,7 @@ import { useTranslation } from "@/i18n/client";
 import { MdMoreVert, MdOutlineHomeWork } from "react-icons/md";
 import HeadingText from "@/components/heading-text";
 import MethodologyCard from "@/components/Cards/methodology-card";
+import SuggestedActivityCard from "@/components/Cards/suggested-activities-card";
 
 const METHODOLOGIES = [
   {
@@ -96,7 +97,7 @@ function SubSectorPage({
 
   const [isChecked, setIsChecked] = useState<boolean>(false);
 
-  const [hasActivityData, setHasActivityData] = useState<boolean>(true);
+  const [hasActivityData, setHasActivityData] = useState<boolean>(false);
 
   const handleSwitch = (e: any) => {
     setIsChecked(!isChecked);
@@ -289,92 +290,15 @@ function SubSectorPage({
                       >
                         Suggested activities to complete.
                       </Text>
-                      {BUILDINGS.map(({ id, name }) => (
-                        <Card
-                          display="flex"
-                          flexDirection="row"
-                          w="full"
-                          h="100px"
-                          p="16px"
-                          gap="16px"
-                          shadow="none"
-                          borderWidth="1px"
-                          borderColor="border.overlay"
-                          cursor="pointer"
-                          _hover={{ shadow: "md" }}
-                        >
-                          <Box display="flex" alignItems="center">
-                            <Checkbox
-                              borderRadius="full"
-                              __css={{
-                                "& .chakra-checkbox__control": {
-                                  bg: "white",
-                                  color: "#2351DC", // TODO: figure out how to include theme variable in css
-                                  borderRadius: "full",
-                                  borderColor: "#D7D8FA",
-                                  h: "24px",
-                                  w: "24px",
-                                  fontSize: "24px",
-                                },
-                              }}
-                              _checked={{
-                                "& .chakra-checkbox__control": {
-                                  bg: "white",
-                                  color: "content.link",
-                                  borderRadius: "full",
-                                  borderColor: "content.link",
-                                  h: "24px",
-                                  w: "24px",
-                                  fontSize: "24px",
-                                },
-                              }}
-                            />
-                          </Box>
-                          <Box
-                            display="flex"
-                            flexDirection="column"
-                            justifyContent="center"
-                            h="full"
-                            w="full"
-                          >
-                            <Text
-                              letterSpacing="wide"
-                              fontSize="body.md"
-                              fontWeight="normal"
-                              color="interactive.control"
-                            >
-                              Type of buildings
-                            </Text>
-                            <Text
-                              letterSpacing="wide"
-                              fontSize="16px"
-                              fontWeight="medium"
-                              fontFamily="heading"
-                            >
-                              {name}
-                            </Text>
-                          </Box>
-                          <Box
-                            display="flex"
-                            alignItems="center"
-                            w="full"
-                            justifyContent="end"
-                          >
-                            <Button
-                              title="Add Activity"
-                              leftIcon={<AddIcon h="16px" w="16px" />}
-                              h="48px"
-                              variant="ghost"
-                              aria-label="activity-button"
-                              fontSize="button.md"
-                              color="content.link"
-                              gap="8px"
-                            >
-                              add activity
-                            </Button>
-                          </Box>
-                        </Card>
-                      ))}
+                      {hasActivityData ? (
+                        <Box>Add Data</Box>
+                      ) : (
+                        <Box className="flex flex-col gap-4">
+                          {BUILDINGS.map(({ id, name }) => (
+                            <SuggestedActivityCard key={id} name={name} />
+                          ))}
+                        </Box>
+                      )}
                     </Box>
                   </Box>
                 ) : (

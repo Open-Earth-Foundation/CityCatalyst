ede29856ff23766181e2bed33496babc10152fcc
Milan Gruner <milangruner@gmail.com>
2024-05-31 18:12:20 +0200

    fix(api): use createHttpError instead of new Error for more descriptive error messages

--- app/src/backend/CDPService.ts
+++ app/src/backend/CDPService.ts
@@ -1,28 +1,39 @@
-export default class CDPService {
+import { logger } from "@/services/logger";
+import createHttpError from "http-errors";
 
+export default class CDPService {
   static get mode(): string {
     return process.env.CDP_MODE || "test";
   }
 
   static get key(): string {
+    if (!process.env.CDP_API_KEY) {
+      logger.error("Missing CDP_API_KEY env var!");
+    }
     return process.env.CDP_API_KEY || "test-key";
   }
 
   private static url(relative: string): string {
-    const host = (this.mode === "test") ? "api-pre" : "api-prd";
+    const host = this.mode === "test" ? "api-pre" : "api-prd";
     return `https://${host}.cdpgreenstar.net/${relative}`;
   }
 
-  public static async getCityID(city: string, country: string): Promise<string>  {
+  public static async getCityID(
+    city: string,
+    country: string,
+  ): Promise<string> {
     const url = this.url("response/partner/organizations");
     const response = await fetch(url, {
       headers: [
         ["user-agent", "CityCatalyst/0.10.0"],
-        ["subscription-key", this.key]
-      ]
+        ["subscription-key", this.key],
+      ],
     });
     if (!response.ok) {
-      throw new Error(`Failed to get city ID: ${response.statusText}`);
+      console.log(await response.text());
+      throw createHttpError.BadRequest(
+        `Failed to get city ID from CDP: ${response.statusText}`,
+      );
     }
     const data = await response.json();
     const organizations = data.organizations;
@@ -41,8 +52,8 @@ export default class CDPService {
       headers: [
         ["user-agent", "CityCatalyst/0.10.0"],
         ["organization-id", cityID],
-        ["subscription-key", this.key]
-      ]
+        ["subscription-key", this.key],
+      ],
     });
     if (!response.ok) {
       throw new Error(`Failed to get questions: ${response.statusText}`);
@@ -50,14 +61,18 @@ export default class CDPService {
     return await response.json();
   }
 
-  public static async submitResponse(cityID: string, question: string, response: string): Promise<boolean> {
+  public static async submitResponse(
+    cityID: string,
+    question: string,
+    response: string,
+  ): Promise<boolean> {
     const url = this.url(`response/response`);
     const body = {
       id: question,
-      "updateResponseInput": {
-        "content": response,
-        "status": "ANSWERED"
-      }
+      updateResponseInput: {
+        content: response,
+        status: "ANSWERED",
+      },
     };
     const res = await fetch(url, {
       method: "PUT",
@@ -65,9 +80,9 @@ export default class CDPService {
         ["user-agent", "CityCatalyst/0.10.0"],
         ["subscription-key", this.key],
         ["organization-id", cityID],
-        ["content-type", "application/json"]
+        ["content-type", "application/json"],
       ],
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
     });
     if (!res.ok) {
       throw new Error(`Failed to submit response: ${res.statusText}`);
@@ -76,3 +91,4 @@ export default class CDPService {
     return doc.responseVersion === 0;
   }
 }
+

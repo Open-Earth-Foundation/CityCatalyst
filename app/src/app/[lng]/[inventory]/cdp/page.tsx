9f4bcbdd541d7545c0e047a27dd4e4f84c6305d0
cephaschapa <cephaschapa@gmail.com>
2024-06-07 20:28:52 +0800

    feat: adds translation

--- app/src/app/[lng]/[inventory]/cdp/page.tsx
+++ app/src/app/[lng]/[inventory]/cdp/page.tsx
@@ -1,20 +1,45 @@
-import { NavigationBar } from '@/components/navigation-bar';
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Text } from '@chakra-ui/react';
-import React from 'react'
+"use client";
 
-const Page = () => {
-    return (
-        <Box>
-            <NavigationBar lng='' />
-            <Box className='h-[100vh] w-full flex justify-center items-center'>
-                <Card className='h-[300px] w-[300px] flex '>
-                    <CardHeader fontFamily='heading' fontWeight='bold' fontSize='headline.lg' className='flex items-center justify-center'><Text>Add Data</Text></CardHeader>
-                    <CardBody className='flex items-center justify-center'><Text>Sumbit data into CDP</Text></CardBody>
-                    <CardFooter p='0'><Button className='w-[100%]' h='50px'>submit data into cdp</Button></CardFooter>
-                </Card>
-            </Box>
-        </Box>
-    )
+import { NavigationBar } from "@/components/navigation-bar";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import { useTranslation } from "@/i18n/client";
+
+function Page({ params: { lng } }: { params: { lng: string } }) {
+  const { t } = useTranslation(lng, "cdp");
+  return (
+    <Box>
+      <NavigationBar lng="" />
+      <Box className="h-[100vh] w-full flex justify-center items-center">
+        <Card className="h-[300px] w-[300px] flex ">
+          <CardHeader
+            fontFamily="heading"
+            fontWeight="bold"
+            fontSize="headline.lg"
+            className="flex items-center justify-center"
+          >
+            <Text>{t("add-data")}</Text>
+          </CardHeader>
+          <CardBody className="flex items-center justify-center">
+            <Text>{t("submit-data-to-cdp")}</Text>
+          </CardBody>
+          <CardFooter p="0">
+            <Button className="w-[100%]" h="50px">
+              {t("submit-data-to-cdp")}
+            </Button>
+          </CardFooter>
+        </Card>
+      </Box>
+    </Box>
+  );
 }
 
 export default Page;

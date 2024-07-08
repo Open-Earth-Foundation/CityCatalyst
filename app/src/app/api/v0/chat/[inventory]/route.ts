55ea651658c3ea117cd20f9d4cb7a76b929b9b4c
Mirco Rudolph <mirco.rudolph@gmail.com>
2024-07-08 11:44:35 +0200

    Used utils function for getting population data, changed snake_case to camelCase for function names

--- app/src/app/api/v0/chat/[inventory]/route.ts
+++ app/src/app/api/v0/chat/[inventory]/route.ts
@@ -9,6 +9,11 @@ import UserService from "@/backend/UserService";
 import { db } from "@/models";
 import { Inventory } from "@/models/Inventory";
 import { Population } from "@/models/Population";
+import {
+  PopulationEntry,
+  findClosestYear,
+  findClosestYearToInventory,
+} from "@/util/helpers";
 
 let Hf: HfInference, openai: OpenAI;
 
@@ -79,103 +84,29 @@ async function handleOpenAIChat(
 /**
  * Creates a system message with context included
  */
-function create_prompt_template(inventory: Inventory): string {
-  /**
-   * Helper function to extract the latest population value
-   */
-  function find_last_city_population(
-    populations: Population[],
-  ): [number | undefined, number | undefined] {
-    let result: [number | undefined, number | undefined] = [
-      undefined,
-      undefined,
-    ];
-
-    // Loop through the array from back to front
-    for (let i = populations.length - 1; i >= 0; i--) {
-      // Check if the population field is populated
-      if (populations[i].dataValues.population) {
-        // Add the year and population to the results array
-        result = [
-          populations[i].dataValues.year,
-          populations[i].dataValues.population,
-        ];
-        break;
-      }
-    }
-    // Return either undefined or the latest known population
-    return result;
-  }
-
-  /**
-   * Helper function to extract the latest region population value
-   */
-  function find_last_region_population(
-    populations: Population[],
-  ): [number | undefined, number | undefined] {
-    let result: [number | undefined, number | undefined] = [
-      undefined,
-      undefined,
-    ];
-
-    // Loop through the array from back to front
-    for (let i = populations.length - 1; i >= 0; i--) {
-      // Check if the population field is populated
-      if (populations[i].dataValues.regionPopulation) {
-        // Add the year and population to the results array
-        result = [
-          populations[i].dataValues.year,
-          populations[i].dataValues.regionPopulation,
-        ];
-        break;
-      }
-    }
-    // Return either undefined or the latest known population
-    return result;
-  }
-
-  /**
-   * Helper function to extract the latest country population value
-   */
-  function find_last_country_population(
-    populations: Population[],
-  ): [number | undefined, number | undefined] {
-    let result: [number | undefined, number | undefined] = [
-      undefined,
-      undefined,
-    ];
-
-    // Loop through the array from back to front
-    for (let i = populations.length - 1; i >= 0; i--) {
-      // Check if the population field is populated
-      if (populations[i].dataValues.countryPopulation) {
-        // Add the year and population to the results array
-        result = [
-          populations[i].dataValues.year,
-          populations[i].dataValues.countryPopulation,
-        ];
-        break;
-      }
-    }
-    // Return either undefined or the latest known population
-    return result;
-  }
-
+function createPromptTemplate(inventory: Inventory): string {
   const inventoryYear = inventory.dataValues.year;
+
+  let population = null;
+  let cityPopulation = undefined;
+  let cityPopulationYear = undefined;
+
+  if (inventoryYear) {
+    // The following function returns only the city population closest to the inventory data.
+    // Currrently there is no function that returns all 3 population values closest to the inventory data.
+    population = findClosestYearToInventory(
+      inventory.city.populations as PopulationEntry[],
+      inventoryYear,
+    );
+    cityPopulation = population?.population;
+    cityPopulationYear = population?.year;
+  }
+
   const cityName = inventory.city.dataValues.name;
   const regionName = inventory.city.dataValues.region;
   const countryName = inventory.city.dataValues.country;
   const countryLocode = inventory.city.dataValues.countryLocode;
   const cityArea = inventory.city.dataValues.area;
-
-  const [cityPopulationYear, cityPopulation] = find_last_city_population(
-    inventory.city.populations,
-  );
-  const [regionPopulationYear, regionPopulation] = find_last_region_population(
-    inventory.city.populations,
-  );
-  const [countryPopulationYear, countryPopulation] =
-    find_last_country_population(inventory.city.populations);
 
   return `You are a climate assistant for creating 
 'Global Protocol for Community-Scale (GPC) Greenhouse Gas (GHG) Inventories' using CityCatalyst, 
@@ -190,9 +121,7 @@ CONTEXT
 + Name of the corresponding region: ${regionName},
 + Name of the corresponding country: ${countryName},
 + UN/LOCODE of the corresponding country: ${countryLocode},
-+ Population of the city for the year ${cityPopulationYear} (latest know value in the database): ${cityPopulation},
-+ Population of the region for the year ${regionPopulationYear} (latest know value in the database): ${regionPopulation},
-+ Population of the country for the year ${countryPopulationYear} (latest know value in the database): ${countryPopulation},
++ Population of the city for the year ${cityPopulationYear} (closest known value to the inventory year): ${cityPopulation},
 + Area of the city in km\u00B2: ${cityArea},
 + Year for which the the inventory is being created: ${inventoryYear},
 + Current inventory values: .`;
@@ -236,7 +165,7 @@ export const POST = apiHandler(async (req, { params, session }) => {
     ],
   );
 
-  const prompt = create_prompt_template(inventory);
+  const prompt = createPromptTemplate(inventory);
 
   const { messages } = chatRequest.parse(await req.json());
   if (!messages[0].content.startsWith("You are a")) {

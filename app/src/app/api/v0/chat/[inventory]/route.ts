693487544779730134783e7be4306770fcd66f3a
Mirco Rudolph <mirco.rudolph@gmail.com>
2024-07-08 10:03:04 +0200

    removed unused function

--- app/src/app/api/v0/chat/[inventory]/route.ts
+++ app/src/app/api/v0/chat/[inventory]/route.ts
@@ -41,27 +41,6 @@ const chatRequest = z.object({
 });
 type Messages = z.infer<typeof messagesSchema>;
 
-// Function below is not being referenced/implemented
-
-// async function streamToString(stream: any) {
-//   const reader = stream.getReader();
-//   const textDecoder = new TextDecoder();
-//   let result = "";
-
-//   async function read() {
-//     const { done, value } = await reader.read();
-
-//     if (done) {
-//       return result;
-//     }
-
-//     result += textDecoder.decode(value, { stream: true });
-//     return read();
-//   }
-
-//   return read();
-// }
-
 async function handleHuggingFaceChat(
   messages: Messages,
 ): Promise<StreamingTextResponse> {

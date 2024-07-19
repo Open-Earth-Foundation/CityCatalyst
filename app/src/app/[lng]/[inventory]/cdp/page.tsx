e2edf847cb753d4afc981afc117c02968d6b9a6c
Milan Gruner <milangruner@gmail.com>
2024-07-19 14:03:12 +0200

    feat(ui): add CDP status message to CDP page and improve layout

--- app/src/app/[lng]/[inventory]/cdp/page.tsx
+++ app/src/app/[lng]/[inventory]/cdp/page.tsx
@@ -9,8 +9,9 @@ import {
   CardFooter,
   CardHeader,
   Text,
+  VStack,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "@/i18n/client";
 import { api } from "@/services/api";
 
@@ -20,17 +21,29 @@ function Page({
   params: { lng: string; inventory: string };
 }) {
   const { t } = useTranslation(lng, "cdp");
+  const [statusMessage, setStatusMessage] = useState(t("submit-data-to-cdp"));
+  const [wasSuccessful, setWasSuccessful] = useState(true);
+
   const [connectToCDP] = api.useConnectToCDPMutation();
   const handleConnectToCDP = async () => {
-    await connectToCDP({ inventoryId: inventory }).then((res) =>
-      console.log(res),
-    );
+    const res: { data?: any; error?: any } = await connectToCDP({
+      inventoryId: inventory,
+    });
+    console.log("CDP response", res, res.error);
+    if (res.error) {
+      const message = res.error.data.error.message;
+      setStatusMessage(message);
+      setWasSuccessful(false);
+    } else {
+      setStatusMessage("Success");
+      setWasSuccessful(true);
+    }
   };
   return (
     <Box>
       <NavigationBar lng="" />
       <Box className="h-[100vh] w-full flex justify-center items-center">
-        <Card className="h-[300px] w-[300px] flex ">
+        <Card className="min-h-[300px] min-w-[300px] flex">
           <CardHeader
             fontFamily="heading"
             fontWeight="bold"
@@ -40,10 +53,14 @@ function Page({
             <Text>{t("add-data")}</Text>
           </CardHeader>
           <CardBody className="flex items-center justify-center">
-            <Text>{t("submit-data-to-cdp")}</Text>
+            <VStack>
+              <Text color={wasSuccessful ? "green" : "red"} align="center">
+                {statusMessage}
+              </Text>
+            </VStack>
           </CardBody>
-          <CardFooter p="0">
-            <Button onClick={handleConnectToCDP} className="w-[100%]" h="50px">
+          <CardFooter px="0">
+            <Button onClick={handleConnectToCDP} className="w-[100%]" size="lg">
               {t("submit-data-to-cdp")}
             </Button>
           </CardFooter>

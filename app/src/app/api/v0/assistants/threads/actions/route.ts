11994e93a4aa5f302cf4157e9df17e164bcbc9a7
Mirco Rudolph <mirco.rudolph@gmail.com>
2024-08-02 16:03:05 +0200

    removed debugging code

--- app/src/app/api/v0/assistants/threads/actions/route.ts
+++ app/src/app/api/v0/assistants/threads/actions/route.ts
@@ -5,16 +5,10 @@ import { NextResponse } from "next/server";
 // Send a new message to a thread
 export const POST = apiHandler(async (req) => {
   const { threadId, runId, toolCallOutputs } = await req.json();
-  //const { toolCallOutputs, runId } = await req.json();
-  console.log("server api call");
-  console.log(threadId);
-  console.log(runId);
-  console.log(toolCallOutputs);
 
   const stream = openai.beta.threads.runs.submitToolOutputsStream(
     threadId,
     runId,
-    // { tool_outputs: [{ output: result, tool_call_id: toolCallId }] },
     { tool_outputs: toolCallOutputs },
   );
 

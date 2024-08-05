f15fc471d17cc5bcba8ed81ed79528fe4fcca5b9
Mirco Rudolph <mirco.rudolph@gmail.com>
2024-08-05 16:12:29 +0200

    removed debugging code from file

--- app/src/app/api/v0/assistants/files/[fileId]/route.ts
+++ app/src/app/api/v0/assistants/files/[fileId]/route.ts
@@ -3,12 +3,7 @@ import { openai } from "@/util/openai";
 import { NextResponse } from "next/server";
 
 export const GET = apiHandler(async (req, { params }) => {
-  //const { fileId } = await req.json();
-  console.log("get file!");
-  console.log(params.fileId);
-
   const file = await openai.files.retrieve(params.fileId);
-  console.log("file name: ", file.filename);
 
   return NextResponse.json({ file: file });
 });

e8819fca335523b30e08bca48edffca6f2ce928d
isaacbello <isaacbello3200@gmail.com>
2024-08-21 15:34:39 +0100

    e2e: make tests work in headless mode

--- app/e2e/manual-input.spec.ts
+++ app/e2e/manual-input.spec.ts
@@ -111,7 +111,7 @@ const sectorData = [
   },
 ];
 
-test.describe("Manual Input", () => {
+test.describe.serial("Manual Input", () => {
   let page: Page;
   let id: string;
 
@@ -128,9 +128,8 @@ test.describe("Manual Input", () => {
   });
 
   test("should render sector list page", async () => {
-    const navButton = `[data-testid="${testIds.addDataToInventoryNavButton}"]`;
-    await page.waitForSelector(navButton);
-    await page.click(navButton);
+    const navButton = page.getByTestId(testIds.addDataToInventoryNavButton);
+    await navButton.click();
     await page.waitForURL(regexForPath("/data/"));
     await expect(page).toHaveURL(regexForPath("/data/"));
     const pageHeader = page.getByTestId(testIds.addDataStepHeading);
@@ -154,19 +153,16 @@ test.describe("Manual Input", () => {
   });
 
   sectorData.forEach((sector) => {
-    test.describe(() => {
+    test.describe.serial(() => {
       test(`should navigate to ${sector.sectorName} sector page`, async () => {
         await page.goto(`/en/${id}/data/`);
         await page.waitForURL(regexForPath("/data/"));
         await expect(page).toHaveURL(regexForPath("/data/"));
-
         // wait for sector card to load
-        const sectorCard = await page.waitForSelector(
-          `[data-testid="${sector.testId}"]`,
-        );
+        const sectorCard = await page.getByTestId(sector.testId);
         expect(sectorCard).toBeTruthy();
-        const sectorCardBtn = await sectorCard?.$(
-          `[data-testid="${testIds.sectorCardButton}"]`,
+        const sectorCardBtn = await sectorCard?.getByTestId(
+          testIds.sectorCardButton,
         );
         await sectorCardBtn?.click();
         await page.waitForURL(regexForPath(sector.url1));
@@ -176,15 +172,11 @@ test.describe("Manual Input", () => {
         // wait for 10 seconds
         await page.waitForTimeout(3000);
 
-        const subsectorCrds = await page.$$(
-          `[data-testid=${testIds.subsectorCard}]`,
-        );
-        expect(subsectorCrds.length).toBeGreaterThan(0);
+        const subsectorCrds = await page.getByTestId(testIds.subsectorCard);
+        expect(await subsectorCrds.count()).toBeGreaterThan(0);
 
         // await page response
-
-        await page.waitForSelector(`[data-testid="${testIds.subsectorCard}"]`);
-        const targetSubSector = subsectorCrds[0];
+        const targetSubSector = subsectorCrds.first();
         await targetSubSector.click();
         await expect(page).toHaveURL(
           /\/data\/\d+\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/$/,
@@ -193,56 +185,58 @@ test.describe("Manual Input", () => {
 
       test(`should list methodologies in ${sector.sectorName}`, async () => {
         // check on a list of methodologies
-        const methodologyCards = await page.$$(
-          `[data-testid=${testIds.methodologyCard}]`,
+        const methodologyCards = await page.getByTestId(
+          testIds.methodologyCard,
         );
-        expect(methodologyCards.length).toBeGreaterThan(0);
+        expect(await methodologyCards.count()).toBeGreaterThan(0);
       });
 
       test(`test direct measure methodology in scope 1 with incomplete  & complete values in in ${sector.sectorName}`, async () => {
         test.skip(sector.sectorName === "Waste");
         // look for a direct measure
         // select all the methodology card headers and check if any of them is direct measure
-        const directMeasureCardHeader = await page.$(
-          `[data-testid=${testIds.methodologyCardHeader}]` &&
-            "text=Direct Measure",
-        );
+        const directMeasureCardHeader = await page
+          .getByTestId(testIds.methodologyCardHeader)
+          .filter({
+            hasText: "Direct Measure",
+          })
+          .first();
 
-        expect(directMeasureCardHeader).toBeTruthy();
+        expect(directMeasureCardHeader).toBeVisible();
         // click on the direct measure card
         await directMeasureCardHeader?.click();
 
-        const addEmissionButton = await page.$(
-          `[data-testid=${testIds.addEmissionButton}]`,
+        const addEmissionButton = await page.getByTestId(
+          testIds.addEmissionButton,
         );
         expect(addEmissionButton).toBeTruthy();
         await addEmissionButton?.click();
 
         // wait for the modal to open;
-        const addEmissionModal = await page.waitForSelector(
-          `[data-testid=${testIds.addEmissionModal}]`,
+        const addEmissionModal = await page.getByTestId(
+          testIds.addEmissionModal,
         );
 
         // fill in the select fields
-        const selectElements = await addEmissionModal.$$("select");
-
-        for (const selectElement of selectElements) {
-          await selectElement.selectOption({ index: 1 });
-          await page.waitForTimeout(1000); // Selects the first option (index 0)
+        const selectElements = await page.locator("select");
+        for (let i = 0; i < (await selectElements.count()); i++) {
+          const dropdown = selectElements.nth(i);
+          await dropdown.selectOption({ index: 1 });
         }
 
         // fill in the  numeric input fields
-        const numericInputs = await addEmissionModal.$$(
+        const numericInputs = await addEmissionModal.locator(
           'input[inputmode="decimal"][pattern="[0-9]*(\\.[0-9]+)?"]',
         );
 
-        for (const input of numericInputs) {
+        for (let i = 0; i < (await numericInputs.count()); i++) {
+          const input = numericInputs.nth(i);
           await input.fill("122"); // Example: filling with the value '1.0'
         }
 
         // try to submit the form
-        const submitButton = await addEmissionModal.$(
-          `[data-testid=${testIds.addEmissionModalSubmitButton}]`,
+        const submitButton = await addEmissionModal.getByTestId(
+          testIds.addEmissionModalSubmitButton,
         );
 
         await submitButton?.click();
@@ -254,10 +248,11 @@ test.describe("Manual Input", () => {
 
         expect(element).toBeTruthy();
 
-        // fill in the text field
-        const textInputs = await addEmissionModal.$$("textarea");
-        for (const textInput of textInputs) {
-          await textInput.fill("test values");
+        // fill in the text fields
+        const textInputs = await addEmissionModal.locator("textarea");
+        for (let i = 0; i < (await textInputs.count()); i++) {
+          const input = textInputs.nth(i);
+          await input.fill("122");
         }
 
         await submitButton?.click();
@@ -272,16 +267,16 @@ test.describe("Manual Input", () => {
         test.skip(sector.sectorName === "Waste");
         // wait for the page to load
         // wait for the table to load
-        const table = await page.waitForSelector("table");
+        const table = await page.locator("table");
 
         // Ensure the table exists
         expect(table).not.toBeNull();
 
         // Get all the rows in the table (excluding the header row if there is one)
-        const rows = await table?.$$("tbody tr");
+        const rows = await table?.locator("tbody tr");
 
         // Ensure the table has at least one row
-        expect(rows?.length).toBeGreaterThan(0);
+        expect(await rows.count()).toBeGreaterThan(0);
 
         const cellWithValue = await page
           ?.getByRole("cell", { name: "tCO2" })

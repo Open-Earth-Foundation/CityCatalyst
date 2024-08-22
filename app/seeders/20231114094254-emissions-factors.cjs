488bceba20132845c254adc852fda5229fc409d0
thehighestprimenumber <thehighestprimenumber@gmail.com>
2024-08-22 15:46:19 -0300

    add Methodology upsert

--- app/seeders/20231114094254-emissions-factors.cjs
+++ app/seeders/20231114094254-emissions-factors.cjs
@@ -58,7 +58,7 @@ module.exports = {
           "DataSourceEmissionsFactor",
           folder,
         );
-
+        const methodologies = await parseFile("Methodology", folder);
         const emissionsFactorsRaw = await parseFile("EmissionsFactor", folder);
         const emissionsFactors = emissionsFactorsRaw.map((ef) => {
           const metadata = (ef.metadata ? ef.metadata : "")
@@ -92,6 +92,14 @@ module.exports = {
           transaction,
         );
         console.info("Finished adding data sources");
+        await bulkUpsert(
+          queryInterface,
+          "Methodology",
+          methodologies,
+          "methodology_id",
+          transaction,
+        );
+        console.info("Finished adding methodologies");
         await bulkUpsert(
           queryInterface,
           "EmissionsFactor",

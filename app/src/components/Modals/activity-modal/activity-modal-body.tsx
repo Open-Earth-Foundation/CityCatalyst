32d5f72cc4c8e23097720d2625fed107b15c6893
isaacbello <isaacbello3200@gmail.com>
2024-09-04 12:33:03 +0100

    code clean up

--- app/src/components/Modals/activity-modal/activity-modal-body.tsx
+++ app/src/components/Modals/activity-modal/activity-modal-body.tsx
@@ -723,7 +723,7 @@ const ActivityModalBody = ({
               borderColor: "content.link",
             }}
             placeholder={t("source-reference-placeholder")}
-            {...register(`activity.sourceReference` as any, {
+            {...register(`activity.sourceReference`, {
               required: t("source-reference-required"),
             })}
           />

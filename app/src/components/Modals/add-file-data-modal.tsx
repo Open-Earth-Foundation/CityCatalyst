14ea026e5e93650d09668283f32fd0251464257b
Milan Gruner <milangruner@gmail.com>
2024-09-10 15:55:12 +0200

    hotfix(ui): don't query inventory GET route in AddFileDataModal

--- app/src/components/Modals/add-file-data-modal.tsx
+++ app/src/components/Modals/add-file-data-modal.tsx
@@ -103,9 +103,11 @@ const AddFileDataModal: FC<AddFileDataModalProps> = ({
     });
   }
 
-  const { data: inventoryData } = api.useGetInventoryQuery(inventory!, {
+  // TODO this is fixed on dev so use that version once stable
+  /* const { data: inventoryData } = api.useGetInventoryQuery(inventory!, {
     skip: !userInfo,
-  });
+  }); */
+  const inventoryData: any = null;
 
   const [addUserFile, { isLoading }] = api.useAddUserFileMutation();
   const DEFAULT_STATUS = "pending";

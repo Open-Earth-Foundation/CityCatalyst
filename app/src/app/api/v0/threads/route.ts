9c3e1ac9e09e308f79e7f02bfc6da12f801f2738
Mirco Rudolph <mirco.rudolph@gmail.com>
2024-10-04 16:18:46 -0600

    feat: improved error handling, removed debugging code

--- app/src/app/api/v0/threads/route.ts
+++ app/src/app/api/v0/threads/route.ts
@@ -1,16 +1,28 @@
 import { db } from "@/models";
 import { apiHandler } from "@/util/api";
-// import createHttpError from "http-errors";
+import createHttpError from "http-errors";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = apiHandler(async (req) => {
-  const input: {
-    threadId: string;
-  } = await req.json();
+  try {
+    const input: {
+      threadId: string;
+    } = await req.json();
 
-  const response = await db.models.AssistantThread.create({
-    assistantThreadId: input.threadId,
-  });
+    if (!input.threadId || typeof input.threadId !== "string") {
+      return NextResponse.json(
+        { error: "Invalid threadId provided" },
+        { status: 400 },
+      );
+    }
 
-  return NextResponse.json({ response });
+    const response = await db.models.AssistantThread.create({
+      assistantThreadId: input.threadId,
+    });
+
+    return NextResponse.json({ data: response });
+  } catch (error) {
+    console.error("Error writing AssistantThread to the DB:", error);
+    throw createHttpError(500, "Server error");
+  }
 });

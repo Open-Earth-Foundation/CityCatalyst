7a0be5105735dd28e83d0ae5c5b369562d1e28f3
Cephas Chapa <cephaschapa@gmail.com>
2024-10-09 16:18:36 +0200

    fix: removes cache control

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -14,19 +14,6 @@ const nextConfig = {
   env: {
     APP_VERSION: packageInfo.version,
   },
-   async headers() {
-    return [
-      {
-        source: "/", 
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;

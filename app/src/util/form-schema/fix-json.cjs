76cd643dd750af60e3b273daf45c17cc78561869
Milan Gruner <milangruner@gmail.com>
2024-10-11 17:23:58 +0200

    feat: add script fix-json.cjs used to do automatic conversions on the schema

---
+++ app/src/util/form-schema/fix-json.cjs
@@ -0,0 +1,20 @@
+const hierarchy = require("./manual-input-hierarchy.json");
+
+// call this script using:
+// node fix-json.cjs > manual-input-hierarchy-fixed.json
+
+Object.entries(hierarchy).forEach(([key, value]) => {
+  value.methodologies = value.methodologies.map((methodology) => {
+    methodology.activities = methodology.activities?.map((activity) => {
+      activity["extra-fields"] = activity["extra-fields"]?.map((field) => {
+        field["title"] = field["id"];
+        field["id"] = field["id"].split("-").slice(2).join("-");
+        return field;
+      });
+      return activity;
+    });
+    return methodology;
+  });
+});
+
+console.log(JSON.stringify(hierarchy, null, 2));

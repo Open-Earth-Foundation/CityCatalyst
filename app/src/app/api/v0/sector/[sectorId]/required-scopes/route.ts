cb2a36ba015bc852d4ae660dd81ee5b17546d9ed
thehighestprimenumber <thehighestprimenumber@gmail.com>
2024-10-22 13:12:58 -0300

    bug: [ON-2663] go back to hardcoded scopes
    because the db returns more scopes than those required by BASIC. We'll have to re-work the db when we start supporting more standards

--- app/src/app/api/v0/sector/[sectorId]/required-scopes/route.ts
+++ app/src/app/api/v0/sector/[sectorId]/required-scopes/route.ts
@@ -3,7 +3,7 @@ import { apiHandler } from "@/util/api";
 import createHttpError from "http-errors";
 import { NextRequest, NextResponse } from "next/server";
 import GPCService from "@/backend/GPCService";
-
+/** this endpoint needs to be reworked because it's returning more scopes than it should. See [ON-2663]**/
 export const GET = apiHandler(async (_req: NextRequest, { params }) => {
   const sector = await db.models.Sector.findByPk(params.sectorId);
   if (!sector) {

b3a4875831f61aec8a0436456558d37cb4e6fe46
isaacbello <isaacbello3200@gmail.com>
2024-10-25 13:52:30 +0100

    fix: fix

--- app/src/util/big_int.ts
+++ app/src/util/big_int.ts
@@ -1,14 +1,7 @@
 import Decimal from "decimal.js";
 
 export function decimalToBigInt(decimal: Decimal): bigint {
-  // Convert the Decimal to a string
-  const decimalString = decimal.toString();
-
-  // Remove any decimal point and trailing zeros
-  const integerString = decimalString.replace(/\.0+$|(\.\d*[1-9])0+$/, "$1");
-
-  // Convert to BigInt
-  return BigInt(integerString);
+  return BigInt(decimal.trunc().toString());
 }
 
 export function bigIntToDecimal(bigIntValue: bigint): Decimal {

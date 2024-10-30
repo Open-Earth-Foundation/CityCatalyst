2ab534d1b3a51840bd5b458767ee7878588fefb0
thehighestprimenumber <thehighestprimenumber@gmail.com>
2024-10-30 09:27:16 -0300

    feat: [ON-2687] 404 for public routes

--- app/src/app/api/v0/user/route.ts
+++ app/src/app/api/v0/user/route.ts
@@ -3,6 +3,7 @@ import { apiHandler } from "@/util/api";
 import createHttpError from "http-errors";
 import { NextResponse } from "next/server";
 import { z } from "zod";
+import UserService from "@/backend/UserService";
 
 export const GET = apiHandler(async (_req: Request, context) => {
   if (!context.session) {
@@ -32,14 +33,25 @@ export const PATCH = apiHandler(async (req: Request, context) => {
     throw new createHttpError.Unauthorized("Unauthorized");
   }
 
-  const user = await db.models.User.update(body, {
-    where: {
-      userId: context.session.user.id,
-    },
-  });
-  if (!user) {
-    throw new createHttpError.NotFound("User not found");
+  let inventory;
+  try {
+    inventory = await UserService.findUserInventory(
+      body.defaultInventoryId,
+      context.session,
+    );
+  } catch (error) {
+    // we will arrive here if a logged in user attempts to access a public view of an inventory they are not part of.
+    // It's not an error, but we don't want to update the user's default inventory in this case.
   }
-
+  if (inventory) {
+    const user = await db.models.User.update(body, {
+      where: {
+        userId: context.session.user.id,
+      },
+    });
+    if (!user) {
+      throw new createHttpError.NotFound("User not found");
+    }
+  }
   return NextResponse.json({ success: true });
 });

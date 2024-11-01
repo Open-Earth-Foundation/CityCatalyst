925dc88ac5781d28cf94c79c0bef30540ec4bfda
Amanda Eames <amandaeames@Amandas-MacBook-Pro.local>
2024-11-01 07:05:16 -0400

    fix: checked metadata column is being parsed correctly into the database

--- app/seeders/20231114094254-emissions-factors.cjs
+++ app/seeders/20231114094254-emissions-factors.cjs
@@ -67,14 +67,9 @@ module.exports = {
         const methodologies = await parseFile("Methodology", folder);
         const emissionsFactorsRaw = await parseFile("EmissionsFactor", folder);
         const emissionsFactors = emissionsFactorsRaw.map((ef) => {
-          const metadata = (ef.metadata ? ef.metadata : "")
-            .split(", ")
-            .map((entry) => entry.split(":"));
-          ef.metadata = JSON.stringify(Object.fromEntries(metadata));
-          if (!ef.year) {
-            ef.year = null;
-          }
-
+          const metadata = ef.metadata ? ef.metadata : "{}";
+          ef.metadata = metadata.replace(/'/g, '"');
+          ef.year = !!ef.year ? parseInt(ef.year) : null;
           return ef;
         });
 

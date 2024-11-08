20fc91388c529cc60b6153a9927576bbdf96917b
Milan Gruner <milangruner@gmail.com>
2024-11-08 12:09:33 +0100

    fix(ui): SegmentedProgress not rounded when using only uploaded data

--- app/src/components/SegmentedProgress.tsx
+++ app/src/components/SegmentedProgress.tsx
@@ -8,8 +8,6 @@ import {
   Tbody,
   Td,
   Text,
-  Tfoot,
-  Th,
   Tooltip,
   Tr,
   useToken,
@@ -46,11 +44,12 @@ export function SegmentedProgress({
 }) {
   const colorValues = useToken("colors", colors);
   const tooltipRef = useRef(null);
-  const normalizedValues = values.map((v) =>
+  const normalizedValues = values.map((v, i) =>
     typeof v === "number"
-      ? { percentage: v, name: `Segment ${values.indexOf(v) + 1}`, value: max }
+      ? { percentage: v, name: `Segment ${i + 1}`, value: max }
       : v,
   );
+  console.log({ normalizedValues }, "TODO remove");
   const tooltipContent = (
     <TableContainer>
       <Table variant="unstyled" size={"sm"}>
@@ -114,7 +113,8 @@ export function SegmentedProgress({
             h={height}
             w={`${(100 * value.percentage) / max}%`}
             borderStartRadius={
-              i === 0 || (i === 1 && normalizedValues[0].value === 0)
+              i === 0 ||
+              (i === 1 && (normalizedValues[0].percentage ?? 0) === 0)
                 ? "full"
                 : undefined
             }

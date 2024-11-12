14bacfabf601cf3e7ce99a36551fb10ba7e289de
Cephas Chapa <cephaschapa@gmail.com>
2024-11-12 13:44:10 +0200

    fix: adds drop type statement to migration

--- app/migrations/20241111122007-alter-add-global-warming-potential-type.cjs
+++ app/migrations/20241111122007-alter-add-global-warming-potential-type.cjs
@@ -5,8 +5,11 @@ alter table "Inventory"
     add global_warming_potential_type global_warming_potential_type default 'ar6' not null;
 `;
 
-const sql_down = `ALTER TABLE "Inventory"
-    DROP COLUMN "global_warming_potential_type";`;
+const sql_down = `
+  ALTER TABLE "Inventory"
+    DROP COLUMN "global_warming_potential_type";
+  DROP TYPE IF EXISTS global_warming_potential_type;
+`;
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {

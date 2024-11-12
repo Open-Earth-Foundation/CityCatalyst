72fe2a3b975243f5b8bde1ce657b19047819994e
Cephas Chapa <cephaschapa@gmail.com>
2024-11-12 12:50:21 +0200

    removes comments in wizard steps

--- app/src/components/wizard-steps.tsx
+++ app/src/components/wizard-steps.tsx
@@ -1,7 +1,5 @@
 import {
   Box,
-  Flex,
-  Progress,
   Step,
   StepIcon,
   StepIndicator,
@@ -35,52 +33,33 @@ export default function WizardSteps({
     { fallback: "md" },
   );
 
-  const progressPercent = ((currentStep + 1) / steps.length) * 100;
-
   return (
-    <Box>
-      {/* <Stepper
-        index={currentStep}
-        my={8}
-        colorScheme="brandScheme"
-        size="lg"
-        orientation={orientation}
-        gap={gap}
-      >
-        {steps.map((step, index) => (
-          <Step key={index} onClick={() => onSelect(index)}>
-            <StepSeparator  />
-          </Step>
-        ))}
-      </Stepper>
-      <Progress
-        value={progressPercent}
-        position="absolute"
-        height="3px"
-        width="full"
-        top="10px"
-        zIndex={-1}
-      /> */}
-      <Flex>
-        {steps.map((step, index) => (
-          <Box
-            key={index}
-            height="8px"
-            w="full"
-            mx="2px" // Adjusts the gap between segments
-            bg={
-              index < currentStep
-                ? "content.link" // Completed steps
-                : index === currentStep
-                  ? "content.link" // Active step
-                  : "background.neutral" // Upcoming steps
-            }
-            borderRadius="md"
-            cursor="pointer"
-            onClick={() => onSelect(index)}
-          ></Box>
-        ))}
-      </Flex>
-    </Box>
+    <Stepper
+      index={currentStep}
+      my={8}
+      colorScheme="brandScheme"
+      size="lg"
+      height="180px"
+      orientation={orientation}
+      gap={gap}
+    >
+      {steps.map((step, index) => (
+        <Step key={index} onClick={() => onSelect(index)}>
+          <StepIndicator>
+            <StepStatus
+              complete={<StepIcon />}
+              incomplete={<StepNumber />}
+              active={<StepNumber />}
+            />
+          </StepIndicator>
+
+          <Box flexShrink="0">
+            <StepTitle>{step.title}</StepTitle>
+          </Box>
+
+          <StepSeparator />
+        </Step>
+      ))}
+    </Stepper>
   );
 }

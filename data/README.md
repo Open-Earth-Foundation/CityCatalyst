df8c550c59fe7264522068cbe835111c700e75ec
Evan Prodromou <evan@openearth.org>
2024-11-13 09:03:11 -0500

    feat: data and schema directories
    Signed-off-by: Evan Prodromou <evan@openearth.org>

---
+++ data/README.md
@@ -0,0 +1,3 @@
+# Data
+
+Placeholder for data.
---
+++ pipeline.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This script is used to run the pipeline for the project
+
+echo "Extractor..."
+# Run extractor
+echo "Done."
+
+echo "Prioritizer..."
+# Run prioritizer
+echo "Done."
+
+echo "Uploader..."
+# Run uploader
+echo "Done."

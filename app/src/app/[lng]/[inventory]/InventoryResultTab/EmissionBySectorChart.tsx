f9db97b7edbdfcc04a93ea59d737c5343f65875a
isaacbello <isaacbello3200@gmail.com>
2024-12-09 14:01:27 +0100

    fix: resolve chart rendering bug

--- app/src/app/[lng]/[inventory]/InventoryResultTab/EmissionBySectorChart.tsx
+++ app/src/app/[lng]/[inventory]/InventoryResultTab/EmissionBySectorChart.tsx
@@ -3,6 +3,8 @@ import { ResponsiveBar } from "@nivo/bar";
 import { SECTORS } from "@/util/constants";
 import { convertKgToKiloTonnes } from "@/util/helpers";
 import { useTranslation } from "@/i18n/client";
+import { toKebabCaseModified } from "@/app/[lng]/[inventory]/InventoryResultTab/index";
+import { Box, Text } from "@chakra-ui/react";
 
 interface EmissionBySectorChartProps {
   data: {
@@ -18,11 +20,12 @@ const EmissionBySectorChart: React.FC<EmissionBySectorChartProps> = ({
   data,
   lng,
 }) => {
+  // const { t: tEnglish } = useTranslation("en", "data");
   const { t: tData } = useTranslation(lng, "data");
   const defaultBreakdown = SECTORS.reduce((acc, sector) => {
     return {
       ...acc,
-      [tData(sector.name).toLowerCase()]: 0,
+      [toKebabCaseModified(sector.name)]: 0,
     };
   }, {});
 
@@ -30,7 +33,7 @@ const EmissionBySectorChart: React.FC<EmissionBySectorChartProps> = ({
     const sectorBreakDown = item.bySector.reduce((acc, sector) => {
       return {
         ...acc,
-        [tData(sector.sectorName).toLowerCase()]: convertKgToKiloTonnes(
+        [toKebabCaseModified(sector.sectorName)]: convertKgToKiloTonnes(
           sector.co2eq,
         ),
       };
@@ -42,84 +45,118 @@ const EmissionBySectorChart: React.FC<EmissionBySectorChartProps> = ({
   });
 
   const chartDataKeys = SECTORS.map((sector) =>
-    tData(sector.name).toLowerCase(),
+    toKebabCaseModified(sector.name),
   );
 
   const colors = ["#5785F4", "#F17105", "#25AC4B", "#BFA937", "#F5D949"];
+
   return (
-    <div style={{ height: "500px" }}>
-      <ResponsiveBar
-        borderRadius={5}
-        enableLabel={false}
-        data={chartData}
-        keys={chartDataKeys}
-        indexBy="year"
-        groupMode={"stacked"}
-        layout={"vertical"}
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-        padding={0.3}
-        valueScale={{ type: "linear", min: 0, max: "auto" }}
-        indexScale={{ type: "band", round: true }}
-        colors={colors}
-        borderColor={{
-          from: "color",
-          modifiers: [["darker", 1.6]],
-        }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: tData("year"),
-          legendPosition: "middle",
-          legendOffset: 40,
-        }}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "CO2eq",
-          legendPosition: "middle",
-          legendOffset: -55,
-          format: (value) => `${value} kt`,
-        }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor={{
-          from: "color",
-          modifiers: [["darker", 1.6]],
-        }}
-        legends={[
-          {
-            dataFrom: "keys",
-            anchor: "bottom-right",
-            direction: "column",
-            justify: false,
-            translateX: 120,
-            translateY: 0,
-            itemsSpacing: 2,
-            itemWidth: 100,
-            itemHeight: 20,
-            itemDirection: "left-to-right",
-            itemOpacity: 0.85,
-            symbolSize: 20,
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemOpacity: 1,
-                },
-              },
-            ],
-          },
-        ]}
-        role="application"
-        ariaLabel="Nivo bar chart demo"
-        barAriaLabel={function (e) {
-          return e.id + ": " + e.formattedValue + " in year: " + e.indexValue;
-        }}
-      />
+    <div className="min-h-[600px]">
+      <div className="h-[600px]">
+        <ResponsiveBar
+          borderRadius={5}
+          enableLabel={false}
+          data={chartData}
+          keys={chartDataKeys}
+          indexBy="year"
+          groupMode={"stacked"}
+          layout={"vertical"}
+          margin={{ top: 50, right: 130, bottom: 50, left: 120 }}
+          padding={0.3}
+          valueScale={{ type: "linear", min: 0, max: "auto" }}
+          indexScale={{ type: "band", round: true }}
+          colors={colors}
+          borderColor={{
+            from: "color",
+            modifiers: [["darker", 1.6]],
+          }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: tData("year"),
+            legendPosition: "middle",
+            legendOffset: 40,
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "CO2eq",
+            legendPosition: "middle",
+            legendOffset: -75,
+            format: (value) => value,
+          }}
+          labelSkipWidth={12}
+          labelSkipHeight={12}
+          labelTextColor={{
+            from: "color",
+            modifiers: [["darker", 1.6]],
+          }}
+          // legends={[
+          //   {
+          //     dataFrom: "keys",
+          //     anchor: "bottom-right",
+          //     direction: "column",
+          //     justify: false,
+          //     translateX: 120,
+          //     translateY: 0,
+          //     itemsSpacing: 2,
+          //     itemWidth: 100,
+          //     itemHeight: 20,
+          //     itemDirection: "left-to-right",
+          //     itemOpacity: 0.85,
+          //     symbolSize: 20,
+          //     effects: [
+          //       {
+          //         on: "hover",
+          //         style: {
+          //           itemOpacity: 1,
+          //         },
+          //       },
+          //     ],
+          //   },
+          // ]}
+          role="application"
+          ariaLabel="Nivo bar chart demo"
+          barAriaLabel={function (e) {
+            return e.id + ": " + e.formattedValue + " in year: " + e.indexValue;
+          }}
+        />
+      </div>
+      <Text
+        color="content.secondary"
+        fontSize="label.large"
+        className="capitalize"
+        fontWeight="500"
+      >
+        {tData("legend")}
+      </Text>
+      <Box className="flex items-center justify-start flex-wrap mt-3 gap-2">
+        {SECTORS.map((sector, index) => (
+          <Box
+            key={sector.name}
+            backgroundColor="background.neutral"
+            borderRadius="50px"
+            className="flex items-center justify-center px-3 py-1.5"
+            mr={4}
+          >
+            <Box
+              className="h-4 w-4"
+              style={{ backgroundColor: colors[index] }}
+            ></Box>
+            <Text
+              fontSize="body.md"
+              className="ml-2"
+              color="content.alternative"
+            >
+              {tData(toKebabCaseModified(sector.name))}
+            </Text>
+          </Box>
+        ))}
+      </Box>
     </div>
   );
 };

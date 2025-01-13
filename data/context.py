1d62c3d311189fe12296cfc32cab88cd01efe866
Mirco Rudolph <mirco.rudolph@gmail.com>
2025-01-13 20:52:12 +0800

    chore: added context for defining SGDs

---
+++ data/context.py
@@ -0,0 +1,87 @@
+sgds = [
+    {
+        "sdg_number": 1,
+        "name": "No Poverty",
+        "description": "End poverty in all its forms everywhere.",
+    },
+    {
+        "sdg_number": 2,
+        "name": "Zero Hunger",
+        "description": "End hunger, achieve food security and improved nutrition and promote sustainable agriculture.",
+    },
+    {
+        "sdg_number": 3,
+        "name": "Good Health and Well-being",
+        "description": "Ensure healthy lives and promote well-being for all at all ages.",
+    },
+    {
+        "sdg_number": 4,
+        "name": "Quality Education",
+        "description": "Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all.",
+    },
+    {
+        "sdg_number": 5,
+        "name": "Gender Equality",
+        "description": "Achieve gender equality and empower all women and girls.",
+    },
+    {
+        "sdg_number": 6,
+        "name": "Clean Water and Sanitation",
+        "description": "Ensure availability and sustainable management of water and sanitation for all.",
+    },
+    {
+        "sdg_number": 7,
+        "name": "Affordable and Clean Energy",
+        "description": "Ensure access to affordable, reliable, sustainable and modern energy for all.",
+    },
+    {
+        "sdg_number": 8,
+        "name": "Decent Work and Economic Growth",
+        "description": "Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all.",
+    },
+    {
+        "sdg_number": 9,
+        "name": "Industry, Innovation, and Infrastructure",
+        "description": "Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation.",
+    },
+    {
+        "sdg_number": 10,
+        "name": "Reduced Inequalities",
+        "description": "Reduce inequality within and among countries.",
+    },
+    {
+        "sdg_number": 11,
+        "name": "Sustainable Cities and Communities",
+        "description": "Make cities and human settlements inclusive, safe, resilient and sustainable.",
+    },
+    {
+        "sdg_number": 12,
+        "name": "Responsible Consumption and Production",
+        "description": "Ensure sustainable consumption and production patterns.",
+    },
+    {
+        "sdg_number": 13,
+        "name": "Climate Action",
+        "description": "Take urgent action to combat climate change and its impacts.",
+    },
+    {
+        "sdg_number": 14,
+        "name": "Life Below Water",
+        "description": "Conserve and sustainably use the oceans, seas and marine resources for sustainable development.",
+    },
+    {
+        "sdg_number": 15,
+        "name": "Life on Land",
+        "description": "Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation, and halt biodiversity loss.",
+    },
+    {
+        "sdg_number": 16,
+        "name": "Peace, Justice, and Strong Institutions",
+        "description": "Promote peaceful and inclusive societies for sustainable development, provide access to justice for all, and build effective, accountable and inclusive institutions at all levels.",
+    },
+    {
+        "sdg_number": 17,
+        "name": "Partnerships for the Goals",
+        "description": "Strengthen the means of implementation and revitalize the Global Partnership for Sustainable Development.",
+    },
+]

8c1673f6aeb39dca18bc2acddf22e45953538702
Mirco Rudolph <mirco.rudolph@gmail.com>
2025-01-13 10:58:33 +0800

    chore: added document to vector store

--- scripts/add_document_to_vectorstore.py
+++ scripts/add_document_to_vectorstore.py
@@ -3,10 +3,9 @@
 from pathlib import Path
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.docstore.document import Document
 from langchain_chroma import Chroma
-
 from langchain_openai import OpenAIEmbeddings
+from utils.load_vectorstore import load_vectorstore
 
 base_path = Path(__file__).parent.parent
 file_folder_base_path = base_path / "data" / "files"

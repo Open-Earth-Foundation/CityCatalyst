3057f3e556cdb2f3277f94d0f8f0028aee736cdf
Mirco Rudolph <mirco.rudolph@gmail.com>
2025-01-17 18:46:09 +0800

    feat: added argparse

--- main.py
+++ main.py
@@ -1,45 +1,79 @@
 import json
+import argparse
 from graph_definition import create_graph
 from state.agent_state import AgentState
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from pathlib import Path
 
-# Path to data
-template_path = Path(__file__).parent / "data" / "climate_action_plan_template.json"
-climate_action_data_path = Path(__file__).parent / "data" / "climate_action.json"
-city_data_path = Path(__file__).parent / "data" / "city.json"
 
-# Load the template with docstring
-with open(template_path, "r") as f:
-    template = json.load(f)
+def mock_api_body(climate_action_id: str, city_data_loc: str):
+    """
+    Function that mocks the API request body for the climate action and city data.
+    """
+    # Path to data
+    climate_action_data_path = (
+        Path(__file__).parent / "data" / "input" / (climate_action_id + ".json")
+    )
+    city_data_path = (
+        Path(__file__).parent / "data" / "input" / (city_data_loc + ".json")
+    )
 
-with open(climate_action_data_path, "r") as f:
-    climate_action_data = json.load(f)
+    with open(climate_action_data_path, "r") as f:
+        climate_action_data = json.load(f)
 
-with open(city_data_path, "r") as f:
-    city_data = json.load(f)
+    with open(city_data_path, "r") as f:
+        city_data = json.load(f)
 
-# Create the graph
-graph = create_graph()
+    return climate_action_data, city_data
 
-# Initial shared state
-initial_state = AgentState(
-    climate_action_data=climate_action_data,
-    city_data=city_data,
-    response_agent_1=AIMessage(""),
-    response_agent_2=AIMessage(""),
-    response_agent_3=AIMessage(""),
-    response_agent_4=AIMessage(""),
-    response_agent_5=AIMessage(""),
-    response_agent_6=AIMessage(""),
-    response_agent_7=AIMessage(""),
-    response_agent_8=AIMessage(""),
-    response_agent_combine="",
-    messages=[],
-)
 
-# Invoke the compiled graph
-result = graph.invoke(input=initial_state)
+def create_plan(climate_action_data: dict, city_data: dict):
 
-# Print the result
-print("All done")
+    # Create the graph
+    graph = create_graph()
+
+    # Initial shared state
+    initial_state = AgentState(
+        climate_action_data=climate_action_data,
+        city_data=city_data,
+        response_agent_1=AIMessage(""),
+        response_agent_2=AIMessage(""),
+        response_agent_3=AIMessage(""),
+        response_agent_4=AIMessage(""),
+        response_agent_5=AIMessage(""),
+        response_agent_6=AIMessage(""),
+        response_agent_7=AIMessage(""),
+        response_agent_8=AIMessage(""),
+        response_agent_combine="",
+        messages=[],
+    )
+
+    # Invoke the compiled graph
+    result = graph.invoke(input=initial_state)
+
+    print("Plan created successfully.")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Create a climate action plan.")
+    parser.add_argument(
+        "--climate_action_id",
+        type=str,
+        required=True,
+        help="The ID of the climate action to create a plan for. E.g. 'c40_xxxx' or 'ipcc_xxxx'.",
+    )
+    parser.add_argument(
+        "--city_data_loc",
+        type=str,
+        required=True,
+        help="The locode of the city data for the climate action. E.g. BRCCI",
+    )
+
+    args = parser.parse_args()
+
+    climate_action_data, city_data = mock_api_body(
+        climate_action_id=args.climate_action_id, city_data_loc=args.city_data_loc
+    )
+
+    create_plan(climate_action_data, city_data)

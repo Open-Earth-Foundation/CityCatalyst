23c787368ac8dc52257a53d32a9f170c6f33f571
Mirco Rudolph <mirco.rudolph@gmail.com>
2025-01-17 18:45:27 +0800

    feat: added experimental tool

--- tools/tools.py
+++ tools/tools.py
@@ -2,6 +2,8 @@ from typing import Optional
 from langchain.tools import tool
 from utils.load_vectorstore import load_vectorstore
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
 
 
 # Define tools for each agent
@@ -13,7 +15,7 @@ def document_retriever_tool(search_query: str, metadata_filter: Optional[dict] =
     """
         Use this tool to retrieve chunks of text from a collection within a Chroma vector store.
 
-    This Chroma vector store contains a collections with docuemnts related to Brazil's overall climate strategy.
+    This Chroma vector store contains a collections with documents related to Brazil's overall climate strategy.
     Use this tool to retrieve relevant information.
 
     **Input**:
@@ -53,6 +55,43 @@ search = TavilySearchResults(
     Input: A search query in the national language.
     """,
 )
+
+
+# The reasoning_tool is experimental. It increases the time a lot but could be used to make sure that the retrieved chunks are relevant.
+# When using this tool, instruct the agent to use it in its system prompt e.g.:
+#     inside <task> c. Check the relevance of the retrieved information against the search query.
+#     inside <tools> - a reasoning tool that can be used to reason over the relevance of the retrieved information from the documents.
+@tool
+def reasoning_tool(search_query: str, chunk: str):
+    """
+    Use this tool to reason over the relevance of the retrieved information from the documents.
+    Compare the original query with the retrieved information and provide a reasoning for each retrieved document.
+    If the document is relevant, provide a short reasoning why it is relevant.
+    If the document is not relevant, provide a short reasoning why it is not relevant.
+
+    **Input**:
+    - search_query (str) - A search query in the national language.
+
+    **Output**:
+    - reasoning (str) - Your reasoning about the relevance of the chunk to the search query.
+    """
+
+    prompt_str = f"""
+    You are given a user query:
+    "{search_query}"
+
+    And you are given a chunk of text:
+    "{chunk}"
+
+    Determine if the chunk is relevant to the user query. 
+    Start your output with either "Relevant:" or "Irrelevant:", followed by 
+    1-2 sentences explaining your reasoning.
+    """
+
+    llm = ChatOpenAI(temperature=0, seed=42)
+    response = llm.invoke([HumanMessage(content=prompt_str)])
+
+    return response.content
 
 
 @tool

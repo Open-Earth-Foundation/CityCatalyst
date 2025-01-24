70e6ddb24e7b110e57e72c1e1cfc834017da9555
Mirco Rudolph <mirco.rudolph@gmail.com>
2025-01-24 17:39:48 +0800

    chore: updated context for agents

--- data/context.py
+++ data/context.py
@@ -85,3 +85,64 @@ sgds = [
         "description": "Strengthen the means of implementation and revitalize the Global Partnership for Sustainable Development.",
     },
 ]
+
+adaptation = """
+Climate risks (hazards) can be one of the following:
+- **Droughts**
+- **Heatwaves** 
+- **Floods**
+- **Sea-level-rise**
+- **Landslides**
+- **Storms**
+- **Wildfires**
+- **Diseases**
+"""
+
+mitigation = """
+The following gives an overview of the different climate mitigation sectors.
+
+There are 5 sectors according to the 'Global Protocol for Community-Scale Greenhous Gas Inventories' (GPC):
+- **Stationary Energy**,
+- **Transportation**,
+- **Waste"**
+- **Industrial Process And Product Use (IPPU)**
+- **Agriculture, Forestry and other Land Use (AFOLU)**
+
+The following is a description of the different sectors:
+- Stationary Energy: 
+    Stationary energy sources are one of the largest contributors to a city's GHG emissions.
+    These emissions come from the combustion of fuel in residential, commercial and
+    institutional buildings and facilities and manufacturing industries and construction, as well
+    as power plants to generate grid-supplied energy. This sector also includes fugitive
+    emissions, which typically occur during extraction, transformation, and transportation of
+    primary fossil fuels.
+
+- Transportation:
+    Transportation covers all journeys by road, rail, water and air, including inter-city and
+    international travel. GHG emissions are produced directly by the combustion of fuel or
+    indirectly by the use of grid-supplied electricity. 
+
+- Waste:
+    Waste disposal and treatment produces GHG emissions through aerobic or anaerobic
+    decomposition, or incineration. GHG emissions from solid waste shall be calculated by disposal
+    route, namely landfill, biological treatment and incineration and open burning. If methane is
+    recovered from solid waste or wastewater treatment facilities as an energy source, it shall be
+    reported under Stationary Energy. Similarly, emissions from incineration with energy recovery
+    are reported under Stationary Energy.
+
+- Industrial Process And Product Use (IPPU):
+    GHG emissions are produced from a wide variety of non-energy related industrial activities.
+    The main emission sources are releases from industrial processes that chemically or physically
+    transform materials (e.g., the blast furnace in the iron and steel industry, and ammonia and
+    other chemical products manufactured from fossil fuels and used as chemical feedstock).
+    During these processes many different GHGs can be produced. In addition, certain products
+    used by industry and end-consumers, such as refrigerants, foams or aerosol cans, also contain
+    GHGs which can be released during use and disposal.
+
+- Agriculture, Forestry and other Land Use (AFOLU):
+    Emissions and removals from the Agriculture, Forestry and Other Land Use (AFOLU) sector are
+    produced through a variety of pathways, including livestock (enteric fermentation and manure
+    management), land use and land use change (e.g., forested land being cleared for cropland
+    or settlements), and aggregate sources and non-CO2 emission sources on land (e.g., fertilizer
+    application and rice cultivation).    
+"""

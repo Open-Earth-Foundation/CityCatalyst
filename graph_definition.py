30e4722c211693e2b139f128e1cd464866519216
Mirco Rudolph <mirco.rudolph@gmail.com>
2025-01-24 17:41:14 +0800

    chore: added new agents

--- graph_definition.py
+++ graph_definition.py
@@ -8,41 +8,38 @@ from agents.agent_2_sub_actions import build_custom_agent_2
 from agents.agent_3_involved_partners import build_custom_agent_3
 from agents.agent_4_goals_milestones import build_custom_agent_4
 from agents.agent_5_timeline import build_custom_agent_5
-from agents.agent_6_cost_budget import build_custom_agent_6
+
+# from agents.agent_6_cost_budget import build_custom_agent_6
 from agents.agent_7_mer import build_custom_agent_7
-from agents.agent_8_sgds import build_custom_agent_8
+from agents.agent_8_adaptation import build_custom_agent_8
+from agents.agent_9_mitigation import build_custom_agent_9
+from agents.agent_10_sgds import build_custom_agent_10
 from agents.agent_combine import custom_agent_combine
 
-from tools.tools import document_retriever_tool, search, placeholder_tool
-
-# Include following line for experimental tool
-# from tools.tools import reasoning_tool
-
-# 1. Brazil_NDC_November2024.pdf
-#    - Document Name: Brazil's Nationally Determined Contribution (NDC) to the Paris Agreement
-#    - Content: This document outlines Brazil's climate action plan strategy.
-
-# 2. Green_Cities_Brazil.pdf
-#    - Document Name: Green Cities: Cities and Climate Change in Brazil
-#    - Content: This report from the World Bank discusses opportunities for mitigating urban greenhouse gas emissions in sectors like transport, land use, energy efficiency, waste management, and urban forestry in Brazil.
-
-# Both documents are limited in scope to Brazil's climate action plan and its implementation.
+from tools.tools import (
+    retriever_main_action_tool,
+    retriever_sub_action_tool,
+    search_municipalities_tool,
+    placeholder_tool,
+)
 
 
 # Create the agents
 model = ChatOpenAI(model="gpt-4o", temperature=0.0, seed=42)
 
 
-agent_1 = build_custom_agent_1(model, [document_retriever_tool])
-agent_2 = build_custom_agent_2(model, [placeholder_tool])
+agent_1 = build_custom_agent_1(model, [retriever_main_action_tool])
+agent_2 = build_custom_agent_2(model, [retriever_sub_action_tool])
 agent_3 = build_custom_agent_3(
-    model, [search]
+    model, [search_municipalities_tool]
 )  # for debugging purposes, 'search' tool is not provided to save on API calls. Add [search] to the list of tools to enable search tool.
 agent_4 = build_custom_agent_4(model, [placeholder_tool])
 agent_5 = build_custom_agent_5(model, [placeholder_tool])
-agent_6 = build_custom_agent_6(model, [placeholder_tool])
+# agent_6 = build_custom_agent_6(model, [placeholder_tool])
 agent_7 = build_custom_agent_7(model, [placeholder_tool])
 agent_8 = build_custom_agent_8(model, [placeholder_tool])
+agent_9 = build_custom_agent_9(model, [placeholder_tool])
+agent_10 = build_custom_agent_10(model, [placeholder_tool])
 agent_combine = custom_agent_combine
 
 
@@ -54,22 +51,27 @@ def create_graph():
     builder.add_node("agent_3", agent_3)
     builder.add_node("agent_4", agent_4)
     builder.add_node("agent_5", agent_5)
-    builder.add_node("agent_6", agent_6)
+    # builder.add_node("agent_6", agent_6)
     builder.add_node("agent_7", agent_7)
     builder.add_node("agent_8", agent_8)
+    builder.add_node("agent_9", agent_9)
+    builder.add_node("agent_10", agent_10)
     builder.add_node("agent_combine", agent_combine)
 
     # Define the edges
     builder.add_edge(START, "agent_1")
-    builder.add_edge("agent_1", END)
-    # builder.add_edge("agent_1", "agent_2")
+    # builder.add_edge("agent_1", END)
+    builder.add_edge("agent_1", "agent_2")
+    builder.add_edge("agent_2", END)
     # builder.add_edge("agent_2", "agent_3")
     # builder.add_edge("agent_3", "agent_4")
     # builder.add_edge("agent_4", "agent_5")
-    # builder.add_edge("agent_5", "agent_6")
-    # builder.add_edge("agent_6", "agent_7")
+    # builder.add_edge("agent_5", "agent_7")
+    # # builder.add_edge("agent_6", "agent_7")
     # builder.add_edge("agent_7", "agent_8")
-    # builder.add_edge("agent_8", "agent_combine")
+    # builder.add_edge("agent_8", "agent_9")
+    # builder.add_edge("agent_9", "agent_10")
+    # builder.add_edge("agent_10", "agent_combine")
     # builder.add_edge("agent_combine", END)
 
     # Compile the graph

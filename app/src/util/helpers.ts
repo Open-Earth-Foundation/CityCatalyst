648936c2b2ff522254c3238b0aa1f6f79beeb77b
thehighestprimenumber <thehighestprimenumber@gmail.com>
2025-02-05 16:04:15 -0300

    [ON-3316] fix translation of subsector names

--- app/src/util/helpers.ts
+++ app/src/util/helpers.ts
@@ -320,6 +320,7 @@ export const toKebabCase = (input: string | undefined): string => {
     .replace(/([a-z0-9])([A-Z])/g, "$1-$2")
     .replace(/[\s_]+/g, "-")
     .replace(/^-+|-+$/g, "")
+    .replace(/[^\w-]+/g, "")
     .toLowerCase();
 };
 

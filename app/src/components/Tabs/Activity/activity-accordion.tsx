3e28e242ce53ab1faa360081b85eb3a8f3a0d740
Milan Gruner <milangruner@gmail.com>
2025-02-07 16:14:54 +0100

    fix(ui): missing useEffect hook dependency referenceNumber on ActivityAccordion

--- app/src/components/Tabs/Activity/activity-accordion.tsx
+++ app/src/components/Tabs/Activity/activity-accordion.tsx
@@ -151,7 +151,7 @@ const ActivityAccordion: FC<ActivityAccordionProps> = ({
     return {
       activityGroups,
     };
-  }, [activityData, methodology]);
+  }, [activityData, methodology, referenceNumber]);
 
   // let extraFields = (methodology as Methodology)?.activities?.[0]?.[
   //   "extra-fields"

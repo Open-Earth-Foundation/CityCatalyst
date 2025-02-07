d4264d200c3c73247250644871fffa0c730d1f59
Milan Gruner <milangruner@gmail.com>
2025-02-07 12:50:06 +0100

    fix(ui): errors on update password page

--- app/src/app/[lng]/auth/update-password/page.tsx
+++ app/src/app/[lng]/auth/update-password/page.tsx
@@ -2,12 +2,13 @@
 
 import PasswordInput from "@/components/password-input";
 import { useTranslation } from "@/i18n/client";
-import { InfoOutlineIcon } from "@chakra-ui/icons";
-import { Button, FormHelperText, Heading, Text } from "@chakra-ui/react";
+import { MdInfoOutline } from "react-icons/md";
+import { Button, Heading, Text, Icon } from "@chakra-ui/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { logger } from "@/services/logger";
+import { Field } from "@/components/ui/field";
 
 type Inputs = {
   password: string;
@@ -30,9 +31,9 @@ export default function UpdatePassword({
     register,
     formState: { errors, isSubmitting },
     setError: setFormError,
-    watch
+    watch,
   } = useForm<Inputs>();
-  const watchPassword = watch("password", "")
+  const watchPassword = watch("password", "");
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     if (data.password !== data.confirmPassword) {
       setFormError("confirmPassword", {
@@ -72,19 +73,30 @@ export default function UpdatePassword({
         {t("update-password-details")}
       </Text>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-        <PasswordInput
-          register={register}
-          error={errors.password}
-          name={t("new-password")}
-          t={t}
-          shouldValidate
-          watchPassword={watchPassword}
+        <Field
+          helperText={
+            <>
+              <Icon
+                as={MdInfoOutline}
+                color="#2351DC"
+                mr={1.5}
+                mt={-0.5}
+                boxSize={4}
+              />
+              {t("password-hint")}
+            </>
+          }
+          label={t("new-password")}
         >
-          <FormHelperText>
-            <InfoOutlineIcon color="#2351DC" mr={1.5} mt={-0.5} boxSize={4} />
-            {t("password-hint")}
-          </FormHelperText>
-        </PasswordInput>
+          <PasswordInput
+            register={register}
+            error={errors.password}
+            name={t("new-password")}
+            t={t}
+            shouldValidate
+            watchPassword={watchPassword}
+          ></PasswordInput>
+        </Field>
         <PasswordInput
           register={register}
           error={errors.confirmPassword}
@@ -93,7 +105,7 @@ export default function UpdatePassword({
           t={t}
         />
         {error && <Text color="semantic.danger">{error}</Text>}
-        <Button type="submit" isLoading={isSubmitting} h={16} width="full">
+        <Button type="submit" loading={isSubmitting} h={16} width="full">
           {t("reset-button")}
         </Button>
         <Button

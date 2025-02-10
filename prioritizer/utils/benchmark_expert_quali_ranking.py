4b65c881f18dc03b6f0b9f491bfef244d3ee6b4d
Mirco Rudolph <mirco.rudolph@gmail.com>
2025-02-10 20:00:04 +0100

    feat: new benchmark file for qualitative scoring

--- prioritizer/utils/benchmark_expert_quali_ranking.py
+++ prioritizer/utils/benchmark_expert_quali_ranking.py
@@ -1,9 +1,8 @@
-from prioritizer.prioritizer import quantitative_score
-from reading_writing_data import read_city_inventory, read_actions
+"""
+This script benchmarks the linear ranking system against the expert labeled data.
+"""
+
 import pandas as pd
-import glob
-import os
-import glob
 from pathlib import Path
 import json
 import sys
@@ -17,6 +16,9 @@ project_root = str(Path(__file__).resolve().parent.parent.parent)
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
+from prioritizer.prioritizer import quantitative_score, qualitative_score
+from reading_writing_data import read_city_inventory, read_actions
+
 
 def load_data_from_folder(folder_path):
     """
@@ -24,7 +26,7 @@ def load_data_from_folder(folder_path):
     Assumes each JSON file contains a list of comparison dictionaries.
     """
     all_data = []
-    json_files = glob.glob(os.path.join(folder_path, "*.json"))
+    json_files = list(folder_path.glob("*.json"))
     if not json_files:
         print(f"No JSON files found in {folder_path}. Please check the folder path.")
         return pd.DataFrame()
@@ -46,6 +48,9 @@ def load_data_from_folder(folder_path):
 
 
 def get_action_by_id(actions, target_action_id):
+    """
+    Returns the action dictionary with the specified ActionID from the list of actions.
+    """
 
     for action in actions:
         if action.get("ActionID") == target_action_id:
@@ -57,15 +62,14 @@ def get_action_by_id(actions, target_action_id):
 df_all_comparisons = load_data_from_folder(folder_path)
 
 print(df_all_comparisons.head())
-
 print(len(df_all_comparisons))
 
 # Delete the irrelevant and unsure comparisons
 df_all_comparisons_cleaned = df_all_comparisons[
     df_all_comparisons["PreferredAction"] != "Irrelevant"
 ]
-df_all_comparisons_cleaned = df_all_comparisons[
-    df_all_comparisons["PreferredAction"] != "Unsure"
+df_all_comparisons_cleaned = df_all_comparisons_cleaned[
+    df_all_comparisons_cleaned["PreferredAction"] != "Unsure"
 ]
 
 print(df_all_comparisons_cleaned.head())
@@ -77,12 +81,12 @@ score = 0
 
 for index, row in df_all_comparisons_cleaned.iterrows():
 
-    # print("\n\n\n")
-    # print(f"Processing row {index}")
-    # print("City locode: ", row["CityLocode"])
-    # print("ActionID A: ", row["ActionA"])
-    # print("ActionID B: ", row["ActionB"])
-    # print("Expert label: ", row["PreferredAction"])
+    print("\n\n\n")
+    print(f"Processing row {index}")
+    print("City locode: ", row["CityLocode"])
+    print("ActionID A: ", row["ActionA"])
+    print("ActionID B: ", row["ActionB"])
+    print("Expert label: ", row["PreferredAction"])
 
     # Read the city data for the comparison
     city_data = read_city_inventory(row["CityLocode"])
@@ -93,21 +97,34 @@ for index, row in df_all_comparisons_cleaned.iterrows():
     actionA_with_context = get_action_by_id(actions, actionA)
     actionB_with_context = get_action_by_id(actions, actionB)
 
-    # Calculate the scoring based on the linear ranking system
-    score_A = quantitative_score(city_data, actionA_with_context)
-    score_B = quantitative_score(city_data, actionB_with_context)
+    # Calculate the scoring based on the qualitative ranking system
+    qual_score = qualitative_score(
+        city_data, [actionA_with_context, actionB_with_context]
+    )
 
-    # print(f"Score for A is {score_A}")
-    # print(f"Score for B is {score_B}")
+    if qual_score:
 
-    if score_A > score_B:
-        predicted_label = actionA
+        # print(qual_score.actions[0])
+
+        winner = qual_score.actions[0].action_id
+        loser = qual_score.actions[1].action_id
+
+        print(f"Winner: {winner}")
+        print(f"Loser: {loser}")
+
+        predicted_label = winner
+
+        # If prediction is correct, add 1 to the score
+        if predicted_label == row["PreferredAction"]:
+            print("Prediction is correct")
+            score += 1
+        else:
+            pass
+            print("Prediction is incorrect")
+
     else:
-        predicted_label = actionB
-
-    # If prediction is correct, add 1 to the score
-    if predicted_label == row["PreferredAction"]:
-        score += 1
+        print("Qualitative score is None")
+        print("Skipping this comparison")
 
     # print("Prediction:")
     # print(f"Predicted label is {predicted_label}")

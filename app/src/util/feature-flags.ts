3174eebf9ba9fd124369b8994800f2a90c7943b9
Milan Gruner <milangruner@gmail.com>
2025-02-14 14:23:06 +0100

    feat(ui): add helper methods for interacting with feature flags

---
+++ app/src/util/feature-flags.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import { env } from "next-runtime-env";
+
+export function getFeatureFlags(): string[] {
+  const flags = env("NEXT_PUBLIC_FEATURE_FLAGS");
+  let flagsList: string[] = [];
+
+  if (flags) {
+    flagsList = flags.split(",");
+  }
+
+  return flagsList;
+}
+
+export function hasFeatureFlag(flag: string): boolean {
+  return getFeatureFlags().includes(flag);
+}

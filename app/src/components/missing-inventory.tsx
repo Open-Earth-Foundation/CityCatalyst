0509024898c6050f5cd2e6280d0b271e9425afef
Milan Gruner <milangruner@gmail.com>
2025-02-21 09:26:12 +0100

    fix(ui): redirect to absolute path /onboarding on MissingInventory on dashboard

--- app/src/components/missing-inventory.tsx
+++ app/src/components/missing-inventory.tsx
@@ -14,7 +14,7 @@ const MissingInventory = ({ lng }: { lng: string }) => {
   const router = useRouter();
   useEffect(() => {
     if (!isUserInfoLoading && !userInfo?.defaultInventoryId) {
-      router.push("onboarding");
+      router.push("/onboarding");
     }
   }, [isUserInfoLoading, userInfo, router]);
 
@@ -71,7 +71,7 @@ const MissingInventory = ({ lng }: { lng: string }) => {
               router.push(
                 userInfo?.defaultInventoryId
                   ? `/${userInfo?.defaultInventoryId}`
-                  : "onboarding",
+                  : "/onboarding",
               )
             }
             gap="8px"

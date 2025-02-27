ada877f11b1bdc3d3178e658e93dff8847bb22fd
Milan Gruner <milangruner@gmail.com>
2025-02-27 14:29:27 +0100

    feat(api): improve validation for bulk inventory creation route

--- app/src/app/api/v0/admin/bulk/route.ts
+++ app/src/app/api/v0/admin/bulk/route.ts
@@ -5,8 +5,8 @@ import { z } from "zod";
 
 const createBulkInventoriesRequest = z.object({
   cityLocodes: z.array(z.string()), // List of city locodes
-  emails: z.array(z.string()), // Comma separated list of emails to invite to the all of the created inventories
-  years: z.array(z.number().int()), // List of years to create inventories for (can be comma separated input, multiple select dropdown etc., so multiple years can be chosen)
+  emails: z.array(z.string().email()), // Comma separated list of emails to invite to the all of the created inventories
+  years: z.array(z.number().int().positive()), // List of years to create inventories for (can be comma separated input, multiple select dropdown etc., so multiple years can be chosen)
   scope: z.enum(["gpc_basic", "gpc_basic_plus"]), // Scope selection (gpc_basic or gpc_basic_plus)
   gwp: z.enum(["AR5", "AR6"]), // GWP selection (AR5 or AR6)
 });

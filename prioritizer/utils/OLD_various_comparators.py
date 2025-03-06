569b2c4a37d93662045c28c89dbc067780fce77f
Mirco Rudolph <mirco@openearth.org>
2025-03-06 16:54:17 +0100

    chore: deprecated this module

--- prioritizer/utils/OLD_various_comparators.py
+++ prioritizer/utils/OLD_various_comparators.py
@@ -27,17 +27,17 @@ def majority_vote_compare(city: dict, action_A: dict, action_B: dict) -> int:
 
     # print("winner_quant", winner_quant)
 
-    # # Get the winner of qualitative score
-    # quali_score = qualitative_score(city, [action_A, action_B])
+    # Get the winner of qualitative score
+    quali_score = qualitative_score(city, [action_A, action_B])
 
     # print("quali_score", quali_score)
 
-    # if quali_score:
-    #     winner_quali_action_id = quali_score.actions[0].action_id
+    if quali_score:
+        winner_quali_action_id = quali_score.actions[0].action_id
 
-    #     winner_quali = 1 if winner_quali_action_id == action_A["ActionID"] else -1
+        winner_quali = 1 if winner_quali_action_id == action_A["ActionID"] else -1
 
-    #     print("winner_quali", winner_quali)
+        # print("winner_quali", winner_quali)
 
     # Get the winner of the ML model
     ml_winner = ml_compare(city, action_A, action_B)
@@ -45,15 +45,25 @@ def majority_vote_compare(city: dict, action_A: dict, action_B: dict) -> int:
     # print("ml_winner", ml_winner)
 
     # Get the majority vote
+    print("Calculating majority vote...")
+    print("winner_quant", winner_quant)
+    print("winner_quali", winner_quali)
+    print("ml_winner", ml_winner)
     majority_vote = winner_quant + winner_quali + ml_winner
+    print("majority_vote", majority_vote)
 
     # print("majority_vote", majority_vote)
 
-    if majority_vote == 0:
-        print("No majority vote, using ML model winner.")
-        return ml_winner
+    # if majority_vote == 0:
+    #     print("No majority vote, using ML model winner.")
+    #     return ml_winner
 
     final_winner = 1 if majority_vote > 0 else -1
+
+    print("final_winner", final_winner)
+
+    if final_winner != ml_winner:
+        print("ML != final_winner")
 
     # print("final_winner", final_winner)
 
@@ -91,8 +101,11 @@ def consensus_compare(city: dict, action_A: dict, action_B: dict) -> int:
         return winner_quant
 
     else:
+        print("Disagreement between winner_quant and ml_winner.")
+        print("Using winner_quali to decide...")
         # Get the winner of qualitative score (always -1 or 1)
         quali_score = qualitative_score(city, [action_A, action_B])
+        print("quali_score", quali_score)
         winner_quali_action_id = quali_score.actions[0].action_id
         winner_quali = 1 if winner_quali_action_id == action_A["ActionID"] else -1
 

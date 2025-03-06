7436b7a8988665e54cef4ca5af6fde4ef03cafbe
isaacbello <isaacbello3200@gmail.com>
2025-03-06 15:15:16 +0100

    feat(gtm):[on-3546] correct migrations

--- app/migrations/20250306133212-adds_default_project_id_to_user_table.cjs
+++ app/migrations/20250306133212-adds_default_project_id_to_user_table.cjs
@@ -6,11 +6,12 @@ module.exports = {
     await queryInterface.addColumn("User", "default_project_id", {
       type: Sequelize.UUID,
       allowNull: true,
-      foreignKey: true,
       references: {
         model: "Project",
         key: "project_id",
       },
+      onDelete: "SET NULL",
+      onUpdate: "CASCADE",
     });
   },
 

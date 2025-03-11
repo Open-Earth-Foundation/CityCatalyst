eb4191788494745c0fdf2360350ce740be9de632
Cephas Chapa <cephas@openearth.com>
2025-03-11 15:39:26 +0200

    adds heading component

--- app/src/app/[lng]/[inventory]/data/manage-sectors/page.tsx
+++ app/src/app/[lng]/[inventory]/data/manage-sectors/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+
 import { Box } from "@chakra-ui/react";
 import React from "react";
+import Heading from "./Heading";
+import { useTranslation } from "@/i18n/client";
 
-const ManageSectors = () => {
-  return <Box>MissingKeys</Box>;
+const ManageSubSectors = ({
+  params: { lng, step, inventory },
+}: {
+  params: { lng: string; step: string; inventory: string };
+}) => {
+  const { t } = useTranslation(lng, "manage-subsectors");
+  return (
+    <Box w="full" bg="background.backgroundLight">
+      <Box maxW={1090} mx="auto" px={8}>
+        <Heading t={t} />
+      </Box>
+    </Box>
+  );
 };
 
-export default ManageSectors;
+export default ManageSubSectors;

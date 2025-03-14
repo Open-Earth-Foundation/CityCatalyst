b8b288ba35908bcbf38c82ff604f85ca02abf003
isaacbello <isaacbello3200@gmail.com>
2025-03-14 18:04:38 +0100

    fix: [on-3550] code cleanups suggested by pr comment

--- app/migrations/20250313150119-add_new_number_type_for_project_city_count_limit.cjs
+++ app/migrations/20250313150119-add_new_number_type_for_project_city_count_limit.cjs
@@ -5,12 +5,6 @@ const defaultProjectId = "ebe82f61-b51b-4015-90ef-8b94f86fb0b7";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
     await queryInterface.changeColumn("Project", "city_count_limit", {
       type: Sequelize.BIGINT,
       allowNull: false,
@@ -30,13 +24,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-
     await queryInterface.changeColumn("Project", "city_count_limit", {
       type: Sequelize.INTEGER,
       allowNull: false,

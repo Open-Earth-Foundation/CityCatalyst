467bf2241a5496394815e218c29338a1d9af5a0a
Milan Gruner <milan@openearth.org>
2025-03-18 12:30:08 +0100

    fix(api): adjust health check route to used cached DB query

--- app/src/app/api/v0/check/health/route.ts
+++ app/src/app/api/v0/check/health/route.ts
@@ -2,22 +2,52 @@ import { apiHandler } from "@/util/api";
 import { NextResponse } from "next/server";
 import pkg from "../../../../../../package.json";
 import { db } from "@/models/index";
+import { logger } from "@/services/logger";
 
-export const GET = apiHandler(async () => {
-  if (!db.initialized) {
-    throw new Error("Database not yet initialized");
+let lastCheckTime = 0;
+let lastCheckResult = false;
+const CACHE_TTL = 5000; // 5 seconds
+
+const checkDatabase = async () => {
+  const now = Date.now();
+  if (now - lastCheckTime < CACHE_TTL) {
+    return lastCheckResult;
   }
   try {
-    await db.sequelize?.query('SELECT 1');
-    return NextResponse.json({
-      message: "healthy",
-      version: pkg.version
-    });
-  } catch (error:any) {
-    throw new Error(
+    await db.sequelize?.query("SELECT 1");
+    lastCheckResult = true;
+  } catch (error: any) {
+    lastCheckResult = false;
+    logger.error(
       "Database connection is not working: " + (error instanceof Error)
         ? error.message
         : "unknown reason",
     );
   }
+  lastCheckTime = now;
+  return lastCheckResult;
+};
+
+export const GET = apiHandler(async () => {
+  if (!db.initialized) {
+    throw new Error("Database not yet initialized");
+  }
+
+  const result = await checkDatabase();
+
+  if (result) {
+    return NextResponse.json({
+      message: "healthy",
+      version: pkg.version,
+    });
+  } else {
+    return NextResponse.json(
+      {
+        message: "database-connection-failed",
+        version: pkg.version,
+      },
+      { status: 500 },
+    );
+  }
 });
+

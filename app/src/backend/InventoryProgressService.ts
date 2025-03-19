e2b3c360c86e3d3727622693fd759c039e39b38a
Milan Gruner <milan@openearth.org>
2025-03-19 10:19:08 +0100

    feat(api): allow editing notation keys after save with finished subsector

--- app/src/backend/InventoryProgressService.ts
+++ app/src/backend/InventoryProgressService.ts
@@ -162,26 +162,28 @@ export default class InventoryProgressService {
           inventoryTypeSubCategoryCount,
         ); // TODO remove this when scope 3 is added back for SECTOR 1 and 2 in BASIC+;
         let completedCount = 0;
+        let unavailableReasons: string[] = [];
         if (inventoryValues?.length > 0) {
-          completedCount = inventoryValues.filter(
+          const currentSubSectorValues = inventoryValues.filter(
             (inventoryValue) =>
               inventoryValue.subSectorId === subSector.subsectorId,
-          ).length;
-          completed = completedCount === totalCount;
+          );
+          unavailableReasons = currentSubSectorValues
+            .map((inventoryValue) => inventoryValue.unavailableReason!)
+            .filter((reason) => !!reason);
+          completed = completedCount === currentSubSectorValues.length;
         }
         return {
           completed,
           completedCount,
           totalCount,
-          ...{
-            sectorId: subSector.sectorId, // optional string defaults to empty string
-            referenceNumber: subSector.referenceNumber, // optional string defaults to empty string
-            scopeId: subSector.scopeId,
-            subsectorId: subSector.subsectorId,
-            subsectorName: subSector.subsectorName,
-            subCategories: subSector.subCategories,
-          },
-          // ...subSector,
+          unavailableReasons,
+          sectorId: subSector.sectorId, // optional string defaults to empty string
+          referenceNumber: subSector.referenceNumber, // optional string defaults to empty string
+          scopeId: subSector.scopeId,
+          subsectorId: subSector.subsectorId,
+          subsectorName: subSector.subsectorName,
+          subCategories: subSector.subCategories,
         };
       });
 

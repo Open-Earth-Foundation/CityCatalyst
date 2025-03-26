b0a69ebcce395517ddd1e02e7d5340ff8bf378fe
Mirco Rudolph <mirco@openearth.org>
2025-03-26 14:56:28 +0100

    chore: added translation to target language

--- agents/agent_combine.py
+++ agents/agent_combine.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from styles.styles import styles_block
 import markdown
 
-OUTPUT_PATH = Path(__file__).parent.parent / "data" / "output"
+# OUTPUT_PATH = Path(__file__).parent.parent / "data" / "output"
 
 
 def custom_agent_combine(state: AgentState) -> AgentState:
@@ -12,8 +12,8 @@ def custom_agent_combine(state: AgentState) -> AgentState:
     print("Finalizing outputs...")
 
     # Get meta data
-    climate_action_id = state["climate_action_data"]["ActionID"]
-    city_locode = state["city_data"]["locode"]
+    # climate_action_id = state["climate_action_data"]["ActionID"]
+    # city_locode = state["city_data"]["locode"]
 
     # Get individual responses from the agents
     response_agent_1 = state["response_agent_1"].content
@@ -48,31 +48,31 @@ def custom_agent_combine(state: AgentState) -> AgentState:
     # Store the combined Markdown response under a new key
     result_state["response_agent_combine"] = combined_markdown
 
-    # Convert Markdown to HTML
-    html_content = markdown.markdown(combined_markdown, extensions=["extra"])
+    # # Convert Markdown to HTML
+    # html_content = markdown.markdown(combined_markdown, extensions=["extra"])
 
-    # File output
-    OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
+    # # File output
+    # OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
 
-    # Get current date/time in hh:mm format
-    current_time = datetime.now().strftime("%Y%m%d_%H%M")
+    # # Get current date/time in hh:mm format
+    # current_time = datetime.now().strftime("%Y%m%d_%H%M")
 
-    file_name = (
-        f"{current_time}_{city_locode}_{climate_action_id}_implementation_plan.md"
-    )
-    file_name_html = (
-        f"{current_time}_{city_locode}_{climate_action_id}_implementation_plan.html"
-    )
+    # file_name = (
+    #     f"{current_time}_{city_locode}_{climate_action_id}_implementation_plan.md"
+    # )
+    # file_name_html = (
+    #     f"{current_time}_{city_locode}_{climate_action_id}_implementation_plan.html"
+    # )
 
-    # Write the combined Markdown text to a local file
-    with open(OUTPUT_PATH / file_name, "w", encoding="utf-8") as md_file:
-        md_file.write(combined_markdown)
+    # # Write the combined Markdown text to a local file
+    # with open(OUTPUT_PATH / file_name, "w", encoding="utf-8") as md_file:
+    #     md_file.write(combined_markdown)
 
-    # Write the html to a local file
-    with open(OUTPUT_PATH / file_name_html, "w", encoding="utf-8") as md_file:
-        md_file.write(html_content)
+    # # Write the html to a local file
+    # with open(OUTPUT_PATH / file_name_html, "w", encoding="utf-8") as md_file:
+    #     md_file.write(html_content)
 
-    print("Outputs finalized\n")
+    print("Outputs combined\n")
 
     # Return an AgentState with the updated responses
     return AgentState(**result_state)

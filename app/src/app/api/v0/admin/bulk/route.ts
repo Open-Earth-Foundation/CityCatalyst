e20753542d2b4202e379fa903273e2c8a2ad3b16
Milan Gruner <lemilonkh@users.noreply.github.com>
2025-03-27 17:00:21 +0100

    feat(api): add missing city data, add update inventories route (#1254)
    * feat(api): implement update-inventories admin route add missing data to city
    
    * fix(api): update comments for route body validators of admin routes
    
    * fix(api): remove commented code from AdminService
    
    * fix(api): resolve PR feedback for update inventories route
    
    * fix(api): correctly extract region/ country names and locodes from OpenClimate
    
    * feat(api): return list of errors from AdminService.bulkUpdateInventories

--- app/src/app/api/v0/admin/bulk/route.ts
+++ app/src/app/api/v0/admin/bulk/route.ts
@@ -6,7 +6,7 @@ import { z } from "zod";
 const createBulkInventoriesRequest = z.object({
   cityLocodes: z.array(z.string()), // List of city locodes
   emails: z.array(z.string().email()), // Comma separated list of emails to invite to the all of the created inventories
-  years: z.array(z.number().int().positive()), // List of years to create inventories for (can be comma separated input, multiple select dropdown etc., so multiple years can be chosen)
+  years: z.array(z.number().int().positive()), // List of years to create inventories for
   scope: z.enum(["gpc_basic", "gpc_basic_plus"]), // Scope selection (gpc_basic or gpc_basic_plus)
   gwp: z.enum(["AR5", "AR6"]), // GWP selection (AR5 or AR6)
 });

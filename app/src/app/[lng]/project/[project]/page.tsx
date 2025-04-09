1c288e7ce6d7c7d61a8e19a0f2ac1d401a875a48
Milan Gruner <milangruner@gmail.com>
2025-04-09 20:14:11 +0200

    feat(ui): add ProjectMap component to project page

--- app/src/app/[lng]/project/[project]/page.tsx
+++ app/src/app/[lng]/project/[project]/page.tsx
@@ -1,3 +1,9 @@
-export default function ProjectPage() {
-  return "Hello world";
+import ProjectMap from "./ProjectMap";
+
+export default function ProjectPage({
+  params: { project, lng },
+}: {
+  params: { project: string; lng: string };
+}) {
+  return <ProjectMap height={800} width={800} projectId={project} />;
 }

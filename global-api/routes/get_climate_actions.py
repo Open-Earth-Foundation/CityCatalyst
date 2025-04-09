25b53e2bc9e926669c5c6cf35c990efd2cd978f4
Mirco Rudolph <mirco@openearth.org>
2025-04-09 15:52:35 -0300

    chore: updated formatting of response object

--- global-api/routes/get_climate_actions.py
+++ global-api/routes/get_climate_actions.py
@@ -11,37 +11,42 @@ def db_climate_actions() -> List[Dict[str, Any]]:
         query = text(
             """
             SELECT 
-                action_id as "ActionID",
-                action_name as "ActionName",
-                action_type as "ActionType",
-                hazard as "Hazard",
-                sector as "Sector",
-                subsector as "Subsector",
-                primary_purpose as "PrimaryPurpose",
-                description as "Description",
-                -- CoBenefits columns
-                co_benefits_air_quality as "CoBenefits_air_quality",
-                co_benefits_water_quality as "CoBenefits_water_quality",
-                co_benefits_habitat as "CoBenefits_habitat",
-                co_benefits_cost_of_living as "CoBenefits_cost_of_living",
-                co_benefits_housing as "CoBenefits_housing",
-                co_benefits_mobility as "CoBenefits_mobility",
-                co_benefits_stakeholder_engagement as "CoBenefits_stakeholder_engagement",
-                equity_and_inclusion_considerations as "EquityAndInclusionConsiderations",
-                -- GHG Reduction Potential columns
-                ghg_reduction_potential_stationary_energy as "GHGReductionPotential_stationary_energy",
-                ghg_reduction_potential_transportation as "GHGReductionPotential_transportation",
-                ghg_reduction_potential_waste as "GHGReductionPotential_waste",
-                ghg_reduction_potential_ippu as "GHGReductionPotential_ippu",
-                ghg_reduction_potential_afolu as "GHGReductionPotential_afolu",
-                adaptation_effectiveness as "AdaptationEffectiveness",
-                cost_investment_needed as "CostInvestmentNeeded",
-                timeline_for_implementation as "TimelineForImplementation",
-                dependencies as "Dependencies",
-                key_performance_indicators as "KeyPerformanceIndicators",
-                powers_and_mandates as "PowersAndMandates",
-                adaptation_effectiveness_per_hazard as "AdaptationEffectivenessPerHazard",
-                biome as "biome"
+                ActionID,
+                ActionName,
+                ActionType,
+                Hazard,
+                Sector,
+                Subsector,
+                PrimaryPurpose,
+                Description,
+                CoBenefitsAirQuality,
+                CoBenefitsWaterQuality,
+                CoBenefitsHabitat,
+                CoBenefitsCostOfLiving,
+                CoBenefitsHousing,
+                CoBenefitsMobility,
+                CoBenefitsStakeholderEngagement,
+                EquityAndInclusionConsiderations,
+                GHGReductionPotentialStationaryEnergy,
+                GHGReductionPotentialTransportation,
+                GHGReductionPotentialWaste,
+                GHGReductionPotentialIPPU,
+                GHGReductionPotentialAFOLU,
+                AdaptationEffectiveness,
+                AdaptationEffectivenessDroughts,
+                AdaptationEffectivenessHeatwaves,
+                AdaptationEffectivenessFloods,
+                AdaptationEffectivenessSeaLevelRise,
+                AdaptationEffectivenessLandslides,
+                AdaptationEffectivenessStorms,
+                AdaptationEffectivenessWildfires,
+                AdaptationEffectivenessDiseases,
+                CostInvestmentNeeded,
+                TimelineForImplementation,
+                Dependencies,
+                KeyPerformanceIndicators,
+                PowersAndMandates,
+                Biome
             FROM climate_action
         """
         )
@@ -55,14 +60,14 @@ def db_climate_actions() -> List[Dict[str, Any]]:
 
             # Construct CoBenefits dictionary
             co_benefits = {
-                "air_quality": action_dict.pop("CoBenefits_air_quality"),
-                "water_quality": action_dict.pop("CoBenefits_water_quality"),
-                "habitat": action_dict.pop("CoBenefits_habitat"),
-                "cost_of_living": action_dict.pop("CoBenefits_cost_of_living"),
-                "housing": action_dict.pop("CoBenefits_housing"),
-                "mobility": action_dict.pop("CoBenefits_mobility"),
+                "air_quality": action_dict.pop("CoBenefitsAirQuality"),
+                "water_quality": action_dict.pop("CoBenefitsWaterQuality"),
+                "habitat": action_dict.pop("CoBenefitsHabitat"),
+                "cost_of_living": action_dict.pop("CoBenefitsCostOfLiving"),
+                "housing": action_dict.pop("CoBenefitsHousing"),
+                "mobility": action_dict.pop("CoBenefitsMobility"),
                 "stakeholder_engagement": action_dict.pop(
-                    "CoBenefits_stakeholder_engagement"
+                    "CoBenefitsStakeholderEngagement"
                 ),
             }
             action_dict["CoBenefits"] = co_benefits
@@ -70,16 +75,33 @@ def db_climate_actions() -> List[Dict[str, Any]]:
             # Construct GHGReductionPotential dictionary
             ghg_reduction = {
                 "stationary_energy": action_dict.pop(
-                    "GHGReductionPotential_stationary_energy"
+                    "GHGReductionPotentialStationaryEnergy"
                 ),
                 "transportation": action_dict.pop(
-                    "GHGReductionPotential_transportation"
+                    "GHGReductionPotentialTransportation"
                 ),
-                "waste": action_dict.pop("GHGReductionPotential_waste"),
-                "ippu": action_dict.pop("GHGReductionPotential_ippu"),
-                "afolu": action_dict.pop("GHGReductionPotential_afolu"),
+                "waste": action_dict.pop("GHGReductionPotentialWaste"),
+                "ippu": action_dict.pop("GHGReductionPotentialIPPU"),
+                "afolu": action_dict.pop("GHGReductionPotentialAFOLU"),
             }
             action_dict["GHGReductionPotential"] = ghg_reduction
+
+            # Construct AdaptationEffectivenessPerHazard dictionary
+            adaptation_effectiveness_per_hazard = {
+                "droughts": action_dict.pop("AdaptationEffectivenessDroughts"),
+                "heatwaves": action_dict.pop("AdaptationEffectivenessHeatwaves"),
+                "floods": action_dict.pop("AdaptationEffectivenessFloods"),
+                "sea-level-rise": action_dict.pop(
+                    "AdaptationEffectivenessSeaLevelRise"
+                ),
+                "landslides": action_dict.pop("AdaptationEffectivenessLandslides"),
+                "storms": action_dict.pop("AdaptationEffectivenessStorms"),
+                "wildfires": action_dict.pop("AdaptationEffectivenessWildfires"),
+                "diseases": action_dict.pop("AdaptationEffectivenessDiseases"),
+            }
+            action_dict["AdaptationEffectivenessPerHazard"] = (
+                adaptation_effectiveness_per_hazard
+            )
 
             actions.append(action_dict)
 

f709aaa4cff17e2d7a8cc6ff8086c4f8a7d92414
Evan Prodromou <evan@openearth.org>
2025-04-09 15:07:10 -0300

    fix: add missing columns

--- global-api/migrations/versions/df2243cdb86d_create_climate_actions_table.py
+++ global-api/migrations/versions/df2243cdb86d_create_climate_actions_table.py
@@ -20,58 +20,150 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     op.create_table(
-        'climate_action',
-        sa.Column('ActionID', sa.String(length=255), primary_key=True),
-        sa.Column('ActionName', sa.String(length=255), nullable=False),
-        sa.Column('ActionType', ARRAY(sa.String), nullable=False),
-        sa.Column('Hazard', ARRAY(sa.String), nullable=True),
-        sa.Column('Sector', ARRAY(sa.String), nullable=True),
-        sa.Column('Subsector', ARRAY(sa.String), nullable=True),
-        sa.Column('PrimaryPurpose', ARRAY(sa.String), nullable=False),
-        sa.Column('Description', sa.Text, nullable=True),
-        sa.Column('CoBenefitsAirQuality', sa.Integer, nullable=True),
-        sa.Column('CoBenefitsWaterQuality', sa.Integer, nullable=True),
-        sa.Column('CoBenefitsHabitat', sa.Integer, nullable=True),
-        sa.Column('CoBenefitsCostOfLiving', sa.Integer, nullable=True),
-        sa.Column('CoBenefitsHousing', sa.Integer, nullable=True),
-        sa.Column('CoBenefitsMobility', sa.Integer, nullable=True),
-        sa.Column('CoBenefitsStakeholderEngagement', sa.Integer, nullable=True),
-        sa.Column('EquityAndInclusionConsiderations', sa.Text, nullable=True),
-        sa.Column('GHGReductionPotentialStationaryEnergy', sa.String, nullable=True),
-        sa.Column('GHGReductionPotentialTransportation', sa.String, nullable=True),
-        sa.Column('GHGReductionPotentialWaste', sa.String, nullable=True),
-        sa.Column('GHGReductionPotentialIPPU', sa.String, nullable=True),
-        sa.Column('GHGReductionPotentialAFOLU', sa.String, nullable=True),
-        sa.Column('AdaptationEffectiveness', sa.String, nullable=True),
-        sa.Column('CostInvestmentNeeded', sa.String, nullable=True),
-        sa.Column('TimelineForImplementation', sa.String, nullable=True),
-        sa.Column('Dependencies', ARRAY(sa.String), nullable=True),
-        sa.Column('KeyPerformanceIndicators', ARRAY(sa.String), nullable=True),
-        sa.Column('PowersAndMandates', ARRAY(sa.String), nullable=True),
-
-        sa.CheckConstraint('"CoBenefitsAirQuality" >= -2 AND "CoBenefitsAirQuality" <= 2', 'check_cobenefits_air_quality'),
-        sa.CheckConstraint('"CoBenefitsWaterQuality" >= -2 AND "CoBenefitsWaterQuality" <= 2', 'check_cobenefits_water_quality'),
-        sa.CheckConstraint('"CoBenefitsHabitat" >= -2 AND "CoBenefitsHabitat" <= 2', 'check_cobenefits_habitat'),
-        sa.CheckConstraint('"CoBenefitsCostOfLiving" >= -2 AND "CoBenefitsCostOfLiving" <= 2', 'check_cobenefits_cost_of_living'),
-        sa.CheckConstraint('"CoBenefitsHousing" >= -2 AND "CoBenefitsHousing" <= 2', 'check_cobenefits_housing'),
-        sa.CheckConstraint('"CoBenefitsMobility" >= -2 AND "CoBenefitsMobility" <= 2', 'check_cobenefits_mobility'),
-        sa.CheckConstraint('"CoBenefitsStakeholderEngagement" >= -2 AND "CoBenefitsStakeholderEngagement" <= 2', 'check_cobenefits_stakeholder_engagement'),
-
-        sa.CheckConstraint('"GHGReductionPotentialStationaryEnergy" IS NULL or "GHGReductionPotentialStationaryEnergy" IN (\'0-19\', \'20-39\', \'40-59\', \'60-79\', \'80-100\')', 'check_ghg_reduction_potential_stationary_energy'),
-
-        sa.CheckConstraint('"GHGReductionPotentialTransportation" IS NULL or "GHGReductionPotentialTransportation" IN (\'0-19\', \'20-39\', \'40-59\', \'60-79\', \'80-100\')', 'check_ghg_reduction_potential_transportation'),
-
-        sa.CheckConstraint('"GHGReductionPotentialWaste" IS NULL or "GHGReductionPotentialWaste" IN (\'0-19\', \'20-39\', \'40-59\', \'60-79\', \'80-100\')', 'check_ghg_reduction_potential_waste'),
-
-        sa.CheckConstraint('"GHGReductionPotentialIPPU" IS NULL or "GHGReductionPotentialIPPU" IN (\'0-19\', \'20-39\', \'40-59\', \'60-79\', \'80-100\')', 'check_ghg_reduction_potential_ippu'),
-
-        sa.CheckConstraint('"GHGReductionPotentialAFOLU" IS NULL or "GHGReductionPotentialStationaryEnergy" IN (\'0-19\', \'20-39\', \'40-59\', \'60-79\', \'80-100\')', 'check_ghg_reduction_potential_afolu'),
-
-        sa.CheckConstraint('"AdaptationEffectiveness" IS NULL OR "AdaptationEffectiveness" IN (\'high\', \'medium\', \'low\')', 'check_adaptation_effectiveness'),
-
-        sa.CheckConstraint('"CostInvestmentNeeded" IS NULL OR "CostInvestmentNeeded" IN (\'high\', \'medium\', \'low\')', 'check_cost_investment_needed'),
-
-        sa.CheckConstraint('"TimelineForImplementation" IS NULL OR "TimelineForImplementation" IN (\'<5 years\', \'5-10 years\', \'>10 years\')', 'check_timeline_for_implementation'),
+        "climate_action",
+        sa.Column("ActionID", sa.String(length=255), primary_key=True),
+        sa.Column("ActionName", sa.String(length=255), nullable=False),
+        sa.Column("ActionType", ARRAY(sa.String), nullable=False),
+        sa.Column("Hazard", ARRAY(sa.String), nullable=True),
+        sa.Column("Sector", ARRAY(sa.String), nullable=True),
+        sa.Column("Subsector", ARRAY(sa.String), nullable=True),
+        sa.Column("PrimaryPurpose", ARRAY(sa.String), nullable=False),
+        sa.Column("Description", sa.Text, nullable=True),
+        sa.Column("CoBenefitsAirQuality", sa.Integer, nullable=True),
+        sa.Column("CoBenefitsWaterQuality", sa.Integer, nullable=True),
+        sa.Column("CoBenefitsHabitat", sa.Integer, nullable=True),
+        sa.Column("CoBenefitsCostOfLiving", sa.Integer, nullable=True),
+        sa.Column("CoBenefitsHousing", sa.Integer, nullable=True),
+        sa.Column("CoBenefitsMobility", sa.Integer, nullable=True),
+        sa.Column("CoBenefitsStakeholderEngagement", sa.Integer, nullable=True),
+        sa.Column("EquityAndInclusionConsiderations", sa.Text, nullable=True),
+        sa.Column("GHGReductionPotentialStationaryEnergy", sa.String, nullable=True),
+        sa.Column("GHGReductionPotentialTransportation", sa.String, nullable=True),
+        sa.Column("GHGReductionPotentialWaste", sa.String, nullable=True),
+        sa.Column("GHGReductionPotentialIPPU", sa.String, nullable=True),
+        sa.Column("GHGReductionPotentialAFOLU", sa.String, nullable=True),
+        sa.Column("AdaptationEffectiveness", sa.String, nullable=True),
+        sa.Column("AdaptationEffectivenessDroughts", sa.String, nullable=True),
+        sa.Column("AdaptationEffectivenessHeatwaves", sa.String, nullable=True),
+        sa.Column("AdaptationEffectivenessFloods", sa.String, nullable=True),
+        sa.Column("AdaptationEffectivenessSeaLevelRise", sa.String, nullable=True),
+        sa.Column("AdaptationEffectivenessLandslides", sa.String, nullable=True),
+        sa.Column("AdaptationEffectivenessStorms", sa.String, nullable=True),
+        sa.Column("AdaptationEffectivenessWildfires", sa.String, nullable=True),
+        sa.Column("AdaptationEffectivenessDiseases", sa.String, nullable=True),
+        sa.Column("CostInvestmentNeeded", sa.String, nullable=True),
+        sa.Column("TimelineForImplementation", sa.String, nullable=True),
+        sa.Column("Dependencies", ARRAY(sa.String), nullable=True),
+        sa.Column("KeyPerformanceIndicators", ARRAY(sa.String), nullable=True),
+        sa.Column("PowersAndMandates", ARRAY(sa.String), nullable=True),
+        sa.Column("Biome", sa.String, nullable=True),
+        sa.CheckConstraint(
+            '"CoBenefitsAirQuality" >= -2 AND "CoBenefitsAirQuality" <= 2',
+            "check_cobenefits_air_quality",
+        ),
+        sa.CheckConstraint(
+            '"CoBenefitsWaterQuality" >= -2 AND "CoBenefitsWaterQuality" <= 2',
+            "check_cobenefits_water_quality",
+        ),
+        sa.CheckConstraint(
+            '"CoBenefitsHabitat" >= -2 AND "CoBenefitsHabitat" <= 2',
+            "check_cobenefits_habitat",
+        ),
+        sa.CheckConstraint(
+            '"CoBenefitsCostOfLiving" >= -2 AND "CoBenefitsCostOfLiving" <= 2',
+            "check_cobenefits_cost_of_living",
+        ),
+        sa.CheckConstraint(
+            '"CoBenefitsHousing" >= -2 AND "CoBenefitsHousing" <= 2',
+            "check_cobenefits_housing",
+        ),
+        sa.CheckConstraint(
+            '"CoBenefitsMobility" >= -2 AND "CoBenefitsMobility" <= 2',
+            "check_cobenefits_mobility",
+        ),
+        sa.CheckConstraint(
+            '"CoBenefitsStakeholderEngagement" >= -2 AND "CoBenefitsStakeholderEngagement" <= 2',
+            "check_cobenefits_stakeholder_engagement",
+        ),
+        sa.CheckConstraint(
+            "\"GHGReductionPotentialStationaryEnergy\" IS NULL or \"GHGReductionPotentialStationaryEnergy\" IN ('0-19', '20-39', '40-59', '60-79', '80-100')",
+            "check_ghg_reduction_potential_stationary_energy",
+        ),
+        sa.CheckConstraint(
+            "\"GHGReductionPotentialTransportation\" IS NULL or \"GHGReductionPotentialTransportation\" IN ('0-19', '20-39', '40-59', '60-79', '80-100')",
+            "check_ghg_reduction_potential_transportation",
+        ),
+        sa.CheckConstraint(
+            "\"GHGReductionPotentialWaste\" IS NULL or \"GHGReductionPotentialWaste\" IN ('0-19', '20-39', '40-59', '60-79', '80-100')",
+            "check_ghg_reduction_potential_waste",
+        ),
+        sa.CheckConstraint(
+            "\"GHGReductionPotentialIPPU\" IS NULL or \"GHGReductionPotentialIPPU\" IN ('0-19', '20-39', '40-59', '60-79', '80-100')",
+            "check_ghg_reduction_potential_ippu",
+        ),
+        sa.CheckConstraint(
+            "\"GHGReductionPotentialAFOLU\" IS NULL or \"GHGReductionPotentialStationaryEnergy\" IN ('0-19', '20-39', '40-59', '60-79', '80-100')",
+            "check_ghg_reduction_potential_afolu",
+        ),
+        sa.CheckConstraint(
+            "\"AdaptationEffectiveness\" IS NULL OR \"AdaptationEffectiveness\" IN ('high', 'medium', 'low')",
+            "check_adaptation_effectiveness",
+        ),
+        sa.CheckConstraint(
+            "\"AdaptationEffectivenessDroughts\" IS NULL OR \"AdaptationEffectivenessDroughts\" IN ('high', 'medium', 'low')",
+            "check_adaptation_effectiveness_droughts",
+        ),
+        sa.CheckConstraint(
+            "\"AdaptationEffectivenessHeatwaves\" IS NULL OR \"AdaptationEffectivenessHeatwaves\" IN ('high', 'medium', 'low')",
+            "check_adaptation_effectiveness_heatwaves",
+        ),
+        sa.CheckConstraint(
+            "\"AdaptationEffectivenessFloods\" IS NULL OR \"AdaptationEffectivenessFloods\" IN ('high', 'medium', 'low')",
+            "check_adaptation_effectiveness_floods",
+        ),
+        sa.CheckConstraint(
+            "\"AdaptationEffectivenessSeaLevelRise\" IS NULL OR \"AdaptationEffectivenessSeaLevelRise\" IN ('high', 'medium', 'low')",
+            "check_adaptation_effectiveness_sea_level_rise",
+        ),
+        sa.CheckConstraint(
+            "\"AdaptationEffectivenessLandslides\" IS NULL OR \"AdaptationEffectivenessLandslides\" IN ('high', 'medium', 'low')",
+            "check_adaptation_effectiveness_landslides",
+        ),
+        sa.CheckConstraint(
+            "\"AdaptationEffectivenessStorms\" IS NULL OR \"AdaptationEffectivenessStorms\" IN ('high', 'medium', 'low')",
+            "check_adaptation_effectiveness_storms",
+        ),
+        sa.CheckConstraint(
+            "\"AdaptationEffectivenessWildfires\" IS NULL OR \"AdaptationEffectivenessWildfires\" IN ('high', 'medium', 'low')",
+            "check_adaptation_effectiveness_wildfires",
+        ),
+        sa.CheckConstraint(
+            "\"AdaptationEffectivenessDiseases\" IS NULL OR \"AdaptationEffectivenessDiseases\" IN ('high', 'medium', 'low')",
+            "check_adaptation_effectiveness_diseases",
+        ),
+        sa.CheckConstraint(
+            "\"CostInvestmentNeeded\" IS NULL OR \"CostInvestmentNeeded\" IN ('high', 'medium', 'low')",
+            "check_cost_investment_needed",
+        ),
+        sa.CheckConstraint(
+            "\"TimelineForImplementation\" IS NULL OR \"TimelineForImplementation\" IN ('<5 years', '5-10 years', '>10 years')",
+            "check_timeline_for_implementation",
+        ),
+        sa.CheckConstraint(
+            """
+            "Biome" is NULL or "Biome" in (
+            'none',
+            'tropical_rainforest',
+            'temperate_forest',
+            'desert',
+            'grassland_savanna',
+            'tundra',
+            'wetlands',
+            'mountains',
+            'boreal_forest_taiga',
+            'coastal_marine')
+            """
+        ),
     )
     op.execute(
         """

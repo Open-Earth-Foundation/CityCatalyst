b692d4d7ba2520705797b01573cbb39771a8aa6b
Marina Olivella <marina@openearth.org>
2025-04-16 12:32:22 -0300

    feat (cap) [ON-3750] CAP tab feature flag

--- app/src/components/HomePage/HomePage.tsx
+++ app/src/components/HomePage/HomePage.tsx
@@ -28,6 +28,7 @@ import {
   ProgressCircleRoot,
 } from "@/components/ui/progress-circle";
 import CapTab from "@/app/[lng]/[inventory]/CapTab";
+import { hasFeatureFlag } from "@/util/feature-flags";
 
 export default function HomePage({
   lng,
@@ -204,7 +205,7 @@ export default function HomePage({
                       {[
                         "tab-emission-inventory-calculation-title",
                         "tab-emission-inventory-results-title",
-                        ...(inventory?.city?.country === "Brazil" ? ["tab-cap-title"] : []),
+                        ...(inventory?.city?.country === "Brazil" && hasFeatureFlag('CAP_TAB_ENABLED') ? ["tab-cap-title"] : []),
                       ].map((tab, index) => (
                         <Tabs.Trigger key={index} value={tab}>
                           <Text

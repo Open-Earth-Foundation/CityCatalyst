b30a64e0bf85ca7024323ce30f72bb921d4fd774
Milan Gruner <milan@openearth.org>
2025-04-16 13:49:44 +0200

    feat(api): allow defining and updating the project ID of all referenced cities

--- app/src/backend/AdminService.ts
+++ app/src/backend/AdminService.ts
@@ -20,12 +20,14 @@ export interface BulkInventoryCreateProps {
   years: number[]; // List of years to create inventories for
   scope: "gpc_basic" | "gpc_basic_plus"; // Scope selection (gpc_basic or gpc_basic_plus)
   gwp: "AR5" | "AR6" | "ar5" | "ar6"; // global warming potential standard selection
+  projectId: string; // project ID to associate with the inventories
 }
 
 export interface BulkInventoryUpdateProps {
   userEmail: string; // Email of the user whose inventories are to be connected
   cityLocodes: string[]; // List of city locodes
   years: number[]; // List of years to create inventories for
+  projectId?: string;
 }
 
 export interface CreateBulkInventoriesResponse {
@@ -71,7 +73,7 @@ export default class AdminService {
         cityId: randomUUID(),
         locode: cityLocode,
         name: cityName,
-        projectId: DEFAULT_PROJECT_ID,
+        projectId: props.projectId ?? DEFAULT_PROJECT_ID,
       });
 
       // add users to the city
@@ -117,6 +119,7 @@ export default class AdminService {
           cityLocode,
           inventory.year,
           city.cityId,
+          props.projectId,
         );
         errors.push(...populationErrors);
       }
@@ -156,11 +159,19 @@ export default class AdminService {
       ],
     });
 
+    const updateableCityIds: string[] = [];
     for (const inventory of inventories) {
       if (!inventory.city?.locode) {
         throw new createHttpError.NotFound(
           "City or locode not found for inventory " + inventory.inventoryId,
         );
+      }
+
+      if (
+        !updateableCityIds.includes(inventory.city.cityId) &&
+        inventory.city.projectId !== props.projectId
+      ) {
+        updateableCityIds.push(inventory.city.cityId);
       }
 
       // Connect all data sources, rank them by priority, check if they connect
@@ -170,6 +181,11 @@ export default class AdminService {
       );
       errors.push(...sourceErrors);
     }
+
+    await db.models.City.update(
+      { projectId: props.projectId },
+      { where: { cityId: { [Op.in]: updateableCityIds } } },
+    );
 
     return { errors };
   }
@@ -191,7 +207,7 @@ export default class AdminService {
   }
 
   public static async bulkUpdateInventories(
-    { cityLocodes, userEmail, years }: BulkInventoryUpdateProps,
+    { cityLocodes, userEmail, years, projectId }: BulkInventoryUpdateProps,
     session: AppSession | null,
   ) {
     this.ensureIsAdmin(session);
@@ -217,6 +233,7 @@ export default class AdminService {
           locode,
           year,
           city?.cityId,
+          projectId,
         );
         errors.push(...newErrors);
       }
@@ -229,6 +246,7 @@ export default class AdminService {
     cityLocode: string,
     inventoryYear: number,
     cityId: string,
+    projectId?: string,
   ) {
     const errors: { locode: string; error: string }[] = [];
 
@@ -284,7 +302,7 @@ export default class AdminService {
         country,
         countryLocode,
         area: area ? Math.round(area) : undefined,
-        projectId: DEFAULT_PROJECT_ID,
+        projectId: projectId ?? undefined,
       },
       { where: { cityId } },
     );

54f684ef0cc06185dc4a02f88022643002718c84
Milan Gruner <milan@openearth.org>
2025-04-16 17:12:37 +0200

    fix(db): create migration to re-run changes from OrganizationInvite migration
    To make sure they are executed on the servers

---
+++ app/migrations/20250416145927-organization-invite-rename-timestamps.cjs
@@ -0,0 +1,31 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // the previous migration was edited after being deployed, so make sure this column is renamed
+    await queryInterface.renameColumn(
+      "OrganizationInvite",
+      "created_at",
+      "created",
+    );
+    await queryInterface.renameColumn(
+      "OrganizationInvite",
+      "updated_at",
+      "last_updated",
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.renameColumn(
+      "OrganizationInvite",
+      "created",
+      "created_at",
+    );
+    await queryInterface.renameColumn(
+      "OrganizationInvite",
+      "last_updated",
+      "updated_at",
+    );
+  },
+};

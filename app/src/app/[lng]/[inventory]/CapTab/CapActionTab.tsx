ff49d0ff84cc4c664935fb7a16aecd1004c5652c
Marina Olivella <marina@openearth.org>
2025-04-17 15:23:16 -0300

    feat(cap)[ON-3749]: make AWS credentials accessible

--- app/src/app/[lng]/[inventory]/CapTab/CapActionTab.tsx
+++ app/src/app/[lng]/[inventory]/CapTab/CapActionTab.tsx
@@ -156,7 +156,7 @@ export function CapActionTab({
   ];
 
   const table = useReactTable({
-    data: actions || [],
+    data: Array.isArray(actions) ? actions : [],
     columns,
     getCoreRowModel: getCoreRowModel(),
   });

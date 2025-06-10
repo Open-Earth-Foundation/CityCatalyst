ec4f828eff602ae06507de3298edc9bfd44dde0c
Milan Gruner <milan@openearth.org>
2025-06-10 15:58:37 +0200

    feat(ui): add swagger getApiDocs configuration method using next-swagger-doc

---
+++ app/src/lib/swagger.ts
@@ -0,0 +1,25 @@
+import { createSwaggerSpec } from "next-swagger-doc";
+
+export const getApiDocs = async () => {
+  const spec = createSwaggerSpec({
+    apiFolder: "app/api", // define api folder under app folder
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "CityCatalyst API",
+        version: "0.1.0",
+      },
+      components: {
+        securitySchemes: {
+          BearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
+        },
+      },
+      security: [],
+    },
+  });
+  return spec;
+};

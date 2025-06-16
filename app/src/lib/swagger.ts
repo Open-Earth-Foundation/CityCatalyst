75fe15be9f66d16abee6ac633c193bb5465a1bca
Milan Gruner <milangruner@gmail.com>
2025-06-16 20:07:23 +0200

    fix(ui): adjust apiFolder config for createSwaggerSpec to point to right path

--- app/src/lib/swagger.ts
+++ app/src/lib/swagger.ts
@@ -2,7 +2,7 @@ import { createSwaggerSpec } from "next-swagger-doc";
 
 export const getApiDocs = async () => {
   const spec = createSwaggerSpec({
-    apiFolder: "app/api", // define api folder under app folder
+    apiFolder: "src/app/api",
     definition: {
       openapi: "3.0.0",
       info: {

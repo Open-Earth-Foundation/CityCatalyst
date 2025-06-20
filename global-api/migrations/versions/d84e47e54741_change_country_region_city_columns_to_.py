63c5278967d9edf321aacc911c2d810e0b9f21e7
Amanda Eames <amanda@openearth.org>
2025-06-20 13:42:24 +0100

    fix: create table for city polygon

--- global-api/migrations/versions/d84e47e54741_change_country_region_city_columns_to_.py
+++ global-api/migrations/versions/d84e47e54741_change_country_region_city_columns_to_.py
@@ -1,4 +1,4 @@
-"""change_country_region_city_columns_to_varchar
+"""create_city_polygon_table
 
 Revision ID: d84e47e54741
 Revises: e3c866a57c19
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = 'd84e47e54741'
@@ -19,40 +19,27 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    # Change country_code column to VARCHAR
-    op.alter_column('modelled.city_polygon', 'country_code',
-                    existing_type=sa.TEXT(),
-                    type_=sa.VARCHAR(),
-                    existing_nullable=True)
-    
-    # Change region_code column to VARCHAR
-    op.alter_column('modelled.city_polygon', 'region_code',
-                    existing_type=sa.TEXT(),
-                    type_=sa.VARCHAR(),
-                    existing_nullable=True)
-    
-    # Change city_id column to VARCHAR
-    op.alter_column('modelled.city_polygon', 'city_id',
-                    existing_type=sa.TEXT(),
-                    type_=sa.VARCHAR(),
-                    existing_nullable=True)
+    # Create city_polygon table
+    op.create_table('city_polygon',
+        sa.Column('city_id', sa.VARCHAR(), nullable=True),
+        sa.Column('city_name', sa.VARCHAR(), nullable=False),
+        sa.Column('city_type', sa.VARCHAR(), nullable=True),
+        sa.Column('country_code', sa.VARCHAR(), nullable=False),
+        sa.Column('region_code', sa.VARCHAR(), nullable=True),
+        sa.Column('locode', sa.CHAR(), nullable=False),
+        sa.Column('osm_id', sa.INTEGER(), nullable=True),
+        sa.Column('geometry', sa.Text(), nullable=False),
+        sa.Column('lat', sa.DOUBLE_PRECISION(), nullable=False),
+        sa.Column('lon', sa.DOUBLE_PRECISION(), nullable=False),
+        sa.Column('bbox_north', sa.DOUBLE_PRECISION(), nullable=False),
+        sa.Column('bbox_south', sa.DOUBLE_PRECISION(), nullable=False),
+        sa.Column('bbox_east', sa.DOUBLE_PRECISION(), nullable=False),
+        sa.Column('bbox_west', sa.DOUBLE_PRECISION(), nullable=False),
+        sa.PrimaryKeyConstraint('locode'),
+        schema='modelled'
+    )
 
 
 def downgrade() -> None:
-    # Revert country_code column back to TEXT
-    op.alter_column('modelled.city_polygon', 'country_code',
-                    existing_type=sa.VARCHAR(),
-                    type_=sa.TEXT(),
-                    existing_nullable=True)
-    
-    # Revert region_code column back to TEXT
-    op.alter_column('modelled.city_polygon', 'region_code',
-                    existing_type=sa.VARCHAR(),
-                    type_=sa.TEXT(),
-                    existing_nullable=True)
-    
-    # Revert city_id column back to TEXT
-    op.alter_column('modelled.city_polygon', 'city_id',
-                    existing_type=sa.VARCHAR(),
-                    type_=sa.TEXT(),
-                    existing_nullable=True)
+    # Drop city_polygon table
+    op.drop_table('city_polygon', schema='modelled')

35f7a27fdb0611bb8749ecc4773996dc1395102d
Marina Olivella <marina@openearth.org>
2025-07-21 15:52:38 -0300

    feat [ON-4317] [ON-4318](JN) - db migrations

---
+++ app/migrations/20250721185038-Module_create.cjs
@@ -0,0 +1,31 @@
+"use strict";
+
+const sql_up = `
+    CREATE TABLE "Module" (
+        id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+        step TEXT NOT NULL,
+        name TEXT NOT NULL,
+        description TEXT,
+        type TEXT,
+        URL TEXT
+    );
+    CREATE INDEX idx_module_step ON "Module" (step);
+    CREATE INDEX idx_module_name ON "Module" (name);
+`;
+
+const sql_down = `
+    DROP INDEX IF EXISTS idx_module_step;
+    DROP INDEX IF EXISTS idx_module_name;
+    DROP TABLE IF EXISTS "Module";
+`;
+
+/** @type {import("sequelize-cli").Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(sql_up);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(sql_down);
+  },
+}; 

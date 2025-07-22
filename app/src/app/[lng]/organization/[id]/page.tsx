9338388fb5fdb2262c8e30318559c1301d0e5f44
Milan Gruner <milangruner@gmail.com>
2025-07-22 12:49:25 +0200

    feat(ui): add blog card and improve layout on organization page

--- app/src/app/[lng]/organization/[id]/page.tsx
+++ app/src/app/[lng]/organization/[id]/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 import { use } from "react";
 
-import { Box, Card, HStack, Icon, VStack } from "@chakra-ui/react";
+import { Button, Card, HStack, Icon, VStack } from "@chakra-ui/react";
 import { SimpleGrid } from "@chakra-ui/react";
 
 import { useTranslation } from "@/i18n/client";
 import { HeadlineLarge, HeadlineSmall } from "@/components/Texts/Headline";
+import { LabelLarge } from "@/components/Texts/Label";
 import type { TFunction } from "i18next";
-import { MdLocationCity, MdPersonOutline } from "react-icons/md";
-import { BodyMedium } from "@/components/Texts/Body";
+import {
+  MdArrowForward,
+  MdLocationCity,
+  MdPersonOutline,
+} from "react-icons/md";
+import { BodyLarge, BodyMedium } from "@/components/Texts/Body";
+import NextLink from "next/link";
 
 function ProjectCard(props: { t: TFunction }) {
   const cityCount = 50;
@@ -51,21 +57,45 @@ export default function OrganizationPage(props: {
 
   return (
     <VStack
-      spaceX={4}
-      spaceY={4}
-      p={4}
+      spaceY="56px"
+      p="56px"
       h="full"
       w="full"
       bg="background.backgroundLight"
+      alignItems="start"
+      justifyContent="start"
     >
-      <HeadlineLarge>{t("organization")}</HeadlineLarge>
-      <SimpleGrid minChildWidth="xs" gap="40px" w="full">
-        {Array(5)
-          .keys()
-          .map((i) => (
+      <VStack spaceY="24px" alignItems="start" justifyContent="start">
+        <HeadlineLarge>{t("all-projects")}</HeadlineLarge>
+        <BodyLarge>{t("explore-projects-description")}</BodyLarge>
+      </VStack>
+      <SimpleGrid minChildWidth="xs" gap="24px" w="full">
+        {
+          // use Array.from to fix iterator warning
+          Array.from(Array(5).keys()).map((i) => (
             <ProjectCard t={t} key={i} />
-          ))}
+          ))
+        }
       </SimpleGrid>
+      <NextLink
+        href="https://citycatalyst.openearth.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ width: "100%" }}
+      >
+        <Card.Root w="full">
+          <Card.Header>
+            <LabelLarge>{t("learning")}</LabelLarge>
+            <HeadlineSmall>{t("learn-more-blog")}</HeadlineSmall>
+          </Card.Header>
+          <Card.Body>{t("read-blog-description")}</Card.Body>
+          <Card.Footer justifyContent="right">
+            <Button variant="outline" colorScheme="primary">
+              {t("visit-blog")} <Icon as={MdArrowForward} />
+            </Button>
+          </Card.Footer>
+        </Card.Root>
+      </NextLink>
     </VStack>
   );
 }

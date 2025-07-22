c4d433bf901ecc4e85ad585d3b2a693fbe1a8f39
Milan Gruner <milangruner@gmail.com>
2025-07-22 14:46:36 +0200

    fix(ui): overly nested containers and unused imports from OrganizationHero

--- app/src/components/Organization/OrganizationHero.tsx
+++ app/src/components/Organization/OrganizationHero.tsx
@@ -1,24 +1,13 @@
-// only render map on the client
-import dynamic from "next/dynamic";
-import type { TFunction } from "i18next";
-import type { InventoryResponse } from "@/util/types";
-import { Box, Heading, Icon, Spinner, Text } from "@chakra-ui/react";
-import { CircleFlag } from "react-circle-flags";
-import {
-  MdArrowOutward,
-  MdGridView,
-  MdGroup,
-  MdInfoOutline,
-  MdLocationCity,
-  MdOutlineAspectRatio,
-} from "react-icons/md";
 import { Tooltip } from "@/components/ui/tooltip";
-import { Trans } from "react-i18next/TransWithoutContext";
-import Link from "next/link";
 import type { OrganizationAttributes } from "@/models/Organization";
+import { Box, Heading, Icon, Spinner, Text } from "@chakra-ui/react";
+import type { TFunction } from "i18next";
+import dynamic from "next/dynamic";
+import { MdGridView, MdInfoOutline, MdLocationCity } from "react-icons/md";
 import { ModulesIcon } from "../icons";
 import { BodyLarge } from "../Texts/Body";
 
+// only render map on the client
 const CityMap = dynamic(() => import("@/components/CityMap"), { ssr: false });
 
 interface OrganizationHeroProps {
@@ -37,158 +26,154 @@ export function OrganizationHero({
 
   return (
     <Box bg="content.alternative" w="full" px="56px" py="56px">
-      <Box className="flex mx-auto max-w-full">
-        <Box className="w-full h-[240px] flex flex-col justify-center">
-          <Box className="flex h-[240px]">
-            <Box className="flex gap-[24px] flex-col h-full w-full">
-              <Box className="flex flex-col gap-2">
-                <Box className="flex items-center gap-4">
-                  {organization ? (
-                    <Heading
-                      fontSize="display.md"
-                      color="base.light"
-                      fontWeight="semibold"
-                      lineHeight="52"
-                      className="flex"
-                    >
-                      <span data-testid="hero-organization-name">
-                        {organization.name}
-                      </span>
-                    </Heading>
-                  ) : (
-                    isLoading && <Spinner size="lg" color="white" />
-                  )}
-                </Box>
-                <BodyLarge color="background.overlay">
-                  {t("organization-name")}
-                </BodyLarge>
-              </Box>
-              <Box className="flex gap-8 mt-[24px]">
-                <Box className="flex align-baseline gap-3">
-                  <Icon as={MdGridView} boxSize={6} fill="base.light" />
-                  <Box>
-                    <Box className="flex gap-1">
-                      <Text
-                        fontFamily="heading"
-                        color="base.light"
-                        fontSize="headline.sm"
-                        fontWeight="semibold"
-                        lineHeight="32"
-                      >
-                        <>
-                          {projectCount}{" "}
-                          <span className="text-[16px]">{t("projects")}</span>
-                        </>
-                      </Text>
-                      <Tooltip
-                        content={t("active-projects-tooltip")}
-                        positioning={{ placement: "bottom-start" }}
-                      >
-                        <Icon
-                          as={MdInfoOutline}
-                          w={3}
-                          h={3}
-                          color="background.overlay"
-                        />
-                      </Tooltip>
-                    </Box>
-                    <Text
-                      fontSize="body.md"
+      <Box className="flex mx-auto max-w-full h-[240px]">
+        <Box className="flex gap-[24px] flex-col w-full">
+          <Box className="flex flex-col gap-2">
+            <Box className="flex items-center gap-4">
+              {organization ? (
+                <Heading
+                  fontSize="display.md"
+                  color="base.light"
+                  fontWeight="semibold"
+                  lineHeight="52"
+                  className="flex"
+                >
+                  <span data-testid="hero-organization-name">
+                    {organization.name}
+                  </span>
+                </Heading>
+              ) : (
+                isLoading && <Spinner size="lg" color="white" />
+              )}
+            </Box>
+            <BodyLarge color="background.overlay">
+              {t("organization-name")}
+            </BodyLarge>
+          </Box>
+          <Box className="flex gap-8 mt-[24px]">
+            <Box className="flex align-baseline gap-3">
+              <Icon as={MdGridView} boxSize={6} fill="base.light" />
+              <Box>
+                <Box className="flex gap-1">
+                  <Text
+                    fontFamily="heading"
+                    color="base.light"
+                    fontSize="headline.sm"
+                    fontWeight="semibold"
+                    lineHeight="32"
+                  >
+                    <>
+                      {projectCount}{" "}
+                      <span className="text-[16px]">{t("projects")}</span>
+                    </>
+                  </Text>
+                  <Tooltip
+                    content={t("active-projects-tooltip")}
+                    positioning={{ placement: "bottom-start" }}
+                  >
+                    <Icon
+                      as={MdInfoOutline}
+                      w={3}
+                      h={3}
                       color="background.overlay"
-                      fontStyle="normal"
-                      fontWeight={400}
-                      lineHeight="20px"
-                      letterSpacing="wide"
-                    >
-                      {t("active-in-total")}
-                    </Text>
-                  </Box>
+                    />
+                  </Tooltip>
                 </Box>
-                <Box className="flex align-baseline gap-3">
-                  <Icon as={MdLocationCity} boxSize={6} fill="base.light" />
-                  <Box>
-                    <Box className="flex gap-1">
-                      <Text
-                        fontFamily="heading"
-                        color="base.light"
-                        fontSize="headline.sm"
-                        fontWeight="semibold"
-                        lineHeight="32"
-                      >
-                        {totalCityCount} {t("cities")}
-                      </Text>
-                      <Tooltip
-                        content={t("city-count-tooltip")}
-                        positioning={{
-                          placement: "bottom-start",
-                        }}
-                      >
-                        <Icon
-                          as={MdInfoOutline}
-                          w={3}
-                          h={3}
-                          color="background.overlay"
-                        />
-                      </Tooltip>
-                    </Box>
-                    <Text
-                      fontSize="body.md"
-                      color="background.overlay"
-                      fontStyle="normal"
-                      fontWeight={400}
-                      lineHeight="20px"
-                      letterSpacing="wide"
-                    >
-                      {t("across-projects")}
-                    </Text>
-                  </Box>
-                </Box>
-                <Box className="flex align-baseline gap-3">
-                  <Icon as={ModulesIcon} boxSize={6} fill="base.light" />
-                  <Box>
-                    <Box className="flex gap-1">
-                      <Text
-                        fontFamily="heading"
-                        color="base.light"
-                        fontSize="headline.sm"
-                        fontWeight="semibold"
-                        lineHeight="32"
-                      >
-                        {t("pro")}
-                      </Text>
-                      <Tooltip
-                        content={t("active-plan-tooltip")}
-                        positioning={{
-                          placement: "bottom-start",
-                        }}
-                      >
-                        <Icon
-                          as={MdInfoOutline}
-                          w={3}
-                          h={3}
-                          color="background.overlay"
-                        />
-                      </Tooltip>
-                    </Box>
-                    <Text
-                      fontSize="body.md"
-                      color="background.overlay"
-                      fontStyle="normal"
-                      fontWeight={400}
-                      lineHeight="20px"
-                      letterSpacing="wide"
-                    >
-                      {t("active-plan")}
-                    </Text>
-                  </Box>
-                </Box>
+                <Text
+                  fontSize="body.md"
+                  color="background.overlay"
+                  fontStyle="normal"
+                  fontWeight={400}
+                  lineHeight="20px"
+                  letterSpacing="wide"
+                >
+                  {t("active-in-total")}
+                </Text>
               </Box>
             </Box>
-            <Box mt={-50}>
-              {/* TODO create organization map or re-use project map? */}
-              <CityMap locode="BR SAO" width={422} height={317} />
+            <Box className="flex align-baseline gap-3">
+              <Icon as={MdLocationCity} boxSize={6} fill="base.light" />
+              <Box>
+                <Box className="flex gap-1">
+                  <Text
+                    fontFamily="heading"
+                    color="base.light"
+                    fontSize="headline.sm"
+                    fontWeight="semibold"
+                    lineHeight="32"
+                  >
+                    {totalCityCount} {t("cities")}
+                  </Text>
+                  <Tooltip
+                    content={t("city-count-tooltip")}
+                    positioning={{
+                      placement: "bottom-start",
+                    }}
+                  >
+                    <Icon
+                      as={MdInfoOutline}
+                      w={3}
+                      h={3}
+                      color="background.overlay"
+                    />
+                  </Tooltip>
+                </Box>
+                <Text
+                  fontSize="body.md"
+                  color="background.overlay"
+                  fontStyle="normal"
+                  fontWeight={400}
+                  lineHeight="20px"
+                  letterSpacing="wide"
+                >
+                  {t("across-projects")}
+                </Text>
+              </Box>
+            </Box>
+            <Box className="flex align-baseline gap-3">
+              <Icon as={ModulesIcon} boxSize={6} fill="base.light" />
+              <Box>
+                <Box className="flex gap-1">
+                  <Text
+                    fontFamily="heading"
+                    color="base.light"
+                    fontSize="headline.sm"
+                    fontWeight="semibold"
+                    lineHeight="32"
+                  >
+                    {t("pro")}
+                  </Text>
+                  <Tooltip
+                    content={t("active-plan-tooltip")}
+                    positioning={{
+                      placement: "bottom-start",
+                    }}
+                  >
+                    <Icon
+                      as={MdInfoOutline}
+                      w={3}
+                      h={3}
+                      color="background.overlay"
+                    />
+                  </Tooltip>
+                </Box>
+                <Text
+                  fontSize="body.md"
+                  color="background.overlay"
+                  fontStyle="normal"
+                  fontWeight={400}
+                  lineHeight="20px"
+                  letterSpacing="wide"
+                >
+                  {t("active-plan")}
+                </Text>
+              </Box>
             </Box>
           </Box>
+        </Box>
+        <Box mt={-50}>
+          {/* TODO create organization map or re-use project map? */}
+          <CityMap locode="BR SAO" width={422} height={317} />
         </Box>
       </Box>
     </Box>

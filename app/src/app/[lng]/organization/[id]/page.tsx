0e923f64bfa7e92fc73cc590c17a2e3a1fae81ef
Milan Gruner <milangruner@gmail.com>
2025-07-22 13:26:55 +0200

    feat(ui): use OrganizationHero on OrganizationPage

--- app/src/app/[lng]/organization/[id]/page.tsx
+++ app/src/app/[lng]/organization/[id]/page.tsx
@@ -15,6 +15,9 @@ import {
 } from "react-icons/md";
 import { BodyLarge, BodyMedium } from "@/components/Texts/Body";
 import NextLink from "next/link";
+import { OrganizationHero } from "@/components/Organization/OrganizationHero";
+import { OrganizationAttributes } from "@/models/Organization";
+import { ButtonMedium } from "@/components/Texts/Button";
 
 function ProjectCard(props: { t: TFunction }) {
   const cityCount = 50;
@@ -54,48 +57,60 @@ export default function OrganizationPage(props: {
 }) {
   const { lng } = use(props.params);
   const { t } = useTranslation(lng, "organization");
+  const organization: OrganizationAttributes = {
+    name: "Test Organization",
+    active: true,
+    organizationId: "70bbe1ea-7869-45e5-af37-c564b82a599f",
+    // description: "This is a test organization description.",
+  };
 
   return (
-    <VStack
-      spaceY="56px"
-      p="56px"
-      h="full"
-      w="full"
-      bg="background.backgroundLight"
-      alignItems="start"
-      justifyContent="start"
-    >
-      <VStack spaceY="24px" alignItems="start" justifyContent="start">
-        <HeadlineLarge>{t("all-projects")}</HeadlineLarge>
-        <BodyLarge>{t("explore-projects-description")}</BodyLarge>
-      </VStack>
-      <SimpleGrid minChildWidth="xs" gap="24px" w="full">
-        {
-          // use Array.from to fix iterator warning
-          Array.from(Array(5).keys()).map((i) => (
-            <ProjectCard t={t} key={i} />
-          ))
-        }
-      </SimpleGrid>
-      <NextLink
-        href="https://citycatalyst.openearth.org"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{ width: "100%" }}
+    <VStack>
+      <OrganizationHero t={t} organization={organization} />
+      <VStack
+        spaceY="56px"
+        p="56px"
+        h="full"
+        w="full"
+        bg="background.backgroundLight"
+        alignItems="start"
+        justifyContent="start"
       >
-        <Card.Root w="full">
-          <Card.Header>
-            <LabelLarge>{t("learning")}</LabelLarge>
-            <HeadlineSmall>{t("learn-more-blog")}</HeadlineSmall>
-          </Card.Header>
-          <Card.Body>{t("read-blog-description")}</Card.Body>
-          <Card.Footer justifyContent="right">
-            <Button variant="outline" colorScheme="primary">
-              {t("visit-blog")} <Icon as={MdArrowForward} />
-            </Button>
-          </Card.Footer>
-        </Card.Root>
-      </NextLink>
+        <VStack spaceY="24px" alignItems="start" justifyContent="start">
+          <HeadlineLarge>{t("all-projects")}</HeadlineLarge>
+          <BodyLarge>{t("explore-projects-description")}</BodyLarge>
+        </VStack>
+        <SimpleGrid minChildWidth="xs" gap="24px" w="full">
+          {
+            // use Array.from to fix iterator warning
+            Array.from(Array(5).keys()).map((i) => (
+              <ProjectCard t={t} key={i} />
+            ))
+          }
+        </SimpleGrid>
+        <NextLink
+          href="https://citycatalyst.openearth.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ width: "100%" }}
+        >
+          <Card.Root w="full">
+            <Card.Header>
+              <LabelLarge>{t("learning")}</LabelLarge>
+              <HeadlineSmall color="content.link">
+                {t("learn-more-blog")}
+              </HeadlineSmall>
+            </Card.Header>
+            <Card.Body>{t("read-blog-description")}</Card.Body>
+            <Card.Footer justifyContent="right">
+              <ButtonMedium color="content.link" textTransform="uppercase">
+                {t("visit-blog")}
+              </ButtonMedium>
+              <Icon as={MdArrowForward} color="content.link" />
+            </Card.Footer>
+          </Card.Root>
+        </NextLink>
+      </VStack>
     </VStack>
   );
 }

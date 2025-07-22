d6fdb7adf51788bbd4cf567232acd20b0a2b497f
Milan Gruner <milangruner@gmail.com>
2025-07-22 11:12:46 +0200

    feat(ui): create organization page and layout

---
+++ app/src/app/[lng]/organization/[id]/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { use } from "react";
+
+import { NavigationBar } from "@/components/navigation-bar";
+import { Toaster } from "@/components/ui/toaster";
+import { Box } from "@chakra-ui/react";
+
+export default function OrganizationLayout(props: {
+  children: React.ReactNode;
+  params: Promise<{ lng: string }>;
+}) {
+  const { lng } = use(props.params);
+
+  return (
+    <Box
+      as="main"
+      bg="background.backgroundLight"
+      className="h-full flex flex-col"
+    >
+      <NavigationBar showMenu lng={lng} showNav={false} />
+      <Toaster />
+      <Box className="w-full h-full bg-no-repeat px-8">{props.children}</Box>
+    </Box>
+  );
+}
---
+++ app/src/app/[lng]/organization/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function OrganizationPage() {
+  return (
+    <div>
+      <h1>Organization Page</h1>
+      <p>This is the organization page.</p>
+    </div>
+  );
+}

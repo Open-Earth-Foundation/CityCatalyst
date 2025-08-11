324c7d63db6d1f7691c42143a2bdfe6a5a03d975
Evan Prodromou <evan@openearth.org>
2025-08-10 20:35:04 -0400

    feat: add table for OAuth 2.0 clients with i18n

---
+++ app/migrations/20250811001707-oauth-client-table.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable("OAuthClient", {
+      client_id: {
+      type: Sequelize.STRING(64),
+      primaryKey: true,
+      allowNull: false
+      },
+      redirect_uri: {
+      type: Sequelize.STRING(256), // Sequelize does not have a URL type
+      allowNull: false,
+      unique: true
+      },
+      created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+    await queryInterface.createTable("OAuthClientI18N", {
+      client_id: {
+        type: Sequelize.STRING(64),
+        allowNull: false,
+        references: {
+          model: "OAuthClient",
+          key: "client_id"
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE"
+      },
+      language: {
+        type: Sequelize.STRING(2),
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING(64),
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+
+    await queryInterface.addConstraint("OAuthClientI18N", {
+      fields: ["client_id", "language"],
+      type: "primary key",
+      name: "OAuthClientI18N_pkey"
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeConstraint("OAuthClientI18N", "OAuthClientI18N_pkey");
+    await queryInterface.dropTable("OAuthClientI18N");
+    await queryInterface.dropTable("OAuthClient");
+  }
+};

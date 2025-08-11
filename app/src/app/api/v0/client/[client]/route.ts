28432d7c1189e60c46926f5a34b5b91ca9db36db
Evan Prodromou <evan@openearth.org>
2025-08-10 23:39:35 -0400

    feat: stub for delete route

--- app/src/app/api/v0/client/[client]/route.ts
+++ app/src/app/api/v0/client/[client]/route.ts
@@ -25,3 +25,17 @@ export const GET = apiHandler(async (_req, { params, session }) => {
 
   return NextResponse.json({ data: client });
 })
+
+/** creates a new client */
+export const DELETE = apiHandler(async (_req, { params, session }) => {
+
+  if (!hasFeatureFlag(FeatureFlags.OAUTH_ENABLED)) {
+    throw createHttpError.InternalServerError("OAuth 2.0 not enabled");
+  }
+
+  if (!session) {
+    throw new createHttpError.Unauthorized("Must be logged in!");
+  }
+
+  throw createHttpError.NotImplemented("Not yet implemented!");
+});

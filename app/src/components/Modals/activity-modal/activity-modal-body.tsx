547565362d1dd9c379f2ccf26fcd804e905a4e97
Milan Gruner <milan@openearth.org>
2025-08-26 12:09:34 +0200

    fix(ui): make activity amount input wider on ActivityModalBody

--- app/src/components/Modals/activity-modal/activity-modal-body.tsx
+++ app/src/components/Modals/activity-modal/activity-modal-body.tsx
@@ -464,6 +464,7 @@ const ActivityModalBody = ({
               <Field
                 invalid={!!resolve(prefix + "activityDataAmount", errors)}
                 label={<Text truncate>{t(title)}</Text>}
+                flex="2"
               >
                 <HStack>
                   <FormattedNumberInput
@@ -472,6 +473,8 @@ const ActivityModalBody = ({
                     defaultValue="0"
                     t={t}
                     miniAddon
+                    minWidth="300px"
+                    flex={2}
                   />
                   {(units?.length as number) > 0 && (
                     <Controller
@@ -556,6 +559,8 @@ const ActivityModalBody = ({
                 <Field
                   label={t("emission-factor-type")}
                   invalid={!!resolve(prefix + "emissionFactorType", errors)}
+                  maxWidth="250px"
+                  flex="1"
                 >
                   <Controller
                     name="activity.emissionFactorType"

fa101a19b25851da21dc018f8cef3cc97ab9c632
Evan Prodromou <evan@openearth.org>
2025-08-27 13:57:03 -0400

    feat: add interface definition for OAuth Metadata

--- app/src/app/api/v0/oauth/metadata/route.ts
+++ app/src/app/api/v0/oauth/metadata/route.ts
@@ -6,6 +6,31 @@ import { hasFeatureFlag, FeatureFlags } from "@/util/feature-flags";
 
 const DOCUMENTATION_URL = 'https://github.com/Open-Earth-Foundation/CityCatalyst/wiki/CityCatalyst-Backend-API'
 
+interface OAuthMetadata {
+  issuer: string;
+  authorization_endpoint: string;
+  token_endpoint: string;
+  jwks_uri?: string;
+  registration_endpoint?: string;
+  scopes_supported: string[];
+  response_types_supported: string[];
+  response_modes_supported?: string[];
+  grant_types_supported?: string[];
+  token_endpoint_auth_methods_supported?: string[];
+  token_endpoint_auth_signing_alg_values_supported?: string[];
+  service_documentation?: string;
+  ui_locales_supported?: string[];
+  op_policy_uri?: string;
+  op_tos_uri?: string;
+  revocation_endpoint?: string;
+  revocation_endpoint_auth_methods_supported?: string[];
+  revocation_endpoint_auth_signing_alg_values_supported?: string[];
+  introspection_endpoint?: string;
+  introspection_endpoint_auth_methods_supported?: string[];
+  introspection_endpoint_auth_signing_alg_values_supported?: string[];
+  code_challenge_methods_supported?: string[];
+}
+
 export const GET = apiHandler(async (_req, { session }) => {
   if (!hasFeatureFlag(FeatureFlags.OAUTH_ENABLED)) {
     throw createHttpError.InternalServerError("OAuth 2.0 not enabled");
@@ -17,7 +42,7 @@ export const GET = apiHandler(async (_req, { session }) => {
     throw createHttpError.InternalServerError('Unable to determine server origin URL');
   }
 
-  return NextResponse.json({
+  return NextResponse.json<OAuthMetadata>({
     issuer: `${origin}/`,
     authorization_endpoint: `${origin}/authorize/`,
     token_endpoint: `${origin}/api/v0/token/`,

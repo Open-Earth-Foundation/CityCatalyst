7eb5f94e53c3b2e0e731dab1aedf49f87c4bf38f
Cephas Chapa <cephas@openearth.com>
2025-08-28 15:44:18 +0200

    feat: adds login to render top section based on language set

--- app/src/app/[lng]/cities/[cityId]/HIAP/page.tsx
+++ app/src/app/[lng]/cities/[cityId]/HIAP/page.tsx
@@ -4,16 +4,18 @@ import { use } from "react";
 import { useTranslation } from "@/i18n/client";
 import {
   useGetCityPopulationQuery,
+  useGetHiapQuery,
   useGetInventoryByCityIdQuery,
 } from "@/services/api";
-import { ACTION_TYPES } from "@/util/types";
+import { ACTION_TYPES, LANGUAGES } from "@/util/types";
 import { Box, Button, Icon, Tabs, Text } from "@chakra-ui/react";
 import { formatEmissions } from "@/util/helpers";
 import { Hero } from "@/components/GHGIHomePage/Hero";
 import { HiapTab } from "@/app/[lng]/cities/[cityId]/HIAP/HiapTab";
 import ProgressLoader from "@/components/ProgressLoader";
 import { AdaptationTabIcon, MitigationTabIcon } from "@/components/icons";
-import { LuRefreshCw } from "react-icons/lu";
+import { ClimateActionsSection } from "@/components/HIAP/ClimateActionsSection";
+import i18next from "i18next";
 
 export default function HIAPPage(props: {
   params: Promise<{ lng: string; cityId: string }>;
@@ -31,6 +33,19 @@ export default function HIAPPage(props: {
     { cityId: inventory?.cityId!, year: inventory?.year! },
     { skip: !inventory?.cityId || !inventory?.year },
   );
+
+  const lang = i18next.language as LANGUAGES;
+
+  const {
+    data: hiapData,
+    isLoading,
+    error,
+    refetch,
+  } = useGetHiapQuery({
+    inventoryId: inventory?.inventoryId!,
+    lng: lang,
+    actionType: ACTION_TYPES.Mitigation,
+  });
   return (
     <Box
       h="full"
@@ -61,72 +76,13 @@ export default function HIAPPage(props: {
             flexDirection="column"
             gap="24px"
           >
-            {/* citycatalyst actions section */}
-            <Box display="flex" flexDirection="column" gap="24px" pb="24px">
-              <Box display="flex" flexDirection="column" gap="16px">
-                <Text
-                  color="content.link"
-                  fontFamily="heading"
-                  fontSize="title.sm"
-                  fontWeight="bold"
-                  textTransform="uppercase"
-                >
-                  {t("citycatalyst-actions-title")}
-                </Text>
-              </Box>
-              <Box
-                display="flex"
-                flexDirection="row"
-                gap="24px"
-                alignItems="center"
-              >
-                <Box display="flex" flexDirection="column" gap="8px">
-                  <Text
-                    fontFamily="heading"
-                    fontWeight="bold"
-                    fontSize="headline.sm"
-                    color="content.secondary"
-                  >
-                    {t("top-actions-for-your-city")}
-                  </Text>
-                  <Text
-                    fontFamily="body"
-                    fontSize="body.lg"
-                    color="content.tertiary"
-                  >
-                    {t("top-actions-for-your-city-description")}
-                  </Text>
-                </Box>
-                <Box>
-                  <Button
-                    bg="content.link"
-                    color="white"
-                    px="24px"
-                    h="84px"
-                    borderRadius="16px"
-                    gap="12px"
-                    onClick={() => {
-                      // TODO: add logic to re-prioritize actions
-                    }}
-                  >
-                    <Icon
-                      as={LuRefreshCw}
-                      rotate={"270deg"}
-                      boxSize={"36px"}
-                      color="white"
-                    />
-                    <Text
-                      fontFamily="heading"
-                      fontWeight="bold"
-                      fontSize="headline.sm"
-                      textTransform="none"
-                    >
-                      {t("re-prioritize-actions")}
-                    </Text>
-                  </Button>
-                </Box>
-              </Box>
-            </Box>
+            <ClimateActionsSection
+              t={t}
+              actions={hiapData}
+              onReprioritize={() => {
+                // TODO: add logic to re-prioritize actions
+              }}
+            />
             <Tabs.Root
               variant="line"
               lazyMount

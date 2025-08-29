d44dc1e8ff96c449137762a2d0b74c3361328443
Milan Gruner <milan@openearth.org>
2025-08-29 18:27:47 +0200

    fix(ui): remove ID from newlyConnectedSourceIds when it is disconnected on sector page

--- app/src/app/[lng]/[inventory]/data/[step]/page.tsx
+++ app/src/app/[lng]/[inventory]/data/[step]/page.tsx
@@ -161,7 +161,11 @@ function NoDataSourcesMessage({
   year?: number;
 }) {
   return (
-    <Flex align="center" direction="column" data-testid="no-data-sources-message">
+    <Flex
+      align="center"
+      direction="column"
+      data-testid="no-data-sources-message"
+    >
       <Box borderRadius="full" p={4} bgColor="background.neutral" mb={6}>
         <Icon
           as={NoDatasourcesIcon}
@@ -525,6 +529,11 @@ export default function AddDataSteps(props: {
       );
       // TODO show alert
       setDisconnectingDataSourceId(null);
+      setNewlyConnectedDataSourceIds(
+        newlyConnectedDataSourceIds.filter(
+          (connectedSource) => connectedSource !== source.datasourceId,
+        ),
+      );
       onSearchDataSourcesClicked();
     } else {
       logger.error("Something went wrong when disconnecting data source");

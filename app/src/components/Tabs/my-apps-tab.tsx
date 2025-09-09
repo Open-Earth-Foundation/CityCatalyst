3b1702be44b4981c9d49d74148898872c84167cc
Evan Prodromou <evan@openearth.org>
2025-09-09 08:29:21 -0400

    style: use HeadlineSmall, BodyLarge components

--- app/src/components/Tabs/my-apps-tab.tsx
+++ app/src/components/Tabs/my-apps-tab.tsx
@@ -6,6 +6,8 @@ import { api } from "@/services/api";
 import ProgressLoader from "@/components/ProgressLoader";
 import { AppCard } from "../Cards/AppCard";
 import { useTranslation } from "@/i18n/client";
+import { HeadlineSmall } from "../Texts/Headline";
+import { BodyLarge } from "../Texts/Body";
 
 interface MyAppsTabProps {
   lng: string /** Language to use for UI and client descriptions */;
@@ -25,26 +27,8 @@ const MyAppsTab: FC<MyAppsTabProps> = ({ lng }) => {
       <Tabs.Content value="my-apps">
         <Box display="flex" flexDirection="column" gap="48px" marginTop="32px">
           <Box>
-            <Text
-              color="content.primary"
-              fontWeight="bold"
-              lineHeight="32"
-              fontSize="headline.sm"
-              fontFamily="heading"
-              fontStyle="normal"
-            >
-              {t("my-apps")}
-            </Text>
-            <Text
-              color="content.tertiary"
-              fontWeight="normal"
-              lineHeight="24"
-              fontSize="body.lg"
-              letterSpacing="wide"
-              marginTop="8px"
-            >
-              {t("my-apps-sub-title")}
-            </Text>
+            <HeadlineSmall text={t("my-apps")} />
+            <BodyLarge text={t("my-apps-sub-title")} />
           </Box>
           {isAppsLoading ? (
             <ProgressLoader />

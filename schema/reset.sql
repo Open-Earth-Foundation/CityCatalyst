d22ff49b5dd7a54d6a0f7fd7ce7d4f3be6b29fab
Evan Prodromou <evan@openearth.org>
2025-09-10 10:45:40 -0400

    fix: remove unused schema directory

--- schema/reset.sql
+++
@@ -1,6 +0,0 @@
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;
-COMMENT ON SCHEMA public IS 'standard public schema';
-

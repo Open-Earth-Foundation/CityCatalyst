5f905ce30a80efe85c7b0c82f44501b723563d71
Marina Olivella <marina@openearth.org>
2025-09-11 10:48:23 -0300

    fix: sector card navigation

--- app/src/components/Cards/SectorCard.tsx
+++ app/src/components/Cards/SectorCard.tsx
@@ -17,6 +17,7 @@ import { Trans } from "react-i18next/TransWithoutContext";
 // import { AddIcon } from "@chakra-ui/icons";
 import { InventoryType, InventoryTypeEnum, ISector } from "@/util/constants";
 import { BsPlus } from "react-icons/bs";
+import { usePathname } from "next/navigation";
 
 import {
   AccordionItem,
@@ -36,6 +37,7 @@ export function SectorCard({
   t: TFunction;
   inventory: InventoryResponse;
 }) {
+  const pathname = usePathname();
   const [isAccordionOpen, setAccordionOpen] = useState(false);
   const toggleAccordion = () => setAccordionOpen(!isAccordionOpen);
 
@@ -116,7 +118,7 @@ export function SectorCard({
             </Box>
             <Box>
               <NextLink
-                href={`/${inventory.inventoryId}/data/${sector.number}`}
+                href={`${pathname}/data/${sector.number}`}
               >
                 <Button
                   variant="outline"
@@ -169,7 +171,7 @@ export function SectorCard({
                 {sectorProgress?.subSectors?.map((subSector, i) => (
                   <NextLink
                     key={i}
-                    href={`/${inventory.inventoryId}/data/${convertSectorReferenceNumberToNumber(sector.referenceNumber)}/${subSector.subsectorId}`}
+                    href={`${pathname}/data/${convertSectorReferenceNumberToNumber(sector.referenceNumber)}/${subSector.subsectorId}`}
                   >
                     <SubSectorCard
                       t={t}

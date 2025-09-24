c8cc247cfd641f04f01c10bf5578a5f7afc6c430
Milan Gruner <milan@openearth.org>
2025-09-24 11:58:49 +0200

    fix(db): use bulkUpsert for ProjectModules in modules seeder, insert timestamps

--- app/seeders/20250923103314-modules.cjs
+++ app/seeders/20250923103314-modules.cjs
@@ -3,11 +3,6 @@
 const { parseJsonFile, bulkUpsert } = require("./util/util.cjs");
 const CCRA_PROJECT_ID = "3d1a4b2c-8e7f-4d5a-9c6b-1f2e3d4c5b6a";
 
-/**
-  INSERT INTO "Module" () VALUES ('9295ad69-72c6-4b1c-b29d-b71f7b8ba8e8', 'assess-and-analyze', 'GHGI', 'CC', '/GHGI', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-  INSERT INTO "Module" (id, step, name, type, url, created, last_updated) VALUES ('072a53d6-cd58-4622-b4e4-4f482baa23b3', 'plan', 'HIAP', 'CC', '/HIAP', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-  */
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
@@ -18,8 +13,6 @@ module.exports = {
         name: JSON.stringify(module.name),
         description: JSON.stringify(module.description),
         tagline: JSON.stringify(module.tagline),
-        created: new Date(),
-        last_updated: new Date(),
       }));
 
       await bulkUpsert(
@@ -28,6 +21,8 @@ module.exports = {
         serializedModules,
         "id",
         transaction,
+        false,
+        true, // insert timestamps on create
       );
 
       // Get all project IDs and create ProjectModules entries for CCRA
@@ -39,12 +34,16 @@ module.exports = {
       const projectModulesData = projects.map((project) => ({
         project_id: project.project_id,
         module_id: CCRA_PROJECT_ID,
-        created: new Date(),
-        last_updated: new Date(),
       }));
 
       if (projectModulesData.length > 0) {
-        await queryInterface.bulkInsert("ProjectModules", projectModulesData);
+        await bulkUpsert(
+          queryInterface,
+          "ProjectModules",
+          projectModulesData,
+          ["project_id", "module_id"],
+          transaction,
+        );
       }
     });
   },

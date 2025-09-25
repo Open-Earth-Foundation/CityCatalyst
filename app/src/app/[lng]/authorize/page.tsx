110589194c1bd8089cc88e5f116cf7d3422d7153
Evan Prodromou <evan@openearth.org>
2025-09-25 12:53:43 -0400

    feat: redirect to login on OAuth authorize if not logged in

--- app/src/app/[lng]/authorize/page.tsx
+++ app/src/app/[lng]/authorize/page.tsx
@@ -2,10 +2,9 @@ import crypto from 'crypto';
 import AuthorizeForm from './AuthorizeForm';
 import { Auth } from "@/lib/auth";
 import { hasFeatureFlag, FeatureFlags } from '@/util/feature-flags';
+import RedirectToLogin from "@/components/Navigation/RedirectToLogin";
 
 export default async function AuthorizePage({ params }: { params: any }) {
-
-  // XXX: Fix i18n here
   if (!hasFeatureFlag(FeatureFlags.OAUTH_ENABLED)) {
     return <div>{"OAuth 2.0 not enabled"}</div>
   }
@@ -15,8 +14,9 @@ export default async function AuthorizePage({ params }: { params: any }) {
   const session = await Auth.getServerSession();
 
   if (!session) {
-    throw new Error('Must have a session to authorize');
+    return <RedirectToLogin lng={lng} />
   }
+
   if (!session.csrfSecret) {
     throw new Error('Must have a csrfSecret');
   }

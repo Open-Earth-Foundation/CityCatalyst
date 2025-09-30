ad13fb446265453103aaf201958e6d436a9a1432
Cephas Chapa <cephas@openearth.com>
2025-09-30 10:14:33 +0200

    fix: documenet not defined error

--- app/src/components/ChatBot/chat-popover.tsx
+++ app/src/components/ChatBot/chat-popover.tsx
@@ -17,7 +17,6 @@ import {
 import { Button } from "@/components/ui/button";
 import { OpenChangeDetails } from "@zag-js/popover";
 import { api } from "@/services/api";
-import { useSession } from "next-auth/react";
 
 export default function ChatPopover({
   lng,
@@ -30,8 +29,6 @@ export default function ChatPopover({
   const { open, onOpen, onClose } = useDisclosure();
   const inputRef = React.useRef(null);
   const { t } = useTranslation(lng as string, "chat");
-
-  const { data: session } = useSession();
 
   // get user info
   const { data: userInfo, isLoading: isUserInfoLoading } =
@@ -51,14 +48,15 @@ export default function ChatPopover({
   // adjust the position of the popover based on the scroll position (i.e when the user scrolls to the bottom of the page)
   const [scrollPosition, setScrollPosition] = useState(0);
 
-  // Get
-
   const handleScroll = () => {
     const scrollPosition = window.scrollY;
     setScrollPosition(scrollPosition);
   };
 
   const getDynamicBottomPosition = () => {
+    if (typeof window === "undefined") {
+      return "8px"; // Default position for SSR
+    }
     const maxScroll =
       document.documentElement.scrollHeight - window.innerHeight;
     const scrollPercentage = (scrollPosition / maxScroll) * 100;

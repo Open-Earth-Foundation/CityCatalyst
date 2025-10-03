da6e57b98c91bd7a9e6cc4712201d71249f0ec6f
Cephas Chapa <cephas@openearth.com>
2025-10-03 13:18:34 +0200

    deletes unimported component

--- app/src/components/HomePageJN/HomePage.tsx
+++ app/src/components/HomePageJN/HomePage.tsx
@@ -273,8 +273,6 @@ export default function HomePage({
               </AccordionRoot>
             )}
           </Box>
-          {/* Clima AI ChatBot Disclaimer Dialog */}
-          <ClimaAIAssistantDisclaimerDialog t={t} />
           <Footer lng={language} />
         </>
       )}

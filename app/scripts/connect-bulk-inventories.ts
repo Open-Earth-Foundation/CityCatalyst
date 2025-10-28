97aa8e7765678cf70e6fee9f98591c30c7a75817
Evan Prodromou <evan@openearth.org>
2025-10-28 10:41:47 -0400

    chore: bulk inventories work

--- app/scripts/connect-bulk-inventories.ts
+++ app/scripts/connect-bulk-inventories.ts
@@ -5,40 +5,30 @@ console.log("Token", apiToken);
 if (!apiToken) {
   console.error("Error: CITY_CATALYST_API_TOKEN is not set in environment.");
   console.error(
-    "Run with: CITY_CATALYST_API_TOKEN=your_token_here npm run bulk:create-inventories",
+    "Run with: CITY_CATALYST_API_TOKEN=your_token_here npm run bulk:connect-inventories",
   );
   process.exit(1);
 }
-// const origin = "https://citycatalyst.openearth.dev";
-const origin = "http://localhost:3000";
-const apiUrl = `${origin}/api/v1/admin/bulk`;
+
+const origin = "https://citycatalyst.openearth.dev";
+// const origin = "http://localhost:3000";
+const apiUrl = `${origin}/api/v1/admin/connect-sources`;
 const chunkSize = 50;
 
-const DEFAULT_EMAILS = [
-    "milan+test1@openearth.org",
-    "milan+test@openearth.org",
-    "amanda@openearth.org",
-    "greta@openearth.org",
-    "marina@openearth.org",
-    "isaac.b@openearth.org",
-    "maureen@openearth.org",
-    "carlos@openearth.org",
-  ];
+const DEFAULT_EMAIL = "greta@openearth.org";
 
-// const EMAILS = DEFAULT_EMAILS;
-const EMAILS = ["evan@openearth.org"];
+const EMAIL = DEFAULT_EMAIL;
+// const EMAIL = "evan@openearth.org";
 
 const DEFAULT_PROJECT_ID = "6169d5a4-0f31-4132-966e-c2feed1b9496";
-// const PROJECT_ID = DEFAULT_PROJECT_ID;
-const PROJECT_ID = "c6592782-ea7d-4231-87f2-39ba8d941ce0";
+const PROJECT_ID = DEFAULT_PROJECT_ID;
+// const PROJECT_ID = "c6592782-ea7d-4231-87f2-39ba8d941ce0";
 
 const baseRequestBody = {
   projectId: PROJECT_ID,
   cityLocodes: [],
-  emails: EMAILS,
-  years: [2022],
-  scope: "gpc_basic_plus",
-  gwp: "AR6",
+  userEmail: EMAIL,
+  years: [2022]
 };
 
 function makeRequest(url: string, body: any, method: string = "POST") {
@@ -52,7 +42,7 @@ function makeRequest(url: string, body: any, method: string = "POST") {
   });
 }
 
-async function createBulkInventories() {
+async function connectBulkInventories() {
   const cityLocodes = fs
     .readFileSync("scripts/data/brazil_locodes.csv")
     .toString()
@@ -68,16 +58,16 @@ async function createBulkInventories() {
     if (!result.ok) {
       const errorText = await result.text();
       throw new Error(
-        `Failed to create bulk inventories (index ${i} to ${i + chunkSize}): ${result.status} - ${errorText}`,
+        `Failed to connect bulk inventories (index ${i} to ${i + chunkSize}): ${result.status} - ${errorText}`,
       );
     }
     console.log(
-      `Created bulk inventories for locodes ${i} to ${i + chunkSize}`,
+      `connected bulk inventories for locodes ${i} to ${i + chunkSize}`,
     );
   }
 }
 
-createBulkInventories().catch((error) => {
+connectBulkInventories().catch((error) => {
   console.error("Error creating bulk inventories:", error);
   process.exit(1);
 });

27e3a30a2a17e2926cf5c3a442a49e7152ac3ea0
Cephas Chapa <cephas@openearth.com>
2026-02-12 11:18:47 +0200

    fix: adds correct import path

--- app/src/util/GHGI/gpc-ref-resolver.ts
+++ app/src/util/GHGI/gpc-ref-resolver.ts
@@ -1,6 +1,6 @@
 import gpcReferenceTable from "./data/gpc-reference-table.json";
 import nameMappings from "./data/gpc-name-mappings.json";
-import type { GPCReferenceRow } from "./types";
+import { GPCReferenceRow } from "scripts/generate-gpc-reference-table";
 
 const table = gpcReferenceTable as GPCReferenceRow[];
 

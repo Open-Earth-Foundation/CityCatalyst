40218a3223b2870295d95964f3b68ecb1a66f872
Milan Gruner <milan@openearth.org>
2026-06-16 13:44:39 +0200

    fix(db): allow removing modules and projects in ProjectModules table constraints

--- app/migrations/20260615135302-remove-duplicate-modules.cjs
+++ app/migrations/20260615135302-remove-duplicate-modules.cjs
@@ -3,6 +3,38 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
+    // allow deleting modules and projects without constraint errors
+    await queryInterface.removeConstraint(
+      "ProjectModules",
+      "ProjectModules_module_id_fkey",
+    );
+    await queryInterface.addConstraint("ProjectModules", {
+      fields: ["module_id"],
+      type: "foreign key",
+      name: "ProjectModules_module_id_fkey",
+      references: {
+        table: "Module",
+        field: "id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+    await queryInterface.removeConstraint(
+      "ProjectModules",
+      "ProjectModules_project_id_fkey",
+    );
+    await queryInterface.addConstraint("ProjectModules", {
+      fields: ["project_id"],
+      type: "foreign key",
+      name: "ProjectModules_project_id_fkey",
+      references: {
+        table: "Project",
+        field: "project_id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+
     // remove duplicate modules GHGI and HIAP (without descriptions)
     await queryInterface.bulkDelete("Module", {
       id: "9295ad69-72c6-4b1c-b29d-b71f7b8ba8e8",
@@ -12,5 +44,33 @@ module.exports = {
     });
   },
 
-  async down() {},
+  async down(queryInterface) {
+    // revert changes to foreign keys
+    await queryInterface.removeConstraint(
+      "ProjectModules",
+      "ProjectModules_module_id_fkey",
+    );
+    await queryInterface.addConstraint("ProjectModules", {
+      fields: ["module_id"],
+      type: "foreign key",
+      name: "ProjectModules_module_id_fkey",
+      references: {
+        table: "Module",
+        field: "id",
+      },
+    });
+    await queryInterface.removeConstraint(
+      "ProjectModules",
+      "ProjectModules_project_id_fkey",
+    );
+    await queryInterface.addConstraint("ProjectModules", {
+      fields: ["project_id"],
+      type: "foreign key",
+      name: "ProjectModules_project_id_fkey",
+      references: {
+        table: "Project",
+        field: "project_id",
+      },
+    });
+  },
 };

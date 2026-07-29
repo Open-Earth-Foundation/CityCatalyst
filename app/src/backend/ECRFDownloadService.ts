34dcc21614f18354c8c38afd30fb6f2fe3e6a024
Milan Gruner <milan@openearth.org>
2026-07-29 18:36:30 +0200

    fix(api): type errors in CSV and ECRF download services

--- app/src/backend/ECRFDownloadService.ts
+++ app/src/backend/ECRFDownloadService.ts
@@ -374,10 +374,12 @@ export default class ECRFDownloadService {
           notation_key: inventoryValue.unavailableReason,
           input_methodology: t(inventoryValue.inputMethodology),
           activityValues: activityValues.map((activityValue) => {
-            const activityTitleKey = activityValue.metadata?.activityTitle;
+            const activityTitleKey =
+              activityValue.metadata?.activityTitle ?? "";
             const dataQuality = activityValue.metadata?.dataQuality;
             const dataSource = activityValue.activityData?.["data-source"];
-            const activityAmount = activityValue.activityData?.[activityTitleKey];
+            const activityAmount =
+              activityValue.activityData?.[activityTitleKey];
             const activityUnit = t(
               activityValue.activityData?.[`${activityTitleKey}-unit`],
             );

f882536d75619404e5da8a4559e40ca70ec32402
Milan Gruner <milan@openearth.org>
2026-08-17 14:56:50 +0200

    draft(api): add unfinished migration for creating MeedActionRanked table

---
+++ app/migrations/20260817120341-create-meed-action-ranked.cjs
@@ -0,0 +1,52 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("MeedActionRanked", {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      inventory_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "Inventory",
+          key: "inventory_id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      action_id: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      lang: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      is_selected: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      created: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      last_updated: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("MeedActionRanked");
+  },
+};

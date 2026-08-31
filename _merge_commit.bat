6c5fb35aa2915fdeb1da63e9a9f06d06e17754c6
Cephas Chapa <cephas@openearth.com>
2026-08-31 10:03:38 +0200

    chore: remove temporary merge script

--- _merge_commit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d c:\Users\frank\Desktop\Workspace\oef\CityCatalyst
-git add -A
-git commit -m "merge(develop): resolve conflicts in HiapApiService and validation"
-git push origin HEAD
-git status -sb

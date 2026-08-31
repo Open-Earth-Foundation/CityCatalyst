6083d2927bfb114cbe2982f5675e70fa4c6712ad
Milan Gruner <milan@openearth.org>
2026-08-31 18:40:54 +0200

    feat(ui): filter out negative emissions in EmissionsForecastChart and adjust description

--- app/src/components/GHGI/inventory-result/EmissionsForecast/EmissionsForecastChart.tsx
+++ app/src/components/GHGI/inventory-result/EmissionsForecast/EmissionsForecastChart.tsx
@@ -138,14 +138,13 @@ const EmissionsForecastChartInner = ({
             ? getSectorByReferenceNumber(sectorRefNo)?.name || sector
             : getSubSectorByReferenceNumber(sector)?.name || sector,
           color: getColorForSeries(sector),
-          data: Object.entries(forecastData.forecast).map(
-            ([year, sectorsData]) => {
-              return {
-                x: year,
-                y: sectorsData[sector] || 0,
-              };
-            },
-          ),
+          data: Object.entries(forecastData.forecast)
+            .map(([year, sectorsData]) => ({
+              x: year,
+              y: sectorsData[sector] || 0,
+            }))
+            // filter out negative emissions (removals), not included in this graph
+            .filter(({ y }) => y >= 0),
         };
       })
       .reverse();

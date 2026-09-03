6ddfbfe34c6427bf684946db1026313a0717f922
Milan Gruner <milan@openearth.org>
2026-09-03 14:11:40 +0200

    feat(db): create migrations for MeedRankSnapshot and MeedActionReport tables

---
+++ app/migrations/20260903120451-create-meed-rank-snapshot.cjs
@@ -0,0 +1,49 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("MeedRankSnapshot", {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      inventory_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "Inventory",
+          key: "inventory_id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      request: {
+        type: Sequelize.JSONB,
+        defaultValue: {},
+        allowNull: false,
+      },
+      response: {
+        type: Sequelize.JSONB,
+        defaultValue: {},
+        allowNull: false,
+      },
+      created: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      last_updated: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("MeedActionReport");
+  },
+};
